313ab97f3b9ac5258f5b8f434d068d204113a783
Mark Ottesen <mottesen@us.ibm.com>
2018-07-25 13:44:36 -0400

    Add typedef guid and name to OMRSInstanceEvent in constructor
    Signed-off-by: Mark Ottesen <mottesen@us.ibm.com>

--- open-metadata-implementation/repository-services/repository-services-apis/src/main/java/org/odpi/openmetadata/repositoryservices/events/OMRSInstanceEvent.java
+++ open-metadata-implementation/repository-services/repository-services-apis/src/main/java/org/odpi/openmetadata/repositoryservices/events/OMRSInstanceEvent.java
@@ -5,9 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.odpi.openmetadata.repositoryservices.events.beans.v1.OMRSEventV1;
 import org.odpi.openmetadata.repositoryservices.events.beans.v1.OMRSEventV1InstanceSection;
-
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.EntityDetail;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.InstanceProvenanceType;
+import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.InstanceType;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.Relationship;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.typedefs.TypeDefSummary;
 
@@ -114,6 +114,11 @@ public class OMRSInstanceEvent extends OMRSEvent
 
         this.instanceEventType = instanceEventType;
         this.entity = entity;
+        InstanceType type = entity.getType();
+        if(type != null) {
+        	this.typeDefGUID = type.getTypeDefGUID();
+        	this.typeDefName = type.getTypeDefName();
+        }
     }
 
 
