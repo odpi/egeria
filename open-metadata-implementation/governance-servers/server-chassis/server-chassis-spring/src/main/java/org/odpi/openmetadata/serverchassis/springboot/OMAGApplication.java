2b239806564d06490a43e9361f703db853cbbf43
Chris Replogle <Chris.Replogle@sas.com>
2018-08-01 14:26:26 -0400

    (EGERIA-18) Adding initial springfox dependency for swagger api exposure
    Signed-off-by: Chris Replogle <Chris.Replogle@sas.com>

--- open-metadata-implementation/governance-servers/server-chassis/server-chassis-spring/src/main/java/org/odpi/openmetadata/serverchassis/springboot/OMAGApplication.java
+++ open-metadata-implementation/governance-servers/server-chassis/server-chassis-spring/src/main/java/org/odpi/openmetadata/serverchassis/springboot/OMAGApplication.java
@@ -4,11 +4,18 @@ package org.odpi.openmetadata.serverchassis.springboot;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan({"org.odpi.openmetadata.*"})
+@EnableSwagger2
 @Configuration
 
 public class OMAGApplication
@@ -19,4 +26,15 @@ public class OMAGApplication
 
         SpringApplication.run(OMAGApplication.class, args);
     }
+
+    @Bean
+    public Docket egeriaAPI()
+    {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
