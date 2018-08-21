b9d77b10fe2fb1a5bd1bc79a312d92b839d64940
Bogdan Sava <bogdan-mihail.sava@ing.com>
2018-08-21 17:14:08 +0300

    Adding possibility to disable strict ssl validation via configuration file
    Signed-off-by: Bogdan Sava <bogdan-mihail.sava@ing.com>

---
+++ open-metadata-implementation/adapters/authentication-plugins/http-helper/src/main/java/org/odpi/openmetadata/http/HttpHelper.java
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+package org.odpi.openmetadata.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+
+public class HttpHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpHelper.class);
+
+    /**
+     * Allows using self signed certificates https connections
+     * makes all the clients and servers trusted no matter the certificate
+     */
+    public static void noStrictSSL(){
+
+        LOGGER.warn("Strict SSL is set to false! Invalid certificates will be accepted for connection!");
+
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                    public void checkClientTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier ((hostname, session) -> true);
+        } catch (GeneralSecurityException e) {
+            LOGGER.error("The configuration for no strict SSL went wrong");
+        }
+    }
+
+}
