feea002ba7e9bcf8567672a55b25bc9b86db4468
Wu Yue Feng <wyf1116@gmail.com>
2018-11-05 14:43:28 +0100

    Revert the security by pass. Change the solution of un-nested categories.
    Signed-off-by: Wu Yue Feng <wyf1116@gmail.com>

--- open-metadata-resources/open-metadata-demos/virtual-data-connector/igc-repository-proxy/src/main/java/org/odpi/openmetadata/virtualdataconnector/igc/connectors/repositoryconnector/IGCOMRSRepositoryConnector.java
+++ open-metadata-resources/open-metadata-demos/virtual-data-connector/igc-repository-proxy/src/main/java/org/odpi/openmetadata/virtualdataconnector/igc/connectors/repositoryconnector/IGCOMRSRepositoryConnector.java
@@ -34,10 +34,34 @@ public class IGCOMRSRepositoryConnector extends OMRSRepositoryConnector {
      * Default constructor used by the OCF Connector Provider.
      */
     public IGCOMRSRepositoryConnector() {
-
+        disableSslVerification();
     }
 
-
+    private static void disableSslVerification() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+            };
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = (hostname, session) -> true;
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Set up the unique Id for this metadata collection.
