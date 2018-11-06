ccd1d6a6b7b3015f64c9a20932dfb864217ce6b3
David Radley <david_radley@uk.ibm.com>
2018-11-06 22:43:02 +0000

    #350 fix layout in response to review
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- open-metadata-implementation/adapters/open-connectors/repository-services-connectors/open-metadata-collection-store-connectors/inmemory-repository-connector/src/main/java/org/odpi/openmetadata/adapters/repositoryservices/inmemory/repositoryconnector/InMemoryEntityNeighbourhood.java
+++ open-metadata-implementation/adapters/open-connectors/repository-services-connectors/open-metadata-collection-store-connectors/inmemory-repository-connector/src/main/java/org/odpi/openmetadata/adapters/repositoryservices/inmemory/repositoryconnector/InMemoryEntityNeighbourhood.java
@@ -15,81 +15,65 @@ import java.util.*;
  */
 public class InMemoryEntityNeighbourhood
 {
-    /* Initialized in constructor */
-    private  OMRSRepositoryValidator repositoryValidator = null;
-    /* Initialized in constructor */
-    private Map<String, EntityDetail>   entityStore = null;
-    /* Initialized in constructor */
-    private Map<String, Relationship>   relationshipStore =null;
+    private OMRSRepositoryValidator repositoryValidator = null;
+    private Map<String, EntityDetail> entityStore = null;
+    private Map<String, Relationship> relationshipStore = null;
     private String rootEntityGUID = null;
-    private List<String>         entityTypeGUIDs = null;
-    private List<String>         relationshipTypeGUIDs = null;
+    private List<String> entityTypeGUIDs = null;
+    private List<String> relationshipTypeGUIDs = null;
     private List<InstanceStatus> limitResultsByStatus = null;
-    private List<String>         limitResultsByClassification =null;
-    private int                  level =0;
-
-
-    Set<String> graphEntities = new HashSet<>();
-    Set<String> graphRelationships = new HashSet<>();
-
-    private Map<String,Set<String>> entityToRelationships= new HashMap<String,Set<String>>();
-    private Map<String,Set<String>> relationshipToEntities= new HashMap<String,Set<String>>();
+    private List<String> limitResultsByClassification = null;
+    private int level = 0;
+    private Set<String> graphEntities = new HashSet<>();
+    private Set<String> graphRelationships = new HashSet<>();
+    private Map<String, Set<String>> entityToRelationships = new HashMap<String, Set<String>>();
+    private Map<String, Set<String>> relationshipToEntities = new HashMap<String, Set<String>>();
 
     /**
-     * Contructor
-     * @param repositoryValidator repository validator
-     * @param entityStore          entity store
-     * @param relationshipStore relationship store
-     * @param rootEntityGUID the starting point of the query.
-     * @param entityTypeGUIDs list of entity types to include in the query results.  Null means include
-     *                          all entities found, irrespective of their type.
-     * @param relationshipTypeGUIDs list of relationship types to include in the query results.  Null means include
-     *                          all entities found, irrespective of their type.
-     * @param limitResultsByStatus By default, relationships in all statuses are returned.  However, it is possible
-     *                             to specify a list of statuses (eg ACTIVE) to restrict the results to.  Null means all
-     *                             status values.
+     * Constructor
+     *
+     * @param repositoryValidator          repository validator
+     * @param entityStore                  entity store
+     * @param relationshipStore            relationship store
+     * @param rootEntityGUID               the starting point of the query.
+     * @param entityTypeGUIDs              list of entity types to include in the query results.  Null means include
+     *                                     all entities found, irrespective of their type.
+     * @param relationshipTypeGUIDs        list of relationship types to include in the query results.  Null means include
+     *                                     all entities found, irrespective of their type.
+     * @param limitResultsByStatus         By default, relationships in all statuses are returned.  However, it is possible
+     *                                     to specify a list of statuses (eg ACTIVE) to restrict the results to.  Null means all
+     *                                     status values.
      * @param limitResultsByClassification List of classifications that must be present on all returned entities.
-     * @param level the number of the relationships out from the starting entity that the query will traverse to
-     *              gather results.
-     * @throws RepositoryErrorException there is a problem communicating with the metadata repository where
-     *                                  the metadata collection is stored.
+     * @param level                        the number of the relationships out from the starting entity that the query will traverse to
+     *                                     gather results.
+     * @throws RepositoryErrorException  there is a problem communicating with the metadata repository where
+     *                                   the metadata collection is stored.
      * @throws InvalidParameterException one of the parameters is invalid or null.
      */
-    public InMemoryEntityNeighbourhood(
-                                       OMRSRepositoryValidator repositoryValidator,
-                                       Map<String, EntityDetail>   entityStore,
-                                       Map<String, Relationship>   relationshipStore,
-                                       String               rootEntityGUID,
-                                       List<String>         entityTypeGUIDs,
-                                       List<String>         relationshipTypeGUIDs,
-                                       List<InstanceStatus> limitResultsByStatus,
-                                       List<String>         limitResultsByClassification,
-                                       int                  level) throws RepositoryErrorException, InvalidParameterException
+    public InMemoryEntityNeighbourhood(OMRSRepositoryValidator repositoryValidator, Map<String, EntityDetail> entityStore, Map<String, Relationship> relationshipStore, String rootEntityGUID, List<String> entityTypeGUIDs, List<String> relationshipTypeGUIDs, List<InstanceStatus> limitResultsByStatus, List<String> limitResultsByClassification, int level) throws RepositoryErrorException, InvalidParameterException
     {
-        this.repositoryValidator =repositoryValidator;
+        this.repositoryValidator = repositoryValidator;
         this.entityStore = entityStore;
         this.relationshipStore = relationshipStore;
         this.rootEntityGUID = rootEntityGUID;
-        this.entityTypeGUIDs =entityTypeGUIDs;
+        this.entityTypeGUIDs = entityTypeGUIDs;
         this.relationshipTypeGUIDs = relationshipTypeGUIDs;
-        this.limitResultsByStatus =limitResultsByStatus;
-        this.limitResultsByClassification=limitResultsByClassification;
+        this.limitResultsByStatus = limitResultsByStatus;
+        this.limitResultsByClassification = limitResultsByClassification;
         // limit the level
-        if (level <1 || level >10 ) {
-            level =10;
+        if (level < 1 || level > 10)
+        {
+            level = 10;
         }
-
         this.level = level;
-
         initializeMaps();
-
     }
 
     /**
      * Initialize maps that help us traverse between entities and relationships using their guids
      *
-     * @throws RepositoryErrorException there is a problem communicating with the metadata repository where
-     *                                  the metadata collection is stored.
+     * @throws RepositoryErrorException  there is a problem communicating with the metadata repository where
+     *                                   the metadata collection is stored.
      * @throws InvalidParameterException one of the parameters is invalid or null.
      */
     private void initializeMaps() throws RepositoryErrorException, InvalidParameterException
@@ -99,44 +83,49 @@ public class InMemoryEntityNeighbourhood
             String relationshipGuid = relationship.getGUID();
             String relationshipEnd1Guid = getEnd1EntityGUID(relationship);
             String relationshipEnd2Guid = getEnd2EntityGUID(relationship);
-//            EntityDetail entity1 = entityStore.get(relationshipEnd1Guid);
-//            EntityDetail entity2 = entityStore.get(relationshipEnd2Guid);
             Set<String> endsGuids = new HashSet<>();
             endsGuids.add(relationshipEnd1Guid);
             endsGuids.add(relationshipEnd2Guid);
-            relationshipToEntities.put(relationshipGuid,endsGuids);
+            relationshipToEntities.put(relationshipGuid, endsGuids);
             Set<String> relationshipGuids = null;
             // process end1
-            if (entityToRelationships.containsKey(relationshipEnd1Guid)) {
+            if (entityToRelationships.containsKey(relationshipEnd1Guid))
+            {
                 relationshipGuids = entityToRelationships.get(relationshipEnd1Guid);
-                if (relationshipGuids ==null) {
+                if (relationshipGuids == null)
+                {
                     relationshipGuids = new HashSet<>();
                 }
-            } else {
+            } else
+            {
                 relationshipGuids = new HashSet<>();
             }
             relationshipGuids.add(relationshipGuid);
-            entityToRelationships.put(relationshipEnd1Guid,relationshipGuids);
+            entityToRelationships.put(relationshipEnd1Guid, relationshipGuids);
             // process end2
-            if (entityToRelationships.containsKey(relationshipEnd2Guid)) {
+            if (entityToRelationships.containsKey(relationshipEnd2Guid))
+            {
                 relationshipGuids = entityToRelationships.get(relationshipEnd2Guid);
-                if (relationshipGuids ==null) {
+                if (relationshipGuids == null)
+                {
                     relationshipGuids = new HashSet<>();
                 }
-            } else {
+            } else
+            {
                 relationshipGuids = new HashSet<>();
             }
             relationshipGuids.add(relationshipGuid);
-            entityToRelationships.put(relationshipEnd2Guid,relationshipGuids);
+            entityToRelationships.put(relationshipEnd2Guid, relationshipGuids);
         }
     }
 
     /**
      * Verify that the supplied relationship and the 2 entities that enclose it are valid , bying chaecking the scoping conditions
+     *
      * @param relationship relationship to verify
      * @return true if valid otherwise false
-     * @throws RepositoryErrorException there is a problem communicating with the metadata repository where
-     *                                  the metadata collection is stored.
+     * @throws RepositoryErrorException  there is a problem communicating with the metadata repository where
+     *                                   the metadata collection is stored.
      * @throws InvalidParameterException one of the parameters is invalid or null.
      */
     private boolean verifyRelationshipForEntityNeighbourhood(Relationship relationship) throws RepositoryErrorException, InvalidParameterException
@@ -150,8 +139,8 @@ public class InMemoryEntityNeighbourhood
             String relationshipEnd1Guid = getEnd1EntityGUID(relationship);
             String relationshipEnd2Guid = getEnd2EntityGUID(relationship);
             EntityDetail entity1 = entityStore.get(relationshipEnd1Guid);
-            EntityDetail entity2  = entityStore.get(relationshipEnd2Guid);
-            if (relationshipTypeGUIDs !=null)
+            EntityDetail entity2 = entityStore.get(relationshipEnd2Guid);
+            if (relationshipTypeGUIDs != null)
             {
                 for (String relationshipTypeGUID : relationshipTypeGUIDs)
                 {
@@ -160,17 +149,15 @@ public class InMemoryEntityNeighbourhood
                         validRelationship = true;
                     }
                 }
-            } else {
+            } else
+            {
                 validRelationship = true;
             }
             if (repositoryValidator.verifyInstanceHasRightStatus(limitResultsByStatus, relationship))
             {
-                if (limitResultsByClassification ==null ||
-                                limitResultsByClassification != null
-                            && (repositoryValidator.verifyEntityIsClassified(limitResultsByClassification, entity1)
-                            && (repositoryValidator.verifyEntityIsClassified(limitResultsByClassification, entity2))))
+                if (limitResultsByClassification == null || limitResultsByClassification != null && (repositoryValidator.verifyEntityIsClassified(limitResultsByClassification, entity1) && (repositoryValidator.verifyEntityIsClassified(limitResultsByClassification, entity2))))
                 {
-                    if (entityTypeGUIDs !=null)
+                    if (entityTypeGUIDs != null)
                     {
                         for (String typeGUID1 : entityTypeGUIDs)
                         {
@@ -191,13 +178,14 @@ public class InMemoryEntityNeighbourhood
 
                                     if (repositoryValidator.verifyInstanceHasRightStatus(limitResultsByStatus, entity2))
                                     {
-                                        validEntity2= true;
+                                        validEntity2 = true;
                                     }
                                 }
                             }
 
                         }
-                    } else {
+                    } else
+                    {
                         validEntity1 = true;
                         validEntity2 = true;
                     }
@@ -205,17 +193,19 @@ public class InMemoryEntityNeighbourhood
             }
         }
 
-        if (validEntity1 && validEntity2 && validRelationship) {
-            valid =true;
+        if (validEntity1 && validEntity2 && validRelationship)
+        {
+            valid = true;
         }
         return valid;
     }
 
     /**
      * Create the instance graph
+     *
      * @return InstanceGraph  the instance graph that contains the entities and relationships that radiate out from the supplied entity GUID.
-     * @throws RepositoryErrorException there is a problem communicating with the metadata repository where
-     *                                  the metadata collection is stored.
+     * @throws RepositoryErrorException  there is a problem communicating with the metadata repository where
+     *                                   the metadata collection is stored.
      * @throws InvalidParameterException one of the parameters is invalid or null.
      */
     public InstanceGraph createInstanceGraph() throws RepositoryErrorException, InvalidParameterException
@@ -224,40 +214,44 @@ public class InMemoryEntityNeighbourhood
         Set<String> visitedEntities = new HashSet<>();
         Set<String> visitedRelationships = new HashSet<>();
         entities.add(rootEntityGUID);
-        this.createGraph(entities,visitedEntities,visitedRelationships,0);
+        this.createGraph(entities, visitedEntities, visitedRelationships, 0);
         List entityList = new ArrayList();
         // add the root entity so the returned graph is consistent.
         List relationshipList = new ArrayList();
         EntityDetail rootEntity = (entityStore.get(rootEntityGUID));
         entityList.add(rootEntity);
-        for (String entityGuid:this.graphEntities) {
+        for (String entityGuid : this.graphEntities)
+        {
             if (!entityGuid.equals(rootEntityGUID))
             {
                 entityList.add(this.entityStore.get(entityGuid));
             }
         }
-        for (String relationshipGuid:this.graphRelationships) {
+        for (String relationshipGuid : this.graphRelationships)
+        {
             relationshipList.add(this.relationshipStore.get(relationshipGuid));
         }
-        return new InstanceGraph(entityList,relationshipList);
+        return new InstanceGraph(entityList, relationshipList);
     }
 
     /**
      * Create the graph populates amps with entities and relationships that are required in the instance graph. This is a recursive method that is called for each level that is required.
-     * @param entities the entities on which we need to find the relationships out to the radiated level out.
-     * @param visitedEntities the entities that have already been visited (seen)
+     *
+     * @param entities             the entities on which we need to find the relationships out to the radiated level out.
+     * @param visitedEntities      the entities that have already been visited (seen)
      * @param visitedRelationships the relationship that have already been visited (seen)
-     * @param currentLevel the current level
-     * @throws RepositoryErrorException there is a problem communicating with the metadata repository where
-     *                                  the metadata collection is stored.
+     * @param currentLevel         the current level
+     * @throws RepositoryErrorException  there is a problem communicating with the metadata repository where
+     *                                   the metadata collection is stored.
      * @throws InvalidParameterException one of the parameters is invalid or null.
      */
-    private void createGraph(Set<String> entities,Set visitedEntities,Set visitedRelationships,int currentLevel ) throws RepositoryErrorException, InvalidParameterException
+    private void createGraph(Set<String> entities, Set visitedEntities, Set visitedRelationships, int currentLevel) throws RepositoryErrorException, InvalidParameterException
     {
         Set<String> nextEntitySet = new HashSet<>();
-        for (String entityGuid :entities) {
-            Set<String> relationships =this.entityToRelationships.get(entityGuid);
-            if (relationships !=null)
+        for (String entityGuid : entities)
+        {
+            Set<String> relationships = this.entityToRelationships.get(entityGuid);
+            if (relationships != null)
             {
                 for (String relationshipGuid : relationships)
                 {
@@ -292,19 +286,21 @@ public class InMemoryEntityNeighbourhood
                 }
             }
         }
-        int nextLevel = currentLevel+1;
-        if (nextLevel < this.level && nextEntitySet.size() >0) {
+        int nextLevel = currentLevel + 1;
+        if (nextLevel < this.level && nextEntitySet.size() > 0)
+        {
             // recurse
-            createGraph(nextEntitySet,visitedEntities,visitedRelationships,nextLevel);
+            createGraph(nextEntitySet, visitedEntities, visitedRelationships, nextLevel);
         }
     }
+
     /**
      * Return the guid of an entity linked to end 1 of the relationship.
      *
      * @param relationship relationship to parse
      * @return String unique identifier
      */
-   private String  getEnd1EntityGUID(Relationship   relationship)
+    private String getEnd1EntityGUID(Relationship relationship)
     {
         final String methodName = "getEnd1EntityGUID";
 
@@ -322,13 +318,14 @@ public class InMemoryEntityNeighbourhood
         }
         return null;
     }
+
     /**
      * Return the guid of an entity linked to end 1 of the relationship.
      *
      * @param relationship relationship to parse
      * @return String unique identifier
      */
-    private String  getEnd2EntityGUID(Relationship   relationship)
+    private String getEnd2EntityGUID(Relationship relationship)
     {
         final String methodName = "getEnd2EntityGUID";
 
