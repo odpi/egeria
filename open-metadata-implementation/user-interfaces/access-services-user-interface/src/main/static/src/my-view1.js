7fa7466f43e0d414e6fdfde41129045bc46c0898
bogdan-sava <bogdan-mihail.sava@ing.com>
2018-11-07 14:25:52 +0200

    fix license in view files
    Signed-off-by: bogdan-sava <bogdan-mihail.sava@ing.com>

---
+++ open-metadata-implementation/user-interfaces/access-services-user-interface/src/main/static/src/my-view1.js
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import './shared-styles.js';
+
+class MyView1 extends PolymerElement {
+  static get template() {
+    return html`
+      <style include="shared-styles">
+        :host {
+          display: block;
+
+          padding: 10px;
+        }
+      </style>
+
+      <div class="card">
+        <div class="circle">1</div>
+        <h1>View One</h1>
+        <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
+        <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
+      </div>
+    `;
+  }
+}
+
+window.customElements.define('my-view1', MyView1);
