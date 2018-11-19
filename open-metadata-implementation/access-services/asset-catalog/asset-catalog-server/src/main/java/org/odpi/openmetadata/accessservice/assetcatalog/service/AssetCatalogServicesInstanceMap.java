cdaf280b558f3060a04202bfddbd7a474bda8d22
Mandy Chessell <mandy_chessell@uk.ibm.com>
2018-11-19 20:59:19 +0000

    Update Asset Catalog OMAS REST APIs to support mulitple server instances (#334) Improve OMAS registration process so multiple registrations are ignored.
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

---
+++ open-metadata-implementation/access-services/asset-catalog/asset-catalog-server/src/main/java/org/odpi/openmetadata/accessservice/assetcatalog/service/AssetCatalogServicesInstanceMap.java
@@ -0,0 +1,79 @@
+/* SPDX-License-Identifier: Apache 2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+package org.odpi.openmetadata.accessservice.assetcatalog.service;
+
+
+import org.odpi.openmetadata.accessservice.assetcatalog.exception.NewInstanceException;
+import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryConnector;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * AssetCatalogServicesInstanceMap provides the mapping for inbound REST requests to the appropriate instances
+ * for the requested server.  The map is maintained in a static so it is scoped to the class loader.
+ *
+ * Instances of this class call the synchronized static methods to work with the map.
+ */
+public class AssetCatalogServicesInstanceMap
+{
+    private static  Map<String, AssetCatalogServicesInstance>   instanceMap = new HashMap<>();
+
+
+    /**
+     * Add a new server instance to the server map.
+     *
+     * @param serverName name of the server
+     * @param instance instance object
+     */
+    static synchronized void  setNewInstanceForJVM(String                         serverName,
+                                                   AssetCatalogServicesInstance   instance)
+    {
+        instanceMap.put(serverName, instance);
+    }
+
+
+    /**
+     * Return the instance for this server.
+     *
+     * @param serverName name of the server
+     * @return OMRSRepositoryServicesInstance object
+     */
+    private static synchronized AssetCatalogServicesInstance getInstanceForJVM(String    serverName)
+    {
+        AssetCatalogServicesInstance   instance = instanceMap.get(serverName);
+
+        return instance;
+    }
+
+
+    /**
+     * Remove the instance for this server.
+     *
+     * @param serverName name of the server
+     */
+    static synchronized void removeInstanceForJVM(String   serverName)
+    {
+        instanceMap.remove(serverName);
+    }
+
+
+    /**
+     * Constructor
+     */
+    public AssetCatalogServicesInstanceMap()
+    {
+    }
+
+
+    /**
+     * Return the instance for this server.
+     *
+     * @param serverName name of the server
+     * @return OMRSRepositoryServicesInstance object
+     */
+    public AssetCatalogServicesInstance getInstance(String    serverName)
+    {
+        return AssetCatalogServicesInstanceMap.getInstanceForJVM(serverName);
+    }
+}
