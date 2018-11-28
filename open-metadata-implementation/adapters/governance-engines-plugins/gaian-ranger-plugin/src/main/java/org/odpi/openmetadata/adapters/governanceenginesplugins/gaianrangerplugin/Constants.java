4ff655b069b5a5b46071b1aaac242adbbe8d8d4a
Daniela Otelea <daniela-valentina.otelea@ing.com>
2018-11-28 12:11:05 +0200

    Gaian Ranger plugin - LDAP groups added, clean the  dependencies, added the javadoc
    Signed-off-by: Daniela Otelea <daniela-valentina.otelea@ing.com>

---
+++ open-metadata-implementation/adapters/governance-engines-plugins/gaian-ranger-plugin/src/main/java/org/odpi/openmetadata/adapters/governanceenginesplugins/gaianrangerplugin/Constants.java
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+package org.odpi.openmetadata.adapters.governanceenginesplugins.gaianrangerplugin;
+
+public class Constants {
+
+    public static final String DEFAULT_SERVICE_TYPE = "gaian";
+    public static final String DEFAULT_APP_ID = "gaian";
+
+    public static final String SCHEMA_RESOURCE = "SCHEMA";
+    public static final String TABLE_RESOURCE = "TABLE";
+    public static final String COLUMN_RESOURCE = "COLUMN";
+
+    public static final String SELECT_ACTION = "SELECT";
+
+    public static final String DEFAULT_GAIAN_DB_USER = "gaiandb";
+    public static final String DEFAULT_SCHEMA = "Gaian";
+    public static final String UNKNOWN_USER = "<UNKNOWN>";
+
+    public static final String NULL_MASK_TYPE = "NULL";
+}
---
+++ open-metadata-implementation/adapters/governance-engines-plugins/gaian-ranger-plugin/src/main/java/org/odpi/openmetadata/adapters/governanceenginesplugins/gaianrangerplugin/GaianResourceType.java
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+package org.odpi.openmetadata.adapters.governanceenginesplugins.gaianrangerplugin;
+
+public enum GaianResourceType {
+    NONE,
+    SCHEMA,
+    TABLE,
+    COLUMN
+}
