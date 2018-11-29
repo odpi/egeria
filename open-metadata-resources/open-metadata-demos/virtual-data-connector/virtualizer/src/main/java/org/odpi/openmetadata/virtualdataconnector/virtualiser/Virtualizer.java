0f6ddfe42e59be0b711521774b44cea801b1d7e6
Wu Yue Feng <wyf1116@gmail.com>
2018-11-29 23:41:07 +0100

    Added RestController to Virtualizer.
    Signed-off-by: Wu Yue Feng <wyf1116@gmail.com>

--- open-metadata-resources/open-metadata-demos/virtual-data-connector/virtualizer/src/main/java/org/odpi/openmetadata/virtualdataconnector/virtualiser/Virtualizer.java
+++ open-metadata-resources/open-metadata-demos/virtual-data-connector/virtualizer/src/main/java/org/odpi/openmetadata/virtualdataconnector/virtualiser/Virtualizer.java
@@ -2,17 +2,13 @@
 /* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.virtualdataconnector.virtualiser;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
-@Configuration
-@ComponentScan
 public class Virtualizer {
 
     public static void main(String[] args) {
