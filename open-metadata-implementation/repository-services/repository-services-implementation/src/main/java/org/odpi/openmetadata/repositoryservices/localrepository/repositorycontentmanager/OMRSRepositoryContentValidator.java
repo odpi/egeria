cda414a2ea765472fcc7d105630b7fabc5d97f97
Mandy Chessell <mandy_chessell@uk.ibm.com>
2018-12-18 22:16:17 +0000

    Renamed open-metadata-compliance-test to open-metadata-conformance-test (#474) When I reran the suite against the in-memory repository it was failing with an NPE while managing entities and aRelationshipNotKnownException from restoreRelationship.  These are both fixed and the suite runs cleanly. In the process of debugging those problems I noticed some inefficiencies in the way we build InstanceTypes in the repository content manager.  It now has a new cache of InstanceTypes and SuperTypeLists for typeDefs.  This cache is build lazily as instances are requested.
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/repository-services/repository-services-implementation/src/main/java/org/odpi/openmetadata/repositoryservices/localrepository/repositorycontentmanager/OMRSRepositoryContentValidator.java
+++ open-metadata-implementation/repository-services/repository-services-implementation/src/main/java/org/odpi/openmetadata/repositoryservices/localrepository/repositorycontentmanager/OMRSRepositoryContentValidator.java
@@ -11,10 +11,7 @@ import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollec
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryValidator;
 import org.odpi.openmetadata.repositoryservices.ffdc.exception.*;
 
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * OMRSRepositoryContentValidator provides methods to validate TypeDefs and Instances returned from
@@ -2833,15 +2830,11 @@ public class OMRSRepositoryContentValidator implements OMRSRepositoryValidator
                 RelationshipEndDef entityTwoEndDef      = null;
                 TypeDefLink        entityOneTypeDef     = null;
                 TypeDefLink        entityTwoTypeDef     = null;
-                String             entityOneTypeDefGUID = null;
                 String             entityOneTypeDefName = null;
-                String             entityTwoTypeDefGUID = null;
                 String             entityTwoTypeDefName = null;
                 InstanceType       entityOneType        = null;
                 InstanceType       entityTwoType        = null;
-                String             entityOneTypeGUID    = null;
                 String             entityOneTypeName    = null;
-                String             entityTwoTypeGUID    = null;
                 String             entityTwoTypeName    = null;
 
 
@@ -2849,55 +2842,48 @@ public class OMRSRepositoryContentValidator implements OMRSRepositoryValidator
                 {
                     entityOneEndDef = relationshipDef.getEndDef1();
                     entityTwoEndDef = relationshipDef.getEndDef2();
+                    log.debug("Got EndDefs");
                 }
 
                 if ((entityOneEndDef != null) && (entityTwoEndDef != null))
                 {
                     entityOneTypeDef = entityOneEndDef.getEntityType();
                     entityTwoTypeDef = entityTwoEndDef.getEntityType();
+                    log.debug("Got EndDef Types");
                 }
 
                 if ((entityOneTypeDef != null) && (entityTwoTypeDef != null))
                 {
-                    entityOneTypeDefGUID = entityOneTypeDef.getGUID();
                     entityOneTypeDefName = entityOneTypeDef.getName();
-                    entityTwoTypeDefGUID = entityTwoTypeDef.getGUID();
                     entityTwoTypeDefName = entityTwoTypeDef.getName();
+                    log.debug("Got EndDef Types of One: " + entityOneTypeDefName + " and Two: " + entityTwoTypeDefName);
+
                 }
 
+                /*
+                 * At this point we know what the expected types of
+                 * the ends should be.  Now look at the relationship instance.
+                 */
                 if ((entityOneProxy != null) && (entityTwoProxy != null))
                 {
                     entityOneType = entityOneProxy.getType();
                     entityTwoType = entityTwoProxy.getType();
+
+                    log.debug("Got Proxy Types of One: " + entityOneType + " and Two: " + entityOneType);
                 }
 
                 if ((entityOneType != null) && (entityTwoType != null))
                 {
-                    entityOneTypeGUID = entityOneType.getTypeDefGUID();
                     entityOneTypeName = entityOneType.getTypeDefName();
-                    entityTwoTypeGUID = entityTwoType.getTypeDefGUID();
                     entityTwoTypeName = entityTwoType.getTypeDefName();
                 }
 
-                if ((entityOneTypeDefGUID != null) && (entityOneTypeDefName != null) &&
-                    (entityTwoTypeDefGUID != null) && (entityTwoTypeDefName != null) &&
-                    (entityOneTypeGUID != null)    && (entityOneTypeName != null)    &&
-                    (entityTwoTypeGUID != null)    && (entityTwoTypeName != null))
+                if ((repositoryContentManager.isTypeOf(sourceName, entityOneTypeName, entityOneTypeDefName) &&
+                     repositoryContentManager.isTypeOf(sourceName, entityTwoTypeName, entityTwoTypeDefName)))
                 {
-                    List<TypeDefLink> entityOneAllTypes = entityOneType.getTypeDefSuperTypes();
-                    entityOneAllTypes.add(new TypeDefLink(entityOneTypeGUID, entityOneTypeName));
-                    List<TypeDefLink> entityTwoAllTypes = entityTwoType.getTypeDefSuperTypes();
-                    entityTwoAllTypes.add(new TypeDefLink(entityTwoTypeGUID, entityTwoTypeName));
-
-                    String finalEntityOneTypeDefGUID = entityOneTypeDefGUID;
-                    String finalEntityOneTypeDefName = entityOneTypeDefName;
-                    String finalEntityTwoTypeDefGUID = entityTwoTypeDefGUID;
-                    String finalEntityTwoTypeDefName = entityTwoTypeDefName;
-                    if (entityOneAllTypes.stream().anyMatch(e -> e.getGUID().equals(finalEntityOneTypeDefGUID) && e.getName().equals(finalEntityOneTypeDefName)) &&
-                            entityTwoAllTypes.stream().anyMatch(e -> e.getGUID().equals(finalEntityTwoTypeDefGUID) && e.getName().equals(finalEntityTwoTypeDefName))) {
-                        return;
-                    }
+                    return;
                 }
+
 
                 OMRSErrorCode errorCode = OMRSErrorCode.INVALID_RELATIONSHIP_ENDS;
                 String errorMessage = errorCode.getErrorMessageId()
