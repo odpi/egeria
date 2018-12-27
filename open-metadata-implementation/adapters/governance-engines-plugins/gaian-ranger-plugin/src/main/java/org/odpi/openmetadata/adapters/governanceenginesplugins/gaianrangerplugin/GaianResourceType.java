d609651d0f981417f96b48098e094f1e323625df
Nigel Jones <nigel.l.jones+git@gmail.com>
2018-12-27 17:50:49 +0000

    #439 corrected bad headers (2xlicense, 0xcopyright) in GaianResourceType.java
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- open-metadata-implementation/adapters/governance-engines-plugins/gaian-ranger-plugin/src/main/java/org/odpi/openmetadata/adapters/governanceenginesplugins/gaianrangerplugin/GaianResourceType.java
+++ open-metadata-implementation/adapters/governance-engines-plugins/gaian-ranger-plugin/src/main/java/org/odpi/openmetadata/adapters/governanceenginesplugins/gaianrangerplugin/GaianResourceType.java
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
-/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.adapters.governanceenginesplugins.gaianrangerplugin;
 
 public enum GaianResourceType {
