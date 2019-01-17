87b82a990b3e2b760f1eb3180aed9ca22d3e14d3
Daniela Otelea <daniela-valentina.otelea@ing.com>
2019-01-17 18:24:36 +0200

    Security Sync Initializing -  Initial Commit
    Security Sync component can be configured using this endpoint:
    {{omas-url}}/open-metadata/security-sync/users/{{user-id-omas}}/servers/{{server-id-omas}}/configuration
    
    Security Sync is able to listen on GE OMAS output topic*.
    Using the configuration, the Ranger Server URL is provided.
    
    Note:
    * Tthe processing will be added in the SecuritySyncProcessor after the GE OMAS changes are merged
    (the processing is based on new classes from GE OMAS component)
    
    Configuration Example:
    {
    	"class": "SecuritySyncConfig",
    	"securitySyncId": 1,
    	"securitySyncDescription": "Security Sync Service is used to synchronize the tags for assets from the Open Metadata Repository with Apache Ranger Server",
    	"securityServerURL": "http://localhost:6080",
    	"securityServerType": "Ranger",
    	"securityServerAuthorization": "Basic YWRtaW46YWRtaW4",
    	"tagServiceName": "tagsvc",
    	"securitySyncInTopicName": "open-metadata.access-services.GovernanceEngine.outTopic",
    	"securitySyncAdminClass": "org.odpi.openmetadata.securitysyncservices.admin.SecuritySync",
    	"securitySyncWiki": "https://odpi.github.io/egeria/open-metadata-implementation/governance-servers/security-sync-services/"
    }
    
    Signed-off-by: Daniela Otelea <daniela-valentina.otelea@ing.com>

--- open-metadata-implementation/governance-servers/admin-services/admin-services-api/src/main/java/org/odpi/openmetadata/adminservices/configuration/properties/SecuritySyncConfig.java
+++ open-metadata-implementation/governance-servers/admin-services/admin-services-api/src/main/java/org/odpi/openmetadata/adminservices/configuration/properties/SecuritySyncConfig.java
@@ -5,6 +5,9 @@ package org.odpi.openmetadata.adminservices.configuration.properties;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import org.odpi.openmetadata.frameworks.connectors.properties.beans.Connection;
+
+import java.util.Objects;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY;
@@ -13,16 +16,32 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_
 /**
  * SecuritySyncConfig provides the properties for the security-sync-services.
  */
-@JsonAutoDetect(getterVisibility=PUBLIC_ONLY, setterVisibility=PUBLIC_ONLY, fieldVisibility=NONE)
+@JsonAutoDetect(getterVisibility = PUBLIC_ONLY, setterVisibility = PUBLIC_ONLY, fieldVisibility = NONE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown=true)
-public class SecuritySyncConfig extends AdminServicesConfigHeader
-{
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SecuritySyncConfig extends AdminServicesConfigHeader {
+
+    private int securitySyncId = 0;
+    private String securitySyncAdminClass;
+    private String securitySyncName;
+    private String securitySyncDescription;
+    private String securitySyncWiki;
+
+    private String securityServerURL;
+    private String securityServerType;
+    private String securityServerAuthorization;
+    private String tagServiceName;
+
+    private String securitySyncInTopicName;
+    private Connection securitySyncInTopic;
+
+    private String securitySyncOutTopicName;
+    private Connection securitySyncOutTopic;
+
     /**
      * Default constructor
      */
-    public SecuritySyncConfig()
-    {
+    public SecuritySyncConfig() {
         super();
     }
 
@@ -32,25 +51,148 @@ public class SecuritySyncConfig extends AdminServicesConfigHeader
      *
      * @param template object to copy
      */
-    public SecuritySyncConfig(SecuritySyncConfig  template)
-    {
+    public SecuritySyncConfig(SecuritySyncConfig template) {
         super(template);
 
-        if (template != null)
-        {
+        if (template != null) {
+            securitySyncId = template.securitySyncId;
+            securitySyncAdminClass = template.securitySyncAdminClass;
+            securitySyncName = template.securitySyncName;
+            securitySyncDescription = template.securitySyncDescription;
+            securitySyncWiki = template.securitySyncWiki;
 
+            securityServerURL = template.securityServerURL;
+            securityServerAuthorization = template.securityServerAuthorization;
+            tagServiceName = template.tagServiceName;
+
+            securitySyncInTopic = template.securitySyncInTopic;
+
+            securitySyncOutTopic = template.securitySyncOutTopic;
         }
     }
 
+    public int getSecuritySyncId() {
+        return securitySyncId;
+    }
 
+    public void setSecuritySyncId(int securitySyncId) {
+        this.securitySyncId = securitySyncId;
+    }
 
-    /**
-     * Standard toString method.
-     *
-     * @return JSON style description of variables.
-     */
+    public String getSecuritySyncAdminClass() {
+        return securitySyncAdminClass;
+    }
 
+    public void setSecuritySyncAdminClass(String securitySyncAdminClass) {
+        this.securitySyncAdminClass = securitySyncAdminClass;
+    }
 
+    public String getSecuritySyncName() {
+        return securitySyncName;
+    }
+
+    public void setSecuritySyncName(String securitySyncName) {
+        this.securitySyncName = securitySyncName;
+    }
+
+    public String getSecuritySyncDescription() {
+        return securitySyncDescription;
+    }
+
+    public void setSecuritySyncDescription(String securitySyncDescription) {
+        this.securitySyncDescription = securitySyncDescription;
+    }
+
+    public String getSecuritySyncWiki() {
+        return securitySyncWiki;
+    }
+
+    public void setSecuritySyncWiki(String securitySyncWiki) {
+        this.securitySyncWiki = securitySyncWiki;
+    }
+
+    public String getSecurityServerURL() {
+        return securityServerURL;
+    }
+
+    public void setSecurityServerURL(String securityServerURL) {
+        this.securityServerURL = securityServerURL;
+    }
+
+    public String getSecurityServerType() {
+        return securityServerType;
+    }
+
+    public void setSecurityServerType(String securityServerType) {
+        this.securityServerType = securityServerType;
+    }
+
+    public String getSecurityServerAuthorization() {
+        return securityServerAuthorization;
+    }
+
+    public void setSecurityServerAuthorization(String securityServerAuthorization) {
+        this.securityServerAuthorization = securityServerAuthorization;
+    }
+
+    public String getTagServiceName() {
+        return tagServiceName;
+    }
+
+    public void setTagServiceName(String tagServiceName) {
+        this.tagServiceName = tagServiceName;
+    }
+
+    public String getSecuritySyncInTopicName() {
+        return securitySyncInTopicName;
+    }
+
+    public void setSecuritySyncInTopicName(String securitySyncInTopicName) {
+        this.securitySyncInTopicName = securitySyncInTopicName;
+    }
+
+    public Connection getSecuritySyncInTopic() {
+        return securitySyncInTopic;
+    }
+
+    public void setSecuritySyncInTopic(Connection securitySyncInTopic) {
+        this.securitySyncInTopic = securitySyncInTopic;
+    }
+
+    public String getSecuritySyncOutTopicName() {
+        return securitySyncOutTopicName;
+    }
+
+    public void setSecuritySyncOutTopicName(String securitySyncOutTopicName) {
+        this.securitySyncOutTopicName = securitySyncOutTopicName;
+    }
+
+    public Connection getSecuritySyncOutTopic() {
+        return securitySyncOutTopic;
+    }
+
+    public void setSecuritySyncOutTopic(Connection securitySyncOutTopic) {
+        this.securitySyncOutTopic = securitySyncOutTopic;
+    }
+
+    @Override
+    public String toString() {
+        return "SecuritySyncConfig{" +
+                "securitySyncId=" + securitySyncId +
+                ", securitySyncAdminClass='" + securitySyncAdminClass + '\'' +
+                ", securitySyncName='" + securitySyncName + '\'' +
+                ", securitySyncDescription='" + securitySyncDescription + '\'' +
+                ", securitySyncWiki ='" + securitySyncWiki + '\'' +
+                ", securityServerURL='" + securityServerURL + '\'' +
+                ", securityServerType='" + securityServerType + '\'' +
+                ", securityServerAuthorization='" + securityServerAuthorization + '\'' +
+                ", tagServiceName='" + tagServiceName + '\'' +
+                ", securitySyncInTopicName='" + securitySyncInTopicName + '\'' +
+                ", securitySyncInTopic=" + securitySyncInTopic +
+                ", securitySyncOutTopicName='" + securitySyncOutTopicName + '\'' +
+                ", securitySyncOutTopic=" + securitySyncOutTopic +
+                '}';
+    }
 
     /**
      * Validate that an object is equal depending on their stored values.
@@ -58,14 +200,38 @@ public class SecuritySyncConfig extends AdminServicesConfigHeader
      * @param objectToCompare object
      * @return boolean result
      */
-
-
+    @Override
+    public boolean equals(Object objectToCompare) {
+        if (this == objectToCompare) {
+            return true;
+        }
+        if (objectToCompare == null || getClass() != objectToCompare.getClass()) {
+            return false;
+        }
+        SecuritySyncConfig that = (SecuritySyncConfig) objectToCompare;
+        return getSecuritySyncId() == that.getSecuritySyncId() &&
+                Objects.equals(getSecuritySyncAdminClass(), that.getSecuritySyncAdminClass()) &&
+                Objects.equals(getSecuritySyncName(), that.getSecuritySyncName()) &&
+                Objects.equals(getSecuritySyncDescription(), that.getSecuritySyncDescription()) &&
+                Objects.equals(getSecuritySyncWiki(), that.getSecuritySyncWiki()) &&
+                Objects.equals(getSecurityServerURL(), that.getSecurityServerURL()) &&
+                getSecurityServerAuthorization() == that.getSecurityServerAuthorization() &&
+                Objects.equals(getTagServiceName(), that.getTagServiceName()) &&
+                Objects.equals(getSecuritySyncInTopic(), that.getSecuritySyncInTopic()) &&
+                Objects.equals(getSecuritySyncOutTopic(), that.getSecuritySyncOutTopic()) &&
+                Objects.equals(getSecuritySyncInTopicName(), that.getSecuritySyncInTopicName()) &&
+                Objects.equals(getSecuritySyncOutTopicName(), that.getSecuritySyncOutTopicName());
+    }
 
     /**
      * Return a hash code based on the values of this object.
      *
      * @return in hash code
      */
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSecuritySyncId(), getSecuritySyncAdminClass(), getSecuritySyncName(),
+                getSecuritySyncDescription(), getSecuritySyncWiki(), getSecurityServerURL(), getSecurityServerAuthorization(), getTagServiceName(),
+                getSecuritySyncInTopic(), getSecuritySyncOutTopic(), getSecuritySyncInTopicName(), getSecuritySyncOutTopicName());
+    }
 }
---
+++ open-metadata-implementation/governance-servers/security-sync-services/security-sync-services-api/src/main/java/org/odpi/openmetadata/securitysyncservices/model/Ranger/RangerResource.java
@@ -0,0 +1,86 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+package org.odpi.openmetadata.securitysyncservices.model.Ranger;
+
+import java.util.List;
+import java.util.Map;
+
+public class RangerResource {
+
+    private String op = "add_or_update";
+    private String serviceName;
+    private Long tagVersion;
+    private String tagUpdateTime;
+    private Map<Long, RangerTag> tags;
+    private List<RangerServiceResource> serviceResources;
+    private Map<Long, List<Long>> resourceToTagIds;
+
+    public String getOp() {
+        return op;
+    }
+
+    public void setOp(String op) {
+        this.op = op;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public Long getTagVersion() {
+        return tagVersion;
+    }
+
+    public void setTagVersion(Long tagVersion) {
+        this.tagVersion = tagVersion;
+    }
+
+    public String getTagUpdateTime() {
+        return tagUpdateTime;
+    }
+
+    public void setTagUpdateTime(String tagUpdateTime) {
+        this.tagUpdateTime = tagUpdateTime;
+    }
+
+    public Map<Long, RangerTag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Map<Long, RangerTag> tags) {
+        this.tags = tags;
+    }
+
+    public List<RangerServiceResource> getServiceResources() {
+        return serviceResources;
+    }
+
+    public void setServiceResources(List<RangerServiceResource> serviceResources) {
+        this.serviceResources = serviceResources;
+    }
+
+    public Map<Long, List<Long>> getResourceToTagIds() {
+        return resourceToTagIds;
+    }
+
+    public void setResourceToTagIds(Map<Long, List<Long>> resourceToTagIds) {
+        this.resourceToTagIds = resourceToTagIds;
+    }
+
+    @Override
+    public String toString() {
+        return "RangerResource{" +
+                "op='" + op + '\'' +
+                ", serviceName='" + serviceName + '\'' +
+                ", tagVersion=" + tagVersion +
+                ", tagUpdateTime='" + tagUpdateTime + '\'' +
+                ", tags=" + tags +
+                ", serviceResources=" + serviceResources +
+                ", resourceToTagIds=" + resourceToTagIds +
+                '}';
+    }
+}
