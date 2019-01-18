d64f2340b0b5cd67be5dfb5a7bb1ca53d1c36af9
Christopher Grote <chris@thegrotes.net>
2019-01-18 18:25:45 +0000

    Updates v11.5.0.2 for #577
    Signed-off-by: Christopher Grote <chris@thegrotes.net>

--- open-metadata-implementation/adapters/open-connectors/repository-services-connectors/open-metadata-collection-store-connectors/igc-rest-client-library/src/main/java/org/odpi/openmetadata/adapters/repositoryservices/igc/clientlibrary/model/generated/v11502/Linktype.java
+++ open-metadata-implementation/adapters/open-connectors/repository-services-connectors/open-metadata-collection-store-connectors/igc-rest-client-library/src/main/java/org/odpi/openmetadata/adapters/repositoryservices/igc/clientlibrary/model/generated/v11502/Linktype.java
@@ -46,12 +46,17 @@ public class Linktype extends Reference {
         "name",
         "description"
     );
+    private static final List<String> STRING_PROPERTIES = Arrays.asList(
+        "name",
+        "description"
+    );
     private static final List<String> PAGED_RELATIONAL_PROPERTIES = new ArrayList<>();
     private static final List<String> ALL_PROPERTIES = Arrays.asList(
         "name",
         "description"
     );
     public static List<String> getNonRelationshipProperties() { return NON_RELATIONAL_PROPERTIES; }
+    public static List<String> getStringProperties() { return STRING_PROPERTIES; }
     public static List<String> getPagedRelationshipProperties() { return PAGED_RELATIONAL_PROPERTIES; }
     public static List<String> getAllProperties() { return ALL_PROPERTIES; }
     public static Boolean isLinktype(Object obj) { return (obj.getClass() == Linktype.class); }
