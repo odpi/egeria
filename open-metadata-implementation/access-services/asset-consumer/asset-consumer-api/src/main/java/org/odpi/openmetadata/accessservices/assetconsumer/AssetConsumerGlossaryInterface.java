b9d6dfd6549c8b4e82a2c8a77f7dd0849543701f
Mandy Chessell <mandy_chessell@uk.ibm.com>
2019-01-20 17:31:56 +0000

    Remove serverName from Asset Consumer OMAS Glossary Interface.
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/access-services/asset-consumer/asset-consumer-api/src/main/java/org/odpi/openmetadata/accessservices/assetconsumer/AssetConsumerGlossaryInterface.java
+++ open-metadata-implementation/access-services/asset-consumer/asset-consumer-api/src/main/java/org/odpi/openmetadata/accessservices/assetconsumer/AssetConsumerGlossaryInterface.java
@@ -20,7 +20,6 @@ public interface AssetConsumerGlossaryInterface
      * Return the full definition (meaning) of a term using the unique identifier of the glossary term
      * that contains the definition.
      *
-     * @param serverName name of the server instances for this request
      * @param userId userId of the user making the request.
      * @param guid unique identifier of the glossary term.
      *
@@ -29,8 +28,7 @@ public interface AssetConsumerGlossaryInterface
      * @throws PropertyServerException there is a problem retrieving information from the property server(s).
      * @throws UserNotAuthorizedException the requesting user is not authorized to issue this request.
      */
-    GlossaryTerm getMeaning(String serverName,
-                            String userId,
+    GlossaryTerm getMeaning(String userId,
                             String guid) throws InvalidParameterException,
                                                 PropertyServerException,
                                                 UserNotAuthorizedException;
@@ -39,7 +37,6 @@ public interface AssetConsumerGlossaryInterface
     /**
      * Return the full definition (meaning) of the terms matching the supplied name.
      *
-     * @param serverName name of the server instances for this request
      * @param userId the name of the calling user.
      * @param term name of term.  This may include wild card characters.
      * @param startFrom  index of the list ot start from (0 for start)
@@ -50,8 +47,7 @@ public interface AssetConsumerGlossaryInterface
      * @throws PropertyServerException there is a problem retrieving information from the property server(s).
      * @throws UserNotAuthorizedException the requesting user is not authorized to issue this request.
      */
-    List<GlossaryTerm> getMeaningByName(String serverName,
-                                        String userId,
+    List<GlossaryTerm> getMeaningByName(String userId,
                                         String term,
                                         int    startFrom,
                                         int    pageSize) throws InvalidParameterException,
