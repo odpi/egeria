f3bf54c820a11dc21f645bb037b4316d367d48b4
Nigel Jones <nigel.l.jones+git@gmail.com>
2019-01-22 23:17:57 +0000

    #380 initialize Egeria during vdc startup
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- open-metadata-resources/open-metadata-deployment/docker/egeria-init/dist/configure-egeria.sh
+++ open-metadata-resources/open-metadata-deployment/docker/egeria-init/dist/configure-egeria.sh
@@ -3,17 +3,17 @@
 #  SPDX-License-Identifier: Apache-2.0
 #  Copyright Contributors to the ODPi Egeria project. 
 
-# Script to configure Atlas for use with egeria - see below for variables
+# Script to configure Egeria - see below for variables
 # used. Primarily intended as part of automated kubernetes/docker setup,
 # but can also be used interactively if required.
 
-printf "\nConfiguring Atlas for Egeria VDC\n\n" 
+printf "\nConfiguring Egeria for VDC\n\n" 
 
 fail=0
 
-if [ "null" = "$ATLAS_ENDPOINT" ] || [ -z $ATLAS_ENDPOINT ]
+if [ "null" = "$EGERIA_ENDPOINT" ] || [ -z $EGERIA_ENDPOINT ]
 then
-	echo "ATLAS_ENDPOINT must be supplied. This is URL of the Atlas server that will be configured. An example is 'http://atlas.acme.org:9000'"
+	echo "EGERIA_ENDPOINT must be supplied. This is URL of the Egeria server that will be configured. An example is 'http://egeria.acme.org:9000'"
 	fail=1
 fi
 
@@ -25,13 +25,13 @@ fi
 
 if [ "null" = "$EGERIA_USER" ] || [ -z $EGERIA_USER ]
 then
-	echo "EGERIA_USER must be supplied. This is user name for the egeria api in Atlas. An example is 'admin'"
+	echo "EGERIA_USER must be supplied. This is user name for the egeria api. An example is 'admin'"
 	fail=1
 fi
 
 if [ "null" = "$EGERIA_SERVER" ] || [ -z $EGERIA_SERVER ]
 then
-	echo "EGERIA_SERVER must be supplied. This is the server name for the egeria api in Atlas. An example is 'myserver'"
+	echo "EGERIA_SERVER must be supplied. This is the server name for the egeria api. An example is 'myserver'"
 	fail=1
 fi
 
@@ -47,13 +47,13 @@ then
 fi
 
 
-echo "Atlas server URL  : ${ATLAS_ENDPOINT}"
+echo "Egeria server URL : ${EGERIA_ENDPOINT}"
 echo "Kafka broker      : ${KAFKA_ENDPOINT}"
 echo "Egeria user name  : ${EGERIA_USER}"
 echo "Egeria server name: ${EGERIA_SERVER}"
 echo "Egeria Cohort     : ${EGERIA_COHORT}"
 
-echo "Checking ATLAS is up"
+echo "Checking EGERIA is up"
 
 loop=100
 retrytimeout=10
@@ -64,7 +64,7 @@ delay=30
 # So we will do a post, expecting a 401 back when things are ok (full authentication is required)
 while [ $loop -gt 0 ]
 do
-    http --check-status --ignore-stdin --timeout=${retrytimeout} HEAD ${ATLAS_ENDPOINT} &> /dev/null
+    http --check-status --ignore-stdin --timeout=${retrytimeout} HEAD ${EGERIA_ENDPOINT} &> /dev/null
     rc=$?
     if [ $rc -eq 4 ]
     then
@@ -80,36 +80,36 @@ done
 
 if [ $loop -le 0 ]
 then
-	echo "Atlas server was unavailable. Abandoning configuration. Check endpoint URL"
+	echo "Egeria server was unavailable. Abandoning configuration. Check endpoint URL"
 	exit 1
 fi
 
 
-# Issue requests against atlas
+# Issue requests against Egeria
 http --verbose --ignore-stdin \
 	--check-status \
 	--auth admin:admin \
-	POST ${ATLAS_ENDPOINT}/egeria/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/event-bus   \
+	POST ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/event-bus   \
 	producer:='{"bootstrap.servers":"'${KAFKA_ENDPOINT}'"}' \
         consumer:='{"bootstrap.servers":"'${KAFKA_ENDPOINT}'"}' 
 rc=$?
 if [ $rc -ne 0 ]
 then
-	printf "\nAtlas setup Failed configuring event bus\n" 
+	printf "\nEgeria setup Failed configuring event bus\n" 
 	exit 1
 fi
 
 
-# Repository type configuration
+# Enable Access Services
 
 http --verbose --ignore-stdin \
 	--check-status \
         --auth admin:admin \
-        POST ${ATLAS_ENDPOINT}/egeria/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/atlas-repository   
+        POST ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/access-services?serviceMode=ENABLED
 rc=$?
 if [ $rc -ne 0 ]
 then
-        printf "\nAtlas setup Failed configuring repository type\n"
+        printf "\nEgeria setup Failed enabling access services\n"
         exit 1
 fi
 
@@ -117,11 +117,11 @@ fi
 http --verbose --ignore-stdin \
 	--check-status \
         --auth admin:admin \
-        POST ${ATLAS_ENDPOINT}/egeria/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/server-url-root?url=${ATLAS_ENDPOINT}/egeria
+        POST ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/server-url-root?url=${EGERIA_ENDPOINT}/egeria
 rc=$?
 if [ $rc -ne 0 ]
 then
-        printf "\nAtlas setup Failed configuring root\n"
+        printf "\nEgeria setup Failed configuring root\n"
         exit 1
 fi
 
@@ -129,11 +129,11 @@ fi
 http --verbose --ignore-stdin \
 	--check-status \
         --auth admin:admin \
-        POST ${ATLAS_ENDPOINT}/egeria/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/cohorts/${EGERIA_COHORT}
+        POST ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/cohorts/${EGERIA_COHORT}
 rc=$?
 if [ $rc -ne 0 ]
 then
-        printf "\nAtlas setup Failed configuring cohort\n"
+        printf "\nEgeria setup Failed configuring cohort\n"
         exit 1
 fi
 
@@ -143,14 +143,14 @@ http --verbose --ignore-stdin \
 	--check-status \
         --auth admin:admin \
 	--timeout 900 \
-        POST ${ATLAS_ENDPOINT}/egeria/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/instance
+        POST ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/instance
 rc=$?
 if [ $rc -ne 0 ]
 then
-        printf "\nAtlas setup Failed starting instance\n"
+        printf "\nEgeria setup Failed starting instance\n"
         exit 1
 fi
-printf "\nAtlas setup completed\n"
+printf "\nEgeria setup completed\n"
 
 echo "WARNING - Now going into sleep loop - to maintain health successful pod status"
 while [ true ]
