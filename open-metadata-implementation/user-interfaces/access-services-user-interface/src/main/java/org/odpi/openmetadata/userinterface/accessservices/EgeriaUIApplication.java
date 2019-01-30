4895d1d7a94fca0d30bd80aa459601a6e0f11d0f
Bogdan Sava <bogdan-mihail.sava@ing.com>
2019-01-30 13:43:57 +0100

    added strict.ssl property to be able to connect to a server with self signed ssl certificate
    Signed-off-by: Bogdan Sava <bogdan-mihail.sava@ing.com>

--- open-metadata-implementation/user-interfaces/access-services-user-interface/src/main/java/org/odpi/openmetadata/userinterface/accessservices/EgeriaUIApplication.java
+++ open-metadata-implementation/user-interfaces/access-services-user-interface/src/main/java/org/odpi/openmetadata/userinterface/accessservices/EgeriaUIApplication.java
@@ -3,8 +3,10 @@
 package org.odpi.openmetadata.userinterface.accessservices;
 
 import org.odpi.openmetadata.accessservice.assetcatalog.client.AssetCatalog;
+import org.odpi.openmetadata.http.HttpHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,9 +20,24 @@ public class EgeriaUIApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(EgeriaUIApplication.class);
 
+    @Value("${strict.ssl}")
+    Boolean strictSSL;
+
     public static void main(String[] args) {
         SpringApplication.run(EgeriaUIApplication.class, args);
     }
+
+    @Bean
+    public InitializingBean getInitialize()
+    {
+        return () -> {
+            if (!strictSSL)
+            {
+                HttpHelper.noStrictSSL();
+            }
+        };
+    }
+
 
     @Bean
     public AssetCatalog getAssetCatalog(@Value("${omas.server.url}") String serverUrl,
