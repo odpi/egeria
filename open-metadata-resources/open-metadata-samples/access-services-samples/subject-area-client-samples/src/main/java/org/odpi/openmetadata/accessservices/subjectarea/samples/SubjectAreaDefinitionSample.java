a015a20e61873781dac8a1844ef64f7c59c58208
Mandy Chessell <mandy_chessell@uk.ibm.com>
2019-02-05 17:55:53 +0000

    Allow override of server name and userId.
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-resources/open-metadata-samples/access-services-samples/subject-area-client-samples/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/samples/SubjectAreaDefinitionSample.java
+++ open-metadata-resources/open-metadata-samples/access-services-samples/subject-area-client-samples/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/samples/SubjectAreaDefinitionSample.java
@@ -8,12 +8,10 @@ import org.odpi.openmetadata.accessservices.subjectarea.SubjectAreaCategory;
 import org.odpi.openmetadata.accessservices.subjectarea.SubjectAreaGlossary;
 import org.odpi.openmetadata.accessservices.subjectarea.client.SubjectAreaImpl;
 import org.odpi.openmetadata.accessservices.subjectarea.ffdc.exceptions.*;
-import org.odpi.openmetadata.accessservices.subjectarea.properties.objects.category.Category;
 import org.odpi.openmetadata.accessservices.subjectarea.properties.objects.category.SubjectAreaDefinition;
 import org.odpi.openmetadata.accessservices.subjectarea.properties.objects.glossary.Glossary;
 import org.odpi.openmetadata.accessservices.subjectarea.properties.objects.nodesummary.CategorySummary;
 import org.odpi.openmetadata.accessservices.subjectarea.properties.objects.nodesummary.GlossarySummary;
-import org.odpi.openmetadata.accessservices.subjectarea.properties.objects.project.Project;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -50,29 +48,30 @@ public class SubjectAreaDefinitionSample
     private static final String   DISTRIBUTION = "Distribution";
     private static final String   INVOICE = "Invoice";
 
-    private static final String GLOSSARY_NAME = "Coco Pharmaceuticals Subject Area Definitions";
+    private static final String GLOSSARY_NAME        = "Coco Pharmaceuticals Subject Area Definitions";
     private static final String GLOSSARY_DESCRIPTION = "Coco Pharmaceuticals Core Subject Areas for Personalized Medicine.";
-    private static final String SERVER_NAME  = "CocoServer1";
-    /*
-     * Erin Overview userid
-     */
-    private static final String USERID = "Erin";
-    public static final String DEFAULT_URL = "http://localhost:8080";
-    private String serverURLRoot;
-    private String clientUserId;
-    private SubjectArea subjectArea = null;
-    private SubjectAreaGlossary subjectAreaGlossary =null;
-    private SubjectAreaCategory subjectAreaCategory =null;
+    private static final String DEFAULT_SERVER_NAME  = "cocoMDS1";
+    private static final String DEFAULT_USERID       = "Erin";
+    private static final String DEFAULT_URL          = "http://localhost:8080";
+
+    private String              serverURLRoot;
+    private String              clientUserId;
+    private String              serverName;
+    private SubjectAreaGlossary subjectAreaGlossary  =null;
+    private SubjectAreaCategory subjectAreaCategory  =null;
+
 
     /**
      * Constructor
      *
-     * @param serverURLRoot server to connect to.
+     * @param serverURLRoot server platform to connect to.
+     * @param serverName server to connect to.
      * @param clientUserId user id to use to access metadata.
      */
-    private SubjectAreaDefinitionSample(String serverURLRoot, String clientUserId)
+    private SubjectAreaDefinitionSample(String serverURLRoot, String serverName, String clientUserId)
     {
         this.serverURLRoot = serverURLRoot;
+        this.serverName = serverName;
         this.clientUserId = clientUserId;
     }
 
@@ -82,22 +81,23 @@ public class SubjectAreaDefinitionSample
      */
     private void run() throws SubjectAreaCheckedExceptionBase
     {
-        SubjectArea subjectArea = new SubjectAreaImpl(SERVER_NAME,this.serverURLRoot);
+        SubjectArea subjectArea = new SubjectAreaImpl(this.serverName, this.serverURLRoot);
         subjectAreaGlossary = subjectArea.getSubjectAreaGlossary();
         subjectAreaCategory = subjectArea.getSubjectAreaCategory();
 
         System.out.println("----------------------------");
         System.out.println("Creating the Coco Pharmaceutical Glossary for Subject Area Definitions : ");
 
-        Glossary glossary = createGlossary(USERID,GLOSSARY_NAME,GLOSSARY_DESCRIPTION);
+        Glossary glossary = createGlossary(clientUserId, GLOSSARY_NAME, GLOSSARY_DESCRIPTION);
+
         /*
          * create Organisation subject areas
          */
-
         String glossaryGuid =  glossary.getSystemAttributes().getGUID();
         SubjectAreaDefinition organisation = createTopSubjectAreaDefinition(ORGANIZATION,glossaryGuid);
         SubjectAreaDefinition hospital= createChildSubjectAreaDefinition(HOSPITAL,organisation,glossaryGuid);
         SubjectAreaDefinition supplier= createChildSubjectAreaDefinition(SUPPLIER,organisation,glossaryGuid);
+
         /*
          * create Person subject areas
          */
@@ -106,6 +106,7 @@ public class SubjectAreaDefinitionSample
         SubjectAreaDefinition clinician= createChildSubjectAreaDefinition(CLINICIAN,person,glossaryGuid);
         SubjectAreaDefinition employee= createChildSubjectAreaDefinition(EMPLOYEE,person,glossaryGuid);
         SubjectAreaDefinition collaborator= createChildSubjectAreaDefinition(COLLABORATOR,person,glossaryGuid);
+
         /*
          * create Clinical subject areas
          */
@@ -114,6 +115,7 @@ public class SubjectAreaDefinitionSample
         SubjectAreaDefinition measurement= createChildSubjectAreaDefinition(MEASUREMENT,clinical,glossaryGuid);
         SubjectAreaDefinition prescription= createChildSubjectAreaDefinition(PRESCRIPTION,clinical,glossaryGuid);
         SubjectAreaDefinition outcome= createChildSubjectAreaDefinition(OUTCOME,clinical,glossaryGuid);
+
         /*
          * create Treatment subject areas
          */
@@ -121,6 +123,7 @@ public class SubjectAreaDefinitionSample
         SubjectAreaDefinition product= createChildSubjectAreaDefinition(PRODUCT,treatment,glossaryGuid);
         SubjectAreaDefinition order= createChildSubjectAreaDefinition(ORDER,treatment,glossaryGuid);
         SubjectAreaDefinition recipe= createChildSubjectAreaDefinition(RECIPE,treatment,glossaryGuid);
+
         /*
          * create Service Quality subject areas
          */
@@ -130,6 +133,7 @@ public class SubjectAreaDefinitionSample
         SubjectAreaDefinition distribution= createChildSubjectAreaDefinition(DISTRIBUTION,serviceQuality,glossaryGuid);
         SubjectAreaDefinition invoice= createChildSubjectAreaDefinition(INVOICE,serviceQuality,glossaryGuid);
     }
+
 
     /**
      * Create a top level subject area definition - this means a Subject Area Definition that does not have a superCategory.
@@ -147,8 +151,9 @@ public class SubjectAreaDefinitionSample
         GlossarySummary glossarySummary = new GlossarySummary();
         glossarySummary.setGuid(glossaryGuid);
         subjectAreaDefinition.setGlossary(glossarySummary);
-        return subjectAreaCategory.createSubjectAreaDefinition(SERVER_NAME,USERID,subjectAreaDefinition);
+        return subjectAreaCategory.createSubjectAreaDefinition(serverName, clientUserId, subjectAreaDefinition);
     }
+
 
     /**
      * Create a child Subject Area Definition - this means a Subject Area Definition that has a superCategory.
@@ -168,7 +173,9 @@ public class SubjectAreaDefinitionSample
         CategorySummary parentCategorysummary = new CategorySummary();
         parentCategorysummary.setGuid(parent.getSystemAttributes().getGUID());
         subjectAreaDefinition.setParentCategory(parentCategorysummary);
-        SubjectAreaDefinition newSubjectAreaDefinition = subjectAreaCategory.createSubjectAreaDefinition(SERVER_NAME,USERID, subjectAreaDefinition);
+        SubjectAreaDefinition newSubjectAreaDefinition = subjectAreaCategory.createSubjectAreaDefinition(serverName,
+                                                                                                         clientUserId,
+                                                                                                         subjectAreaDefinition);
         if (newSubjectAreaDefinition != null)
         {
             System.out.println("Created Subject Area Definition " + newSubjectAreaDefinition.getName() + " with guid " + newSubjectAreaDefinition.getSystemAttributes().getGUID() + ", parent SubjectArea Definition is " + parent.getName());
@@ -176,40 +183,49 @@ public class SubjectAreaDefinitionSample
         return newSubjectAreaDefinition;
     }
 
+
     /**
      * Create a Glossary
-     * @param userid userid
+     *
+     * @param userId user id
      * @param glossaryName name of the glossary to create
      * @param glossaryDescription description of the Glossary
      * @return created glossary
      * @throws SubjectAreaCheckedExceptionBase error
      */
-    private Glossary createGlossary(String userid,String glossaryName,String glossaryDescription) throws SubjectAreaCheckedExceptionBase
+    private Glossary createGlossary(String userId, String glossaryName, String glossaryDescription) throws SubjectAreaCheckedExceptionBase
     {
         Glossary glossary = new Glossary();
         glossary.setName(glossaryName);
         glossary.setDescription(glossaryDescription);
-        return subjectAreaGlossary.createGlossary(SERVER_NAME,USERID,glossary);
+        return subjectAreaGlossary.createGlossary(serverName, userId, glossary);
     }
+
 
     /**
      * The main program takes the URL root for the server.
      *
-     * @param args URL root for the server
+     * @param args URL root for the OMAG server platform; OMAG server name; userId
      * @throws IOException IO Exception
      */
     public static void main(String[] args) throws IOException
     {
         String  serverURLRoot = SubjectAreaDefinitionSample.getUrl(args);
+        String  serverName = SubjectAreaDefinitionSample.getServerName(args);
+        String  clientUserId = SubjectAreaDefinitionSample.getUserId(args);
+
         org.apache.log4j.BasicConfigurator.configure(new NullAppender());
         System.out.println("===============================");
         System.out.println("Subject Area Definition Sample ");
         System.out.println("===============================");
-        System.out.println("Running against server: " + serverURLRoot);
-        System.out.println("Using userId: " + USERID);
+        System.out.println("Running against OMAG server platform: " + serverURLRoot);
+        System.out.println("OMAG server platform: " + serverName);
+        System.out.println("Using userId: " + clientUserId);
         try
         {
-            SubjectAreaDefinitionSample  sample = new SubjectAreaDefinitionSample(serverURLRoot, USERID);
+            SubjectAreaDefinitionSample  sample = new SubjectAreaDefinitionSample(serverURLRoot,
+                                                                                  serverName,
+                                                                                  clientUserId);
 
             sample.run();
         }
@@ -219,8 +235,10 @@ public class SubjectAreaDefinitionSample
             System.exit(-1);
         }
     }
+
+
     /**
-     * This method gets the url that the tests will use to issue calls to the server.
+     * This method gets the url that the sample will use to issue calls to the server.
      * <p>
      * If arguments are supplied then the first parameter is used as a url.
      * <p>
@@ -228,7 +246,7 @@ public class SubjectAreaDefinitionSample
      *
      * @param args arguments supplied
      * @return the url to use on the calls to the server
-     * @throws IOException IO exception occured while getting input from the user.
+     * @throws IOException IO exception occurred while getting input from the user.
      */
     public static String getUrl(String args[]) throws IOException
     {
@@ -239,7 +257,7 @@ public class SubjectAreaDefinitionSample
         } else
         {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Enter a URL. Press enter to get the default (" + DEFAULT_URL + ".)):");
+            System.out.print("Enter the OMAG Server platform URL. Press enter to get the default (" + DEFAULT_URL + ")):");
             url = br.readLine();
             if (url.equals(""))
             {
@@ -248,6 +266,70 @@ public class SubjectAreaDefinitionSample
 
         }
         return url;
+    }
+
+
+    /**
+     * This method gets the server name that the sample will use to issue calls to the server.
+     * <p>
+     * If arguments are supplied then the second parameter is used as a server name.
+     * <p>
+     * If no name is supplied then prompt the user to enter a valid server name, enter means to use the default server name.
+     *
+     * @param args arguments supplied
+     * @return the url to use on the calls to the server
+     * @throws IOException IO exception occurred while getting input from the user.
+     */
+    public static String getServerName(String args[]) throws IOException
+    {
+        String name = null;
+        if (args.length > 1)
+        {
+            name = args[1];
+        } else
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter a OMAG Server Name. Press enter to get the default (" + DEFAULT_SERVER_NAME + ")):");
+            name = br.readLine();
+            if (name.equals(""))
+            {
+                name = DEFAULT_SERVER_NAME;
+            }
+
+        }
+        return name;
+    }
+
+
+    /**
+     * This method gets the userId that the sample will use to issue calls to the server.
+     * <p>
+     * If arguments are supplied then the third parameter is used as a userId.
+     * <p>
+     * If no userId is supplied then prompt the user to enter a valid userId, enter means to use the default userId.
+     *
+     * @param args arguments supplied
+     * @return the url to use on the calls to the server
+     * @throws IOException IO exception occurred while getting input from the user.
+     */
+    public static String getUserId(String args[]) throws IOException
+    {
+        String userId = null;
+        if (args.length > 1)
+        {
+            userId = args[1];
+        } else
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter a userId. Press enter to get the default (" + DEFAULT_USERID + ")):");
+            userId = br.readLine();
+            if (userId.equals(""))
+            {
+                userId = DEFAULT_USERID;
+            }
+
+        }
+        return userId;
     }
 
 }
