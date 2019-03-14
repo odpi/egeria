84c13b8b730afe6d28c289e955a5485db89ccd42
Cong Chen <cong.chen@ing.com>
2019-03-14 12:32:23 +0100

    add dummy endpoint
    Signed-off-by: Cong Chen <cong.chen@ing.com>

--- open-metadata-implementation/access-services/data-platform/data-platform-spring/src/main/java/org/odpi/openmetadata/accessservices/dataplatform/server/spring/DataPlatformOMASResource.java
+++ open-metadata-implementation/access-services/data-platform/data-platform-spring/src/main/java/org/odpi/openmetadata/accessservices/dataplatform/server/spring/DataPlatformOMASResource.java
@@ -2,9 +2,9 @@
 /* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.accessservices.dataplatform.server.spring;
 
+import org.odpi.openmetadata.accessservices.dataplatform.responses.DataPlatformOMASAPIResponse;
 import org.odpi.openmetadata.accessservices.dataplatform.server.DataPlatformRestServices;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/servers/{serverName}/open-metadata/access-services/data-platform/users/{userId}/")
@@ -12,6 +12,15 @@ public class DataPlatformOMASResource {
 
     private final DataPlatformRestServices restAPI = new DataPlatformRestServices();
 
-    public DataPlatformOMASResource() {
+
+    /**
+     * @param serverName  unique identifier for requested server.
+     * @param userId      the unique identifier for the user
+     * @return
+     */
+    @PostMapping(path = "/test")
+    public String submitReport(@PathVariable("serverName") String serverName,
+                                                    @PathVariable("userId") String userId) {
+        return userId;
     }
 }
