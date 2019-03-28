a49e46dd13a1c4ce14ebfe225f6c9f7c09264982
Bogdan Sava <bogdan-mihail.sava@ing.com>
2019-03-28 11:44:01 +0200

    UI-changes  - refactoring asset search to return asset term without context on first call
    Signed-off-by: Bogdan Sava <bogdan-mihail.sava@ing.com>

--- open-metadata-implementation/user-interfaces/access-services-user-interface/src/main/java/org/odpi/openmetadata/userinterface/accessservices/api/AssetSearchController.java
+++ open-metadata-implementation/user-interfaces/access-services-user-interface/src/main/java/org/odpi/openmetadata/userinterface/accessservices/api/AssetSearchController.java
@@ -4,7 +4,7 @@ package org.odpi.openmetadata.userinterface.accessservices.api;
 
 import org.odpi.openmetadata.accessservices.assetcatalog.exception.InvalidParameterException;
 import org.odpi.openmetadata.accessservices.assetcatalog.exception.PropertyServerException;
-import org.odpi.openmetadata.accessservices.assetcatalog.model.AssetDescription;
+import org.odpi.openmetadata.accessservices.assetcatalog.model.Term;
 import org.odpi.openmetadata.userinterface.accessservices.service.AssetCatalogOMASService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +21,13 @@ public class AssetSearchController {
     @Autowired
     AssetCatalogOMASService omasService;
 
+    /**
+     *
+     * @param q the query parameter with the search phrase
+     * @return list of assets
+     */
     @RequestMapping(method = RequestMethod.GET)
-    public List<AssetDescription>  searchAssets(@RequestParam("q") String q) {
+    public List<Term>  searchAssets(@RequestParam("q") String q) {
         try {
             return omasService.searchAssets(q);
         } catch (PropertyServerException | InvalidParameterException e) {
