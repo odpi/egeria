52f3ad1c712fef5fdec40b83d2bd1d80d49bbe40
Cong Chen <cong.chen@ing.com>
2019-04-01 11:35:07 +0200

    Add Information view listener to listen information view events.
    Signed-off-by: Cong Chen <cong.chen@ing.com>

--- open-metadata-implementation/access-services/data-platform/data-platform-server/src/main/java/org/odpi/openmetadata/accessservices/dataplatform/listeners/InformationViewEventListener.java
+++ open-metadata-implementation/access-services/data-platform/data-platform-server/src/main/java/org/odpi/openmetadata/accessservices/dataplatform/listeners/InformationViewEventListener.java
@@ -1,18 +1,16 @@
-/* SPDX-License-Identifier: Apache-2.0 */
-/* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.accessservices.dataplatform.listeners;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.odpi.openmetadata.accessservices.dataplatform.events.DataPlatformEvent;
+import org.odpi.openmetadata.accessservices.dataplatform.contentmanager.OMEntityDao;
+import org.odpi.openmetadata.accessservices.dataplatform.eventprocessor.EventPublisher;
+import org.odpi.openmetadata.accessservices.dataplatform.events.InformationViewEvent;
+import org.odpi.openmetadata.accessservices.dataplatform.ffdc.DataPlatformErrorCode;
 import org.odpi.openmetadata.accessservices.dataplatform.utils.Constants;
 import org.odpi.openmetadata.accessservices.dataplatform.views.InformationViewAssetHandler;
 import org.odpi.openmetadata.accessservices.dataplatform.views.ViewHandler;
 import org.odpi.openmetadata.accessservices.dataplatform.views.beans.InformationViewAsset;
 import org.odpi.openmetadata.accessservices.dataplatform.views.beans.View;
-import org.odpi.openmetadata.accessservices.dataplatform.contentmanager.OMEntityDao;
-import org.odpi.openmetadata.accessservices.dataplatform.eventprocessor.EventPublisher;
-import org.odpi.openmetadata.accessservices.dataplatform.events.InformationViewEvent;
-import org.odpi.openmetadata.accessservices.dataplatform.ffdc.DataPlatformErrorCode;
 import org.odpi.openmetadata.repositoryservices.auditlog.OMRSAuditLog;
 import org.odpi.openmetadata.repositoryservices.auditlog.OMRSAuditLogRecordSeverity;
 import org.odpi.openmetadata.repositoryservices.connectors.openmetadatatopic.OpenMetadataTopicListener;
@@ -25,15 +23,17 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class DataPlatformInTopicListener implements OpenMetadataTopicListener {
-    private static final Logger log = LoggerFactory.getLogger(DataPlatformInTopicListener.class);
+public class InformationViewEventListener implements OpenMetadataTopicListener {
+
+
+    private static final Logger log = LoggerFactory.getLogger(InformationViewEventListener.class);
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private final OMEntityDao omEntityDao;
     private final OMRSAuditLog auditLog;
     private EventPublisher eventPublisher;
     private OMRSRepositoryHelper helper;
 
-    public DataPlatformInTopicListener(OMEntityDao omEntityDao, OMRSAuditLog auditLog, EventPublisher eventPublisher, OMRSRepositoryHelper helper) {
+    public InformationViewEventListener(OMEntityDao omEntityDao, OMRSAuditLog auditLog, EventPublisher eventPublisher, OMRSRepositoryHelper helper) {
         this.omEntityDao = omEntityDao;
         this.auditLog = auditLog;
         this.eventPublisher = eventPublisher;
@@ -46,10 +46,10 @@ public class DataPlatformInTopicListener implements OpenMetadataTopicListener {
      */
     @Override
     public void processEvent(String eventAsString) {
-        DataPlatformEvent event = null;
 
+        InformationViewEvent event = null;
         try {
-            event = OBJECT_MAPPER.readValue(eventAsString, DataPlatformEvent.class);
+            event = OBJECT_MAPPER.readValue(eventAsString, InformationViewEvent.class);
         } catch (Exception e) {
             DataPlatformErrorCode auditCode = DataPlatformErrorCode.PARSE_EVENT;
 
@@ -67,7 +67,7 @@ public class DataPlatformInTopicListener implements OpenMetadataTopicListener {
             try {
                 log.info("Started processing information view event in DataPlatform OMAS");
 
-               /* InformationViewAssetHandler informationViewAssetHandler = new InformationViewAssetHandler(event, omEntityDao);
+                InformationViewAssetHandler informationViewAssetHandler = new InformationViewAssetHandler(event, omEntityDao);
                 ViewHandler viewsBuilder = new ViewHandler(event, omEntityDao, helper);
                 ExecutorService executor = Executors.newCachedThreadPool();
                 Future<InformationViewAsset> informationViewFuture = executor.submit(informationViewAssetHandler);
@@ -77,8 +77,7 @@ public class DataPlatformInTopicListener implements OpenMetadataTopicListener {
                 View view = viewCreationFuture.get();
                 executor.shutdown();
 
-               // DataPlatformEventHandlder
-                if(view.getViewEntity() != null) {
+                if (view.getViewEntity() != null) {
                     omEntityDao.addRelationship(Constants.ATTRIBUTE_FOR_SCHEMA,
                             informationViewAsset.getRelationalDbSchemaType().getGUID(),
                             view.getViewEntity().getGUID(),
@@ -86,9 +85,9 @@ public class DataPlatformInTopicListener implements OpenMetadataTopicListener {
                             new InstanceProperties());
                     event.getTableSource().setTableGuid(view.getViewEntity().getGUID());
                 }
-                eventPublisher.sendEvent(event);*/
+                eventPublisher.sendEvent(event);
             } catch (Exception e) {
-                log.error("Exception processing event from Data Platform in topic", e);
+                log.error("Exception processing event from in topic", e);
                 DataPlatformErrorCode auditCode = DataPlatformErrorCode.PROCESS_EVENT_EXCEPTION;
 
                 auditLog.logException("processEvent",
@@ -102,6 +101,5 @@ public class DataPlatformInTopicListener implements OpenMetadataTopicListener {
             }
         }
     }
-
 
 }
