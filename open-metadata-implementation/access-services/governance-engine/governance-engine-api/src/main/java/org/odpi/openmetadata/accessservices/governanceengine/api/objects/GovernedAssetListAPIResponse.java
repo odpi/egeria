63d9de6cfe88e37194bd2b1a0f817e92870079a8
Daniela Otelea <daniela-valentina.otelea@ing.com>
2019-04-05 16:48:37 +0300

    Fortify Fix - Small changes in order to fix security issues
    Signed-off-by: Daniela Otelea <daniela-valentina.otelea@ing.com>

--- open-metadata-implementation/access-services/governance-engine/governance-engine-api/src/main/java/org/odpi/openmetadata/accessservices/governanceengine/api/objects/GovernedAssetListAPIResponse.java
+++ open-metadata-implementation/access-services/governance-engine/governance-engine-api/src/main/java/org/odpi/openmetadata/accessservices/governanceengine/api/objects/GovernedAssetListAPIResponse.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.io.Serializable;
 import java.util.List;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
@@ -18,8 +19,9 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_
 @JsonAutoDetect(getterVisibility = PUBLIC_ONLY, setterVisibility = PUBLIC_ONLY, fieldVisibility = NONE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class GovernedAssetListAPIResponse extends GovernanceEngineOMASAPIResponse {
+public class GovernedAssetListAPIResponse extends GovernanceEngineOMASAPIResponse implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private List<GovernedAsset> assetList;
 
     /**
