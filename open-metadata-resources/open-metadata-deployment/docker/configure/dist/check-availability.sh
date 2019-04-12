b4bda4d6f31dae0c0c691a4b88b65549d49d28bd
Nigel Jones <nigel.l.jones+git@gmail.com>
2019-04-12 12:36:19 +0100

    #910 Use correct namespace for k8s queries
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- open-metadata-resources/open-metadata-deployment/docker/configure/dist/check-availability.sh
+++ open-metadata-resources/open-metadata-deployment/docker/configure/dist/check-availability.sh
@@ -3,10 +3,12 @@
 # Copyright Contributors to the Egeria project.
 
 token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
-available_pods=$(curl -s --cacert $CACERT --header "Authorization: Bearer $token" https://kubernetes.default.svc/api/v1/namespaces/$NAMESPACE/endpoints/$SERVICE | jq -r '.subsets[].addresses | length')
+namespace="$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)"
+
+available_pods=$(curl -s --cacert $CACERT --header "Authorization: Bearer $token" https://kubernetes.default.svc/api/v1/namespaces/$namespace/endpoints/$SERVICE | jq -r '.subsets[].addresses | length')
 
 until [ $available_pods -ge 1 ]; do
     echo waiting for $SERVICE
     sleep 2
-    available_pods=$(curl -s --cacert $CACERT --header "Authorization: Bearer $token" https://kubernetes.default.svc/api/v1/namespaces/$NAMESPACE/endpoints/$SERVICE | jq -r '.subsets[].addresses | length')
+    available_pods=$(curl -s --cacert $CACERT --header "Authorization: Bearer $token" https://kubernetes.default.svc/api/v1/namespaces/$namespace/endpoints/$SERVICE | jq -r '.subsets[].addresses | length')
 done
