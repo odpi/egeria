dff6713ed03dc9ec45259681d0ece89d9e7275b6
ruxandraRosu <ruxandra-gabriela.rosu@ing.com>
2019-04-23 16:21:17 +0300

    Remove unused import
    Signed-off-by: ruxandraRosu <ruxandra-gabriela.rosu@ing.com>

--- open-metadata-implementation/access-services/information-view/information-view-server/src/main/java/org/odpi/openmetadata/accessservices/informationview/contentmanager/OMEntityDao.java
+++ open-metadata-implementation/access-services/information-view/information-view-server/src/main/java/org/odpi/openmetadata/accessservices/informationview/contentmanager/OMEntityDao.java
@@ -38,7 +38,6 @@ import org.odpi.openmetadata.repositoryservices.ffdc.exception.UserNotAuthorized
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
