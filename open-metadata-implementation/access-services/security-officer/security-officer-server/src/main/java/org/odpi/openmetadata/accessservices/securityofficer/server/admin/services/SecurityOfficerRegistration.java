baeeaaacccdc2295fce638e585466ee8efcd6954
Daniela Otelea <daniela-valentina.otelea@ing.com>
2019-04-30 13:36:21 +0300

    Security Officer OMAS - fix the licence
    Signed-off-by: Daniela Otelea <daniela-valentina.otelea@ing.com>

--- open-metadata-implementation/access-services/security-officer/security-officer-server/src/main/java/org/odpi/openmetadata/accessservices/securityofficer/server/admin/services/SecurityOfficerRegistration.java
+++ open-metadata-implementation/access-services/security-officer/security-officer-server/src/main/java/org/odpi/openmetadata/accessservices/securityofficer/server/admin/services/SecurityOfficerRegistration.java
@@ -2,8 +2,6 @@
  *  SPDX-License-Identifier: Apache-2.0
  *  Copyright Contributors to the ODPi Egeria project.
  */
-
-/* SPDX-License-Identifier: Apache-2.0 */
 package org.odpi.openmetadata.accessservices.securityofficer.server.admin.services;
 
 import org.odpi.openmetadata.accessservices.securityofficer.server.admin.admin.SecurityOfficerAdmin;
--- open-metadata-implementation/access-services/security-officer/security-officer-server/src/main/java/org/odpi/openmetadata/accessservices/securityofficer/server/admin/services/SecurityOfficerService.java
+++ open-metadata-implementation/access-services/security-officer/security-officer-server/src/main/java/org/odpi/openmetadata/accessservices/securityofficer/server/admin/services/SecurityOfficerService.java
@@ -2,7 +2,6 @@
  *  SPDX-License-Identifier: Apache-2.0
  *  Copyright Contributors to the ODPi Egeria project.
  */
-
 package org.odpi.openmetadata.accessservices.securityofficer.server.admin.services;
 
 /**
