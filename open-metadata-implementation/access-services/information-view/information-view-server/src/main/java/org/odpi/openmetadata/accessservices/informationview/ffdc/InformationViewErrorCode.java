de7c85a5a314bfea594e0dadbffe66ff6c3f4240
ruxandraRosu <ruxandra-gabriela.rosu@ing.com>
2019-05-02 17:31:57 +0300

    Use saveReferenceEntityCopy to save a report insteadof addEntity in order to have the regsitration guid set as metadataCollectionId
    Signed-off-by: ruxandraRosu <ruxandra-gabriela.rosu@ing.com>

--- open-metadata-implementation/access-services/information-view/information-view-server/src/main/java/org/odpi/openmetadata/accessservices/informationview/ffdc/InformationViewErrorCode.java
+++ open-metadata-implementation/access-services/information-view/information-view-server/src/main/java/org/odpi/openmetadata/accessservices/informationview/ffdc/InformationViewErrorCode.java
@@ -104,7 +104,11 @@ public enum InformationViewErrorCode {
     SOURCE_NOT_FOUND_EXCEPTION(500, "OMAS-INFORMATION-VIEW-023",
                             "No entity was found based on source: {0}",
                             "The system is unable to process the request.",
-                            "Check the error message for more details.");
+                            "Check the error message for more details."),
+    ILLEGAL_UPDATE_EXCEPTION(403, "OMAS-INFORMATION-VIEW-024",
+                "Illegal update for entity: {0}",
+                "The system will not process the request.",
+                  "This entity is owned by another tool.");
 
     private static final Logger log = LoggerFactory.getLogger(InformationViewErrorCode.class);
     private String errorMessageId;
