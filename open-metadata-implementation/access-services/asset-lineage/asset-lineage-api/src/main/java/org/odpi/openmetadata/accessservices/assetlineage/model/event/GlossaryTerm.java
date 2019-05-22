eeb0bf9109e3b822d40139de08a8a62e2578d282
DimitriosMaimaris <dimi.maim@gmail.com>
2019-05-22 16:08:58 +0200

    Update GlossaryHandler for type field
    Signed-off-by: DimitriosMaimaris <dimi.maim@gmail.com>

--- open-metadata-implementation/access-services/asset-lineage/asset-lineage-api/src/main/java/org/odpi/openmetadata/accessservices/assetlineage/model/event/GlossaryTerm.java
+++ open-metadata-implementation/access-services/asset-lineage/asset-lineage-api/src/main/java/org/odpi/openmetadata/accessservices/assetlineage/model/event/GlossaryTerm.java
@@ -4,6 +4,7 @@ package org.odpi.openmetadata.accessservices.assetlineage.model.event;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import org.odpi.openmetadata.accessservices.assetlineage.model.assetContext.Element;
 
 import java.util.List;
 
@@ -13,10 +14,8 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_
 @JsonAutoDetect(getterVisibility = PUBLIC_ONLY, setterVisibility = PUBLIC_ONLY, fieldVisibility = NONE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class GlossaryTerm {
+public class GlossaryTerm extends Element {
 
-    private String guid;
-    private String qualifiedName;
     private String displayName;
     //TODO Clean this up
     private List<org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.Classification> classifications;
@@ -31,23 +30,6 @@ public class GlossaryTerm {
 
     public List<org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.Classification> getClassifications() {
         return classifications;
-    }
-
-
-    public String getGuid() {
-        return guid;
-    }
-
-    public void setGuid(String guid) {
-        this.guid = guid;
-    }
-
-    public String getQualifiedName() {
-        return qualifiedName;
-    }
-
-    public void setQualifiedName(String qualifiedName) {
-        this.qualifiedName = qualifiedName;
     }
 
     public void setClassifications(List<org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.Classification> classifications) {
