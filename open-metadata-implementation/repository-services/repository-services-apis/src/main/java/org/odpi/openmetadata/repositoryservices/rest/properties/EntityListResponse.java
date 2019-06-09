8d4e0d872a1cd6e4d5e8f7800f5f7e7a7f814f85
Mandy Chessell <mandy_chessell@uk.ibm.com>
2019-06-09 21:18:43 +0100

    Move packages
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

---
+++ open-metadata-implementation/repository-services/repository-services-apis/src/main/java/org/odpi/openmetadata/repositoryservices/rest/properties/EntityListResponse.java
@@ -0,0 +1,154 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+package org.odpi.openmetadata.repositoryservices.rest.properties;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.EntityDetail;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY;
+
+/**
+ * EntityListResponse support an OMRS REST API response that returns a list of EntityDetail objects.
+ */
+@JsonAutoDetect(getterVisibility=PUBLIC_ONLY, setterVisibility=PUBLIC_ONLY, fieldVisibility=NONE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class EntityListResponse extends OMRSAPIPagedResponse
+{
+    private List<EntityDetail> entities = null;
+
+
+    /**
+     * Default constructor
+     */
+    public EntityListResponse()
+    {
+        super();
+    }
+
+
+    /**
+     * Copy/clone constructor
+     *
+     * @param template object to copy
+     */
+    public EntityListResponse(EntityListResponse template)
+    {
+        super(template);
+
+        if (template != null)
+        {
+            entities = template.getEntities();
+        }
+    }
+
+
+    /**
+     * Return the list of entities.
+     *
+     * @return entity list
+     */
+    public List<EntityDetail> getEntities()
+    {
+        if (entities == null)
+        {
+            return null;
+        }
+        else if (entities.isEmpty())
+        {
+            return null;
+        }
+        else
+        {
+            List<EntityDetail>  clonedEntities = new ArrayList<>();
+
+            for (EntityDetail  attributeTypeDef : entities)
+            {
+                clonedEntities.add(new EntityDetail(attributeTypeDef));
+            }
+
+            return clonedEntities;
+        }
+    }
+
+
+    /**
+     * Set up the list of entities.
+     *
+     * @param entities entity list
+     */
+    public void setEntities(List<EntityDetail> entities)
+    {
+        this.entities = entities;
+    }
+
+
+    /**
+     * Standard toString method.
+     *
+     * @return print out of variables in a JSON-style
+     */
+    @Override
+    public String toString()
+    {
+        return "EntityListResponse{" +
+                "entities=" + entities +
+                ", nextPageURL='" + nextPageURL + '\'' +
+                ", offset=" + offset +
+                ", pageSize=" + pageSize +
+                ", relatedHTTPCode=" + relatedHTTPCode +
+                ", exceptionClassName='" + exceptionClassName + '\'' +
+                ", exceptionErrorMessage='" + exceptionErrorMessage + '\'' +
+                ", exceptionSystemAction='" + exceptionSystemAction + '\'' +
+                ", exceptionUserAction='" + exceptionUserAction + '\'' +
+                ", exceptionProperties=" + exceptionProperties +
+                '}';
+    }
+
+
+    /**
+     * Compare the values of the supplied object with those stored in the current object.
+     *
+     * @param objectToCompare supplied object
+     * @return boolean result of comparison
+     */
+    @Override
+    public boolean equals(Object objectToCompare)
+    {
+        if (this == objectToCompare)
+        {
+            return true;
+        }
+        if (!(objectToCompare instanceof EntityListResponse))
+        {
+            return false;
+        }
+        if (!super.equals(objectToCompare))
+        {
+            return false;
+        }
+        EntityListResponse
+                that = (EntityListResponse) objectToCompare;
+        return Objects.equals(getEntities(), that.getEntities());
+    }
+
+
+    /**
+     * Create a hash code for this element type.
+     *
+     * @return int hash code
+     */
+    @Override
+    public int hashCode()
+    {
+
+        return Objects.hash(super.hashCode(), getEntities());
+    }
+}
---
+++ open-metadata-implementation/repository-services/repository-services-apis/src/main/java/org/odpi/openmetadata/repositoryservices/rest/properties/EntitySummaryResponse.java
@@ -0,0 +1,137 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+package org.odpi.openmetadata.repositoryservices.rest.properties;
+
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.EntitySummary;
+
+import java.util.Objects;
+
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY;
+
+/**
+ * EntitySummaryResponse describes the response structure for an OMRS REST API that returns an EntitySummary object.
+ */
+@JsonAutoDetect(getterVisibility=PUBLIC_ONLY, setterVisibility=PUBLIC_ONLY, fieldVisibility=NONE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class EntitySummaryResponse extends OMRSAPIResponse
+{
+    private EntitySummary entity = null;
+
+    /**
+     * Default constructor
+     */
+    public EntitySummaryResponse()
+    {
+        super();
+    }
+
+
+    /**
+     * Copy/clone constructor
+     *
+     * @param template object to copy
+     */
+    public EntitySummaryResponse(EntitySummaryResponse template)
+    {
+        super(template);
+
+        if (template != null)
+        {
+            entity = template.getEntity();
+        }
+    }
+
+
+    /**
+     * Return the resulting entity object.
+     *
+     * @return entity object
+     */
+    public EntitySummary getEntity()
+    {
+        if (entity == null)
+        {
+            return null;
+        }
+        else
+        {
+            return new EntitySummary(entity);
+        }
+    }
+
+
+    /**
+     * Set up the resulting entity object.
+     *
+     * @param entity entity object
+     */
+    public void setEntity(EntitySummary entity)
+    {
+        this.entity = entity;
+    }
+
+
+    /**
+     * Standard toString method.
+     *
+     * @return print out of variables in a JSON-style
+     */
+    @Override
+    public String toString()
+    {
+        return "EntitySummaryResponse{" +
+                "entity=" + entity +
+                ", relatedHTTPCode=" + relatedHTTPCode +
+                ", exceptionClassName='" + exceptionClassName + '\'' +
+                ", exceptionErrorMessage='" + exceptionErrorMessage + '\'' +
+                ", exceptionSystemAction='" + exceptionSystemAction + '\'' +
+                ", exceptionUserAction='" + exceptionUserAction + '\'' +
+                ", exceptionProperties=" + exceptionProperties +
+                '}';
+    }
+
+
+    /**
+     * Compare the values of the supplied object with those stored in the current object.
+     *
+     * @param objectToCompare supplied object
+     * @return boolean result of comparison
+     */
+    @Override
+    public boolean equals(Object objectToCompare)
+    {
+        if (this == objectToCompare)
+        {
+            return true;
+        }
+        if (!(objectToCompare instanceof EntitySummaryResponse))
+        {
+            return false;
+        }
+        if (!super.equals(objectToCompare))
+        {
+            return false;
+        }
+        EntitySummaryResponse
+                that = (EntitySummaryResponse) objectToCompare;
+        return Objects.equals(getEntity(), that.getEntity());
+    }
+
+
+    /**
+     * Create a hash code for this element type.
+     *
+     * @return int hash code
+     */
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(super.hashCode(), getEntity());
+    }
+}
