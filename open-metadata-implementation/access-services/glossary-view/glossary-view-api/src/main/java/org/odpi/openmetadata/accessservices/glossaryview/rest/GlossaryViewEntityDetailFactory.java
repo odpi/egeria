68f1fb4e4172fb3109ead0789803b176480e290b
Liviu Popa <liviu-constantin.popa@ing.com>
2019-06-12 16:50:41 +0300

    Glossary View OMAS
    Signed-off-by: Liviu Popa <liviu-constantin.popa@ing.com>

--- open-metadata-implementation/access-services/glossary-view/glossary-view-api/src/main/java/org/odpi/openmetadata/accessservices/glossaryview/rest/GlossaryViewEntityDetailFactory.java
+++ open-metadata-implementation/access-services/glossary-view/glossary-view-api/src/main/java/org/odpi/openmetadata/accessservices/glossaryview/rest/GlossaryViewEntityDetailFactory.java
@@ -2,44 +2,32 @@
 /* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.accessservices.glossaryview.rest;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class GlossaryViewEntityDetailFactory {
 
+    private final static String DEFAULT = "Default";
     private final static String GLOSSARY = "Glossary";
     private final static String CATEGORY = "GlossaryCategory";
     private final static String TERM = "GlossaryTerm";
     private final static String EXTERNAL_GLOSSARY_LINK = "ExternalGlossaryLink";
 
+    private final static Map<String, Supplier<GlossaryViewEntityDetail>> workers = new HashMap<>();
+    static{
+        workers.put(DEFAULT, () -> new GlossaryViewEntityDetail());
+        workers.put(GLOSSARY, () -> new Glossary());
+        workers.put(CATEGORY, () -> new Category());
+        workers.put(TERM, () -> new Term());
+        workers.put(EXTERNAL_GLOSSARY_LINK, () -> new GlossaryViewEntityDetail());
+    }
+
     public static GlossaryViewEntityDetail build(String entityType){
-        GlossaryViewEntityDetail prototype = null;
-        if(entityType.equalsIgnoreCase(GLOSSARY)){
-            prototype = buildDefaultGlossary();
+        if(!workers.containsKey(entityType)){
+            return workers.get(DEFAULT).get();
         }
-        if(entityType.equalsIgnoreCase(CATEGORY)){
-            prototype = buildDefaultCategory();
-        }
-        if(entityType.equalsIgnoreCase(TERM)){
-            prototype = buildDefaultTerm();
-        }
-        if(entityType.equalsIgnoreCase(EXTERNAL_GLOSSARY_LINK)){
-            prototype = buildDefaultGlossaryViewEntityDetail();
-        }
-        return prototype;
-    }
-
-    private static GlossaryViewEntityDetail buildDefaultGlossaryViewEntityDetail(){
-        return new GlossaryViewEntityDetail();
-    }
-
-    private static Glossary buildDefaultGlossary(){
-        return new Glossary();
-    }
-
-    private static Category buildDefaultCategory(){
-        return new Category();
-    }
-
-    private static Term buildDefaultTerm(){
-        return new Term();
+        return workers.get(entityType).get();
     }
 
 }
