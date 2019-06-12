aa0420d44e207bb6e00049d9c9b2d7388e31ff3a
Liviu Popa <liviu-constantin.popa@ing.com>
2019-06-12 16:25:11 +0300

    Glossary View OMAS
    Signed-off-by: Liviu Popa <liviu-constantin.popa@ing.com>

--- open-metadata-implementation/access-services/glossary-view/glossary-view-server/src/main/java/org/odpi/openmetadata/accessservices/glossaryview/server/service/GlossaryViewInstanceHandler.java
+++ open-metadata-implementation/access-services/glossary-view/glossary-view-server/src/main/java/org/odpi/openmetadata/accessservices/glossaryview/server/service/GlossaryViewInstanceHandler.java
@@ -1,3 +1,6 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+
 package org.odpi.openmetadata.accessservices.glossaryview.server.service;
 
 import org.odpi.openmetadata.adminservices.configuration.registration.AccessServiceDescription;
