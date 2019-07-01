ca00e0f98f2608d092bfe8cb5e9c23295502a0d8
Daniela Otelea <daniela-valentina.otelea@ing.com>
2019-07-01 14:16:12 +0300

    Security Officer OMAS: publish events on Security Officer Output topic for security tags changes
    Signed-off-by: Daniela Otelea <daniela-valentina.otelea@ing.com>

--- open-metadata-implementation/access-services/security-officer/security-officer-api/src/main/java/org/odpi/openmetadata/accessservices/securityofficer/api/events/SecurityOfficerEventType.java
+++ open-metadata-implementation/access-services/security-officer/security-officer-api/src/main/java/org/odpi/openmetadata/accessservices/securityofficer/api/events/SecurityOfficerEventType.java
@@ -9,7 +9,8 @@ import java.io.Serializable;
 
 public enum SecurityOfficerEventType implements Serializable {
 
-    NEW_SECURITY_ASSIGNMENT(0, "NewSecurityAssignment", "A security tag assigment should be processes."),
+    NEW_SECURITY_ASSIGNMENT(0, "NewSecurityAssignment", "A Security Tags assigment should be processes."),
+    UPDATED_SECURITY_TAGS(1, "UpdatedSecurityTags", "A Security Tags classification has been changed at schema element level"),
     UNKNOWN_SECURITY_OFFICER_EVENT(4, "UnknownSecurityOfficerEvent", "A Security Officer event that is not recognized by the local handlers.");
 
     private static final long serialVersionUID = 1L;
