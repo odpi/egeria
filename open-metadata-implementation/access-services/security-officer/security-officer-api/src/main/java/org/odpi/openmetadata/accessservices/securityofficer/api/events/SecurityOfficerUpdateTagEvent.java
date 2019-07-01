8b2dd8fcf8513d50d11040f4f15943a7d618d88f
Daniela Otelea <daniela-valentina.otelea@ing.com>
2019-07-01 15:26:39 +0300

    Updated the event description + added the missing licence - file header
    Signed-off-by: Daniela Otelea <daniela-valentina.otelea@ing.com>

--- open-metadata-implementation/access-services/security-officer/security-officer-api/src/main/java/org/odpi/openmetadata/accessservices/securityofficer/api/events/SecurityOfficerUpdateTagEvent.java
+++ open-metadata-implementation/access-services/security-officer/security-officer-api/src/main/java/org/odpi/openmetadata/accessservices/securityofficer/api/events/SecurityOfficerUpdateTagEvent.java
@@ -1,3 +1,7 @@
+/*
+ *  SPDX-License-Identifier: Apache-2.0
+ *  Copyright Contributors to the ODPi Egeria project.
+ */
 package org.odpi.openmetadata.accessservices.securityofficer.api.events;
 
 import org.odpi.openmetadata.accessservices.securityofficer.api.model.SecurityClassification;
