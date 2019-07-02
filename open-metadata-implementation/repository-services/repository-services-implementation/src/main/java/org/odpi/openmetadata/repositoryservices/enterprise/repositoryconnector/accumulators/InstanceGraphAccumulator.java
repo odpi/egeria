5856a5156ffecd75253af71aa1fb95db8b693b4c
Mandy Chessell <mandy_chessell@uk.ibm.com>
2019-07-02 22:45:38 +0100

    Manage federation when a cohort member is down
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/repository-services/repository-services-implementation/src/main/java/org/odpi/openmetadata/repositoryservices/enterprise/repositoryconnector/accumulators/InstanceGraphAccumulator.java
+++ open-metadata-implementation/repository-services/repository-services-implementation/src/main/java/org/odpi/openmetadata/repositoryservices/enterprise/repositoryconnector/accumulators/InstanceGraphAccumulator.java
@@ -2,11 +2,29 @@
 /* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.repositoryservices.enterprise.repositoryconnector.accumulators;
 
+import org.odpi.openmetadata.repositoryservices.auditlog.OMRSAuditLog;
+import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryValidator;
+import org.odpi.openmetadata.repositoryservices.enterprise.repositoryconnector.EnterpriseOMRSRepositoryConnector;
+
 /**
  * InstanceGraphAccumulator provides the accumulator for
  * an instance graph - this is a collection of related
  * entities and relationships.
  */
-public class InstanceGraphAccumulator extends ExceptionAccumulatorBase
+public class InstanceGraphAccumulator extends QueryAccumulatorBase
 {
+    /**
+     * Construct a query accumulator.  This base class manages the common variables needed to
+     * control the execution of requests across all members of the cohort(s).
+     *
+     * @param localMetadataCollectionId collection Id of local repository - null means no local repository
+     * @param auditLog audit log provides destination for log messages
+     * @param repositoryValidator validator provides common validation routines
+     */
+    InstanceGraphAccumulator(String                            localMetadataCollectionId,
+                             OMRSAuditLog                      auditLog,
+                             OMRSRepositoryValidator           repositoryValidator)
+    {
+        super(localMetadataCollectionId, auditLog, repositoryValidator);
+    }
 }
--- open-metadata-implementation/repository-services/repository-services-implementation/src/main/java/org/odpi/openmetadata/repositoryservices/enterprise/repositoryconnector/control/FederationControlBase.java
+++ open-metadata-implementation/repository-services/repository-services-implementation/src/main/java/org/odpi/openmetadata/repositoryservices/enterprise/repositoryconnector/control/FederationControlBase.java
@@ -3,8 +3,10 @@
 package org.odpi.openmetadata.repositoryservices.enterprise.repositoryconnector.control;
 
 
+import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.OMRSMetadataCollection;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryConnector;
 import org.odpi.openmetadata.repositoryservices.enterprise.repositoryconnector.executors.RepositoryExecutor;
+import org.odpi.openmetadata.repositoryservices.ffdc.OMRSErrorCode;
 import org.odpi.openmetadata.repositoryservices.ffdc.exception.RepositoryErrorException;
 
 import java.util.List;
@@ -53,4 +55,47 @@ public abstract class FederationControlBase implements FederationControl
      * This is probably a logic error rather than an outage
      */
     public abstract void executeCommand(RepositoryExecutor   executor) throws RepositoryErrorException;
+
+
+    /**
+     * Verify that a cohort member's metadata collection is not null.
+     *
+     * @param cohortMetadataCollection metadata collection
+     * @param methodName name of method
+     * @return metadata collection id
+     * @throws RepositoryErrorException null metadata collection or null metadata collection id
+     */
+    String validateMetadataCollection(OMRSMetadataCollection cohortMetadataCollection,
+                                      String                  methodName) throws RepositoryErrorException
+    {
+        /*
+         * The cohort metadata collection should not be null.  It is in a real mess if this fails.
+         */
+        if (cohortMetadataCollection == null)
+        {
+            /*
+             * A problem in the set up of the metadata collection list.  Repository connectors implemented
+             * with no metadata collection are tested for in the OMRSEnterpriseConnectorManager so something
+             * else has gone wrong.
+             */
+            OMRSErrorCode errorCode    = OMRSErrorCode.NULL_ENTERPRISE_METADATA_COLLECTION;
+            String        errorMessage = errorCode.getErrorMessageId() + errorCode.getFormattedErrorMessage();
+
+            throw new RepositoryErrorException(errorCode.getHTTPErrorCode(),
+                                               this.getClass().getName(),
+                                               methodName,
+                                               errorMessage,
+                                               errorCode.getSystemAction(),
+                                               errorCode.getUserAction());
+        }
+
+        try
+        {
+            return cohortMetadataCollection.getMetadataCollectionId(userId);
+        }
+        catch (RepositoryErrorException error)
+        {
+            return null;
+        }
+    }
 }
