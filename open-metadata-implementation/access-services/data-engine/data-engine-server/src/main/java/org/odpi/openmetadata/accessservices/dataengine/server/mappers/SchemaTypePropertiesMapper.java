d6748503df4e94e95f0861379ae9a01e66aff7ab
Raluca Popa <raluca.popa2@ing.com>
2019-07-03 12:08:11 +0300

    Update port types and relationships to use hex guids
    Signed-off-by: Raluca Popa <raluca.popa2@ing.com>

--- open-metadata-implementation/access-services/data-engine/data-engine-server/src/main/java/org/odpi/openmetadata/accessservices/dataengine/server/mappers/SchemaTypePropertiesMapper.java
+++ open-metadata-implementation/access-services/data-engine/data-engine-server/src/main/java/org/odpi/openmetadata/accessservices/dataengine/server/mappers/SchemaTypePropertiesMapper.java
@@ -6,7 +6,7 @@ package org.odpi.openmetadata.accessservices.dataengine.server.mappers;
  * SchemaTypePropertiesMapper provides property name mapping for SchemaTypes and their relationships.
  */
 public class SchemaTypePropertiesMapper {
-    public static final String LINEAGE_MAPPINGS_TYPE_GUID = "GGQmRWnY-aKYh-Yzsa-UoAi-1DsFzGTLqjJk";
+    public static final String LINEAGE_MAPPINGS_TYPE_GUID = "a5991bB2-660D-A3a1-2955-fAcDA2d5F4Ff";
 
     public static final String QUALIFIED_NAME_PROPERTY_NAME = "qualifiedName";
     public static final String DISPLAY_NAME_PROPERTY_NAME = "displayName";
