d0a4eff9ea084b2c4f0928f0d21c379fe27aaee3
Cong Chen <cong.chen@ing.com>
2019-07-10 16:46:53 +0200

    Refactoring and delete some unused classes.
    Signed-off-by: Cong Chen <cong.chen@ing.com>

--- open-metadata-implementation/access-services/data-platform/data-platform-server/src/main/java/org/odpi/openmetadata/accessservices/dataplatform/auditlog/DataPlatformAuditCode.java
+++ open-metadata-implementation/access-services/data-platform/data-platform-server/src/main/java/org/odpi/openmetadata/accessservices/dataplatform/auditlog/DataPlatformAuditCode.java
@@ -23,13 +23,13 @@ public enum DataPlatformAuditCode {
             "The Data Platform OMAS is registering to receive events from the connected open metadata repositories.",
             "No action is required.  This is part of the normal operation of the server."),
 
-    SERVICE_REGISTERED_WITH_AL_IN_TOPIC("OMAS-DATA-PLATFORM-0003",
+    SERVICE_REGISTERED_WITH_DP_IN_TOPIC("OMAS-DATA-PLATFORM-0003",
             OMRSAuditLogRecordSeverity.INFO,
             "The Data Platform Open Metadata Access Service (OMAS) is registering a listener with the Data Platform In topic {0}",
             "The Data Platform OMAS is registering to receive incoming events from external tools and applications.",
             "No action is required.  This is part of the normal operation of the server."),
 
-    SERVICE_REGISTERED_WITH_AL_OUT_TOPIC("OMAS-DATA-PLATFORM-0004",
+    SERVICE_REGISTERED_WITH_DP_OUT_TOPIC("OMAS-DATA-PLATFORM-0004",
             OMRSAuditLogRecordSeverity.INFO,
             "The Data Platform Open Metadata Access Service (OMAS) is registering a publisher with the Data Platform Out topic {0}",
             "The Data Platform OMAS is registering to publish events to Data Platform Out topic.",
--- open-metadata-implementation/access-services/data-platform/data-platform-server/src/main/java/org/odpi/openmetadata/accessservices/dataplatform/beans/View.java
+++ open-metadata-implementation/access-services/data-platform/data-platform-server/src/main/java/org/odpi/openmetadata/accessservices/dataplatform/beans/View.java
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright Contributors to the ODPi Egeria project. */
-package org.odpi.openmetadata.accessservices.dataplatform.asset;
+package org.odpi.openmetadata.accessservices.dataplatform.beans;
 
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.EntityDetail;
 
