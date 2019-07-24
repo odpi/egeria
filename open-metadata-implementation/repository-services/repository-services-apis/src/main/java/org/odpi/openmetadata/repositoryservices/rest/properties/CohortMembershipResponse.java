4e918dd28e72f77f26178ba89d43e3c55714c63c
Mandy Chessell <mandy_chessell@uk.ibm.com>
2019-07-24 10:46:59 +0100

    Remove dashes from OMRS Topic Name (#599)
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

---
+++ open-metadata-implementation/repository-services/repository-services-apis/src/main/java/org/odpi/openmetadata/repositoryservices/rest/properties/CohortMembershipResponse.java
@@ -0,0 +1,133 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+package org.odpi.openmetadata.repositoryservices.rest.properties;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.odpi.openmetadata.repositoryservices.connectors.stores.cohortregistrystore.properties.MemberRegistration;
+import org.odpi.openmetadata.repositoryservices.properties.CohortDescription;
+
+import java.util.List;
+import java.util.Objects;
+
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY;
+
+/**
+ * CohortListResponse support an OMRS REST API response that returns a list of cohort description objects.
+ */
+@JsonAutoDetect(getterVisibility=PUBLIC_ONLY, setterVisibility=PUBLIC_ONLY, fieldVisibility=NONE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class CohortMembershipResponse extends OMRSAPIResponse
+{
+    private MemberRegistration cohortMember = null;
+
+
+    /**
+     * Default constructor
+     */
+    public CohortMembershipResponse()
+    {
+        super();
+    }
+
+
+    /**
+     * Copy/clone constructor
+     *
+     * @param template object to copy
+     */
+    public CohortMembershipResponse(CohortMembershipResponse template)
+    {
+        super(template);
+
+        if (template != null)
+        {
+            cohortMember = template.getCohortMember();
+        }
+    }
+
+
+    /**
+     * Return the list of cohortMember.
+     *
+     * @return entity list
+     */
+    public MemberRegistration getCohortMember()
+    {
+        return cohortMember;
+    }
+
+
+    /**
+     * Set up the list of cohortMember.
+     *
+     * @param cohortMember entity list
+     */
+    public void setCohortMember(MemberRegistration cohortMember)
+    {
+        this.cohortMember = cohortMember;
+    }
+
+
+    /**
+     * Standard toString method.
+     *
+     * @return print out of variables in a JSON-style
+     */
+    @Override
+    public String toString()
+    {
+        return "CohortMembershipResponse{" +
+                "cohorts=" + cohortMember +
+                ", relatedHTTPCode=" + relatedHTTPCode +
+                ", exceptionClassName='" + exceptionClassName + '\'' +
+                ", exceptionErrorMessage='" + exceptionErrorMessage + '\'' +
+                ", exceptionSystemAction='" + exceptionSystemAction + '\'' +
+                ", exceptionUserAction='" + exceptionUserAction + '\'' +
+                ", exceptionProperties=" + exceptionProperties +
+                '}';
+    }
+
+
+    /**
+     * Compare the values of the supplied object with those stored in the current object.
+     *
+     * @param objectToCompare supplied object
+     * @return boolean result of comparison
+     */
+    @Override
+    public boolean equals(Object objectToCompare)
+    {
+        if (this == objectToCompare)
+        {
+            return true;
+        }
+        if (!(objectToCompare instanceof CohortMembershipResponse))
+        {
+            return false;
+        }
+        if (!super.equals(objectToCompare))
+        {
+            return false;
+        }
+        CohortMembershipResponse
+                that = (CohortMembershipResponse) objectToCompare;
+        return Objects.equals(getCohortMember(), that.getCohortMember());
+    }
+
+
+    /**
+     * Create a hash code for this element type.
+     *
+     * @return int hash code
+     */
+    @Override
+    public int hashCode()
+    {
+
+        return Objects.hash(super.hashCode(), getCohortMember());
+    }
+}
