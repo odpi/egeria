f02327d907202d23c8c5879a0d3f6450d1e3007c
Ilya Ksenofontov <ilya.ksenofontov@ocrv.ru>
2019-07-27 12:05:20 +0300

    # Offer to previous commits
    Signed-off-by: Ilya Ksenofontov <ilya.ksenofontov@ocrv.ru>

--- open-metadata-implementation/repository-services/repository-services-apis/src/test/java/org/odpi/openmetadata/repositoryservices/connectors/stores/metadatacollectionstore/properties/instances/ArrayPropertyValueTest.java
+++ open-metadata-implementation/repository-services/repository-services-apis/src/test/java/org/odpi/openmetadata/repositoryservices/connectors/stores/metadatacollectionstore/properties/instances/ArrayPropertyValueTest.java
@@ -21,7 +21,7 @@ public class ArrayPropertyValueTest {
     @BeforeTest
     private void setObject() {
         ArrayPropertyValue arrayPropertyValue = new ArrayPropertyValue();
-        arrayPropertyValue.setArrayCount(1);
+        arrayPropertyValue.setArrayCount(100);
         arrayPropertyValue.setTypeGUID(typeGUID);
         arrayPropertyValue.setTypeGUID(typeName);
 
@@ -30,32 +30,37 @@ public class ArrayPropertyValueTest {
         propertyValueMock.setTypeName(typeName);
         propertyValueMock.setInstancePropertyCategory(category);
         arrayPropertyValue.setArrayValue(0, propertyValueMock);
+
+        InstancePropertyValue propertyValueMock1 = new InstancePropertyValueMock();
+        propertyValueMock1.setTypeGUID(typeGUID);
+        propertyValueMock1.setTypeName(typeName);
+        propertyValueMock1.setInstancePropertyCategory(category);
+        arrayPropertyValue.setArrayValue(17, propertyValueMock1);
+
+        InstancePropertyValue propertyValueMock2 = new InstancePropertyValueMock();
+        propertyValueMock2.setTypeGUID(typeGUID);
+        propertyValueMock2.setTypeName(typeName);
+        propertyValueMock2.setInstancePropertyCategory(category);
+        arrayPropertyValue.setArrayValue(88, propertyValueMock2);
+
         this.arrayPropertyValue = arrayPropertyValue;
     }
 
     @Test
     public void testValueAsObject() {
-        ArrayPropertyValue arrayPropertyValue = new ArrayPropertyValue(this.arrayPropertyValue);
-        arrayPropertyValue.setArrayCount(2);
-
-        InstancePropertyValue propertyValueMock = new InstancePropertyValueMock();
-        propertyValueMock.setTypeGUID(typeGUID);
-        propertyValueMock.setTypeName(typeName);
-        propertyValueMock.setInstancePropertyCategory(category);
-        arrayPropertyValue.setArrayValue(1, propertyValueMock);
-
         Object actual = arrayPropertyValue.valueAsObject();
         Object expected = new HashMap<String, Object>() {{
             put("0", "PropertyValue");
-            put("1", "PropertyValue");
-        }}.values();
-        assertEquals((Collection) actual, (Collection) expected);
+            put("17", "PropertyValue");
+            put("88", "PropertyValue");
+        }};
+        assertEquals(actual, expected);
     }
 
     @Test
     public void testValueAsString() {
         String actual = arrayPropertyValue.valueAsString();
-        String expected = "[PropertyValue]";
+        String expected = "{0=PropertyValue, 88=PropertyValue, 17=PropertyValue}";
 
         assertEquals(actual, expected);
     }
