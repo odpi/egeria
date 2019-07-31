f58840d9a6234e6454537e13f34468c2562695a9
Guy Ackermans <guy.ackermans92@gmail.com>
2019-07-31 17:10:21 +0200

    Open Lineage Client: Split Enums
    Signed-off-by: Guy Ackermans <guy.ackermans92@gmail.com>

--- open-metadata-implementation/governance-servers/open-lineage-services/open-lineage-services-client/src/main/java/org/odpi/openmetadata/governanceservers/openlineage/client/util/Queries.java
+++ open-metadata-implementation/governance-servers/open-lineage-services/open-lineage-services-client/src/main/java/org/odpi/openmetadata/governanceservers/openlineage/client/util/Queries.java
@@ -2,16 +2,15 @@
 /* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.governanceservers.openlineage.client.util;
 
-public enum Graphs {
+public enum Queries {
 
-    main("main"),
-    buffer("buffer"),
-    history("history"),
-    mock("mock");
+    ultimatesource("ultimate-source"),
+    ultimatedestination("ultimate-destination"),
+    glossary("glossary");
 
     private String name;
 
-    Graphs(String name) {
+    Queries(String name) {
         this.name = name;
     }
 
