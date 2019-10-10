405bd9630144c98d4f98c2ee57588e065b770e14
ruxandraRosu <ruxandra-gabriela.rosu@ing.com>
2019-10-10 13:02:00 +0300

    Modify UserInfoController to get the tokenUser from authentication object
    Signed-off-by: ruxandraRosu <ruxandra-gabriela.rosu@ing.com>

--- open-metadata-implementation/user-interfaces/access-services-user-interface/src/main/java/org/odpi/openmetadata/userinterface/accessservices/api/UserInfoController.java
+++ open-metadata-implementation/user-interfaces/access-services-user-interface/src/main/java/org/odpi/openmetadata/userinterface/accessservices/api/UserInfoController.java
@@ -5,6 +5,8 @@ package org.odpi.openmetadata.userinterface.accessservices.api;
 import org.odpi.openmetadata.userinterface.accessservices.auth.AuthService;
 import org.odpi.openmetadata.userinterface.accessservices.auth.TokenUser;
 import org.odpi.openmetadata.userinterface.accessservices.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,22 +17,22 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/users")
+@DependsOn("securityConfig")
 public class UserInfoController {
 
+    @Autowired
     private AuthService authService;
 
     @RequestMapping( value ="/current", method = RequestMethod.GET)
     public User getUser(HttpServletRequest request) throws HttpClientErrorException{
         Authentication auth = authService.getAuthentication(request);
-        TokenUser tokenUser = authService.getTokenUser(auth);
 
-        if(tokenUser == null || tokenUser.getUser() == null){
+        if(auth == null || auth.getDetails() == null || !(auth.getDetails() instanceof TokenUser)){
             throw new UserNotAuthorizedException("User is not authorized");
         }
+
+        TokenUser tokenUser = (TokenUser) auth.getDetails();
         return tokenUser.getUser();
     }
 
-    public void setAuthService(AuthService authService) {
-        this.authService = authService;
-    }
 }
