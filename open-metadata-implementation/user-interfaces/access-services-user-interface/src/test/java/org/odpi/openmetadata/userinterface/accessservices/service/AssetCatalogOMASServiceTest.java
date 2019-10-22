bd0a20030bbf541e5ac9ff5eceef4e8a565776fa
Daniela Otelea <daniela-valentina.otelea@ing.com>
2019-10-22 15:57:47 +0300

    removed comments
    Signed-off-by: Daniela Otelea <daniela-valentina.otelea@ing.com>

--- open-metadata-implementation/user-interfaces/access-services-user-interface/src/test/java/org/odpi/openmetadata/userinterface/accessservices/service/AssetCatalogOMASServiceTest.java
+++ open-metadata-implementation/user-interfaces/access-services-user-interface/src/test/java/org/odpi/openmetadata/userinterface/accessservices/service/AssetCatalogOMASServiceTest.java
@@ -36,14 +36,13 @@ import static org.mockito.Mockito.when;
 @MockitoSettings(strictness = Strictness.WARN)
 class AssetCatalogOMASServiceTest {
 
+    private static final String CONFIDENTIALITY = "Confidentiality";
     private final String COMPLEX_SCHEMA_TYPE = "ComplexSchemaType";
     private final String assetId = "6662c0f2.e1b1ec6c.54865omh1.pco9ecb.c3g5f1.pfvf6bdv95dnc67jq2jli";
     private final String schemaId = "abababa1.e1b1ec6c.54865omh1.pco9ecb.c3g5f1.pfvf6bdv95dnc67jq2jli";
-    private final String classificationDescription = "A uniquely identifying relational column.";
     private final String typeDef = "Asset";
     private final String relationshipTypeDef = "AssetSchemaType";
     private final String typeDefDescription = "A semantic description of something, such as a concept, object, asset, technology, role or group.";
-    private final String relationshipGuid = "815b004d-73c6-4728-9dd9-536f4fe803cd";
     private final String user = "demo";
 
     @Mock
@@ -84,72 +83,9 @@ class AssetCatalogOMASServiceTest {
     public void testGetClassificationForAsset() throws PropertyServerException, org.odpi.openmetadata.frameworks.connectors.ffdc.InvalidParameterException {
         ClassificationsResponse expectedResponse = mockClassificationsResponse();
         when(assetCatalog.getClassificationsForAsset(anyString(), anyString(), anyString(), anyString())).thenReturn(expectedResponse);
-        List<Classification> resultList = assetCatalogOMASService.getClassificationsForAsset(user, assetId, typeDef, "Confidentiality");
+        List<Classification> resultList = assetCatalogOMASService.getClassificationsForAsset(user, assetId, typeDef, CONFIDENTIALITY);
         verifyClassificationResponse(resultList);
     }
-
-//    @Test()
-//    @DisplayName("Asset Summary - exception")
-//    public void testGetAssetSummaryException() throws PropertyServerException, InvalidParameterException {
-//        InvalidParameterException mockedException = mockExceptionResponse("unit-test");
-//        when(assetCatalog.getAssetSummary(anyString(), anyString())).thenThrow(mockedException);
-//        InvalidParameterException thrown = assertThrows(InvalidParameterException.class,
-//                () -> assetCatalogOMASService.getAssetSummary(user, "asset-id-does-not-exists"));
-//        assertTrue(thrown.getMessage().contains("OMAS-ASSET-CATALOG-400-002"));
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Asset Details - exception")
-//    public void testGetAssetDetailsException() throws PropertyServerException, InvalidParameterException {
-//        InvalidParameterException mockedException = mockExceptionResponse("unit-test");
-//        when(assetCatalog.getAssetDetails(anyString(), anyString())).thenThrow(mockedException);
-//        InvalidParameterException thrown = assertThrows(InvalidParameterException.class,
-//                () -> assetCatalogOMASService.getAssetDetails(user, "asset-id-does-not-exists"));
-//        assertTrue(thrown.getMessage().contains("OMAS-ASSET-CATALOG-400-002"));
-//    }
-//
-//    @Test
-//    @DisplayName("Asset Universe - exception")
-//    public void testGetAssetUniverseException() throws PropertyServerException, InvalidParameterException {
-//        InvalidParameterException mockedException = mockExceptionResponse("unit-test");
-//        when(assetCatalog.getAssetUniverse(, anyString(), anyString())).thenThrow(mockedException);
-//        InvalidParameterException thrown = assertThrows(InvalidParameterException.class,
-//                () -> assetCatalogOMASService.getAssetUniverse(user, "asset-id-does-not-exists"));
-//        assertTrue(thrown.getMessage().contains("OMAS-ASSET-CATALOG-400-002"));
-//    }
-//
-//    @Test
-//    @DisplayName("Asset Relationships - exception")
-//    public void testGetAssetRelationshipsException() throws PropertyServerException, InvalidParameterException {
-//        InvalidParameterException mockedException = mockExceptionResponse("unit-test");
-//        when(assetCatalog.getAssetRelationships(anyString(), anyString())).thenThrow(mockedException);
-//        InvalidParameterException thrown = assertThrows(InvalidParameterException.class,
-//                () -> assetCatalogOMASService.getAssetRelationships(user, "asset-id-does-not-exists"));
-//        assertTrue(thrown.getMessage().contains("OMAS-ASSET-CATALOG-400-002"));
-//    }
-//
-//    @Test
-//    @DisplayName("Asset Relationships for type - exception")
-//    public void testGetAssetRelationshipsForTypeException() throws PropertyServerException, InvalidParameterException {
-//        InvalidParameterException mockedException = mockExceptionResponse("unit-test");
-//        when(assetCatalog.getAssetRelationshipsForType(anyString(), anyString(), anyString())).thenThrow(mockedException);
-//        InvalidParameterException thrown = assertThrows(InvalidParameterException.class,
-//                () -> assetCatalogOMASService.getAssetRelationshipsForType(
-//                        user, "asset-id-does-not-exists",
-//                        "relationship-type-does-not-exists"));
-//        assertTrue(thrown.getMessage().contains("OMAS-ASSET-CATALOG-400-002"));
-//    }
-//
-//    @Test
-//    @DisplayName("Asset Classification - exception")
-//    public void testGetAssetClassificationException() throws PropertyServerException, InvalidParameterException {
-//        InvalidParameterException mockedException = mockExceptionResponse("unit-test");
-//        when(assetCatalog.getClassificationsForAsset(anyString(), anyString())).thenThrow(mockedException);
-//        InvalidParameterException thrown = assertThrows(InvalidParameterException.class, ()
-//                -> assetCatalogOMASService.getClassificationsForAsset(user, "asset-id-does-not-exists"));
-//        assertTrue(thrown.getMessage().contains("OMAS-ASSET-CATALOG-400-002"));
-//    }
 
     private AssetDescriptionResponse mockAssetDescriptionResponse() {
         AssetDescriptionResponse expectedResponse = new AssetDescriptionResponse();
@@ -192,7 +128,7 @@ class AssetCatalogOMASServiceTest {
         ClassificationsResponse expectedResponse = new ClassificationsResponse();
         List<Classification> expectedClassificationList = new ArrayList<>();
         Classification expectedClassification = new Classification();
-        expectedClassification.setTypeDefName("Confidentiality");
+        expectedClassification.setTypeDefName(CONFIDENTIALITY);
         expectedClassificationList.add(expectedClassification);
         expectedResponse.setClassifications(expectedClassificationList);
         return expectedResponse;
@@ -218,6 +154,6 @@ class AssetCatalogOMASServiceTest {
     private void verifyClassificationResponse(List<Classification> resultList) {
         assertFalse(resultList.isEmpty());
         Classification classification = resultList.get(0);
-        assertEquals(classification.getTypeDefName(), "Confidentiality");
+        assertEquals(classification.getTypeDefName(), CONFIDENTIALITY);
     }
 }
