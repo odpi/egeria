374463c332e39ede5111e5b74849a17ce31fd472
David Radley <david_radley@uk.ibm.com>
2019-11-01 15:00:11 +0000

    #1930 adding multi tenancy ui fixing merge issues
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- open-metadata-implementation/user-interfaces/ui-admin-services/ui-admin-services-api/src/main/java/org/odpi/openmetadata/userinterface/adminservices/configStore/package-info.java
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: Apache 2.0 */
-/* Copyright Contributors to the ODPi Egeria project. */
-package org.odpi.openmetadata.userinterface.adminservices.configStore;
-
-/**
- * The <b>configStore</b> package provides the interface definition for the ui configStore.
- * Egeria has a default implementation of this connector that stores each configuration document in a separate file.
- */
