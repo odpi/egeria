706cc8f9616076feeeaaf021b903159421d2da6d
Christopher Grote <chris@thegrotes.net>
2019-11-05 11:18:43 +0000

    Adds missing docs in line with Developer Guidelines
    Signed-off-by: Christopher Grote <chris@thegrotes.net>

---
+++ open-metadata-implementation/governance-servers/data-engine-proxy-services/data-engine-proxy-connector/src/main/java/org/odpi/openmetadata/openconnectors/governancedaemonconnectors/dataengineproxy/model/package-info.java
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: Apache 2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+package org.odpi.openmetadata.openconnectors.governancedaemonconnectors.dataengineproxy.model;
+
+/**
+ * The model package provides the definitions of the objects that data engines are expected to use in order
+ * to integrate with the Data Engine Proxy Services.
+ */
