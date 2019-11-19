a9de91583160845b1019b53d4e5567b1e045767f
Guy Ackermans <guy.ackermans92@gmail.com>
2019-11-19 13:11:55 +0100

    Undo incorrect javadoc fix
    Signed-off-by: Guy Ackermans <guy.ackermans92@gmail.com>

--- open-metadata-implementation/governance-servers/discovery-engine-services/discovery-engine-services-server/src/main/java/org/odpi/openmetadata/discoveryserver/server/DiscoveryServerInstanceHandler.java
+++ open-metadata-implementation/governance-servers/discovery-engine-services/discovery-engine-services-server/src/main/java/org/odpi/openmetadata/discoveryserver/server/DiscoveryServerInstanceHandler.java
@@ -17,7 +17,7 @@ import org.odpi.openmetadata.frameworks.connectors.ffdc.UserNotAuthorizedExcepti
 class DiscoveryServerInstanceHandler extends GovernanceServerServiceInstanceHandler
 {
     /**
-     * Default constructor registers the governance server
+     * Default constructor registers the access service
      */
     DiscoveryServerInstanceHandler()
     {
