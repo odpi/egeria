cb509b95667c962297b69fdb773ead5311a63e5f
Cong Chen <cong.chen@ing.com>
2019-11-26 16:26:34 +0100

    Add deduplicate function for process context.
    Signed-off-by: Cong Chen <cong.chen@ing.com>

--- open-metadata-implementation/access-services/asset-lineage/asset-lineage-server/src/main/java/org/odpi/openmetadata/accessservices/assetlineage/listeners/AssetLineageOMRSTopicListener.java
+++ open-metadata-implementation/access-services/asset-lineage/asset-lineage-server/src/main/java/org/odpi/openmetadata/accessservices/assetlineage/listeners/AssetLineageOMRSTopicListener.java
@@ -170,7 +170,7 @@ public class AssetLineageOMRSTopicListener implements OMRSTopicListener {
 
         Set<String> superTypes = collectSuperTypes(ASSET_LINEAGE_OMAS, entityDetail.getType().getTypeDefName());
 
-        processNewEntityEvent(instanceEvent.getEntity(),
+        processNewEntityEvent(entityDetail,
                 serviceOperationName + NEW_ENTITY_EVENT.getName());
 
     }
@@ -347,7 +347,7 @@ public class AssetLineageOMRSTopicListener implements OMRSTopicListener {
 
         ProcessContextHandler processContextHandler = instanceHandler.getProcessHandler(serverUserName, serverName, serviceOperationName);
         Map<String, Set<GraphContext>> processContext = processContextHandler.getProcessContext(serverUserName, entityDetail.getGUID());
-
+        instanceHandler.getCommonHandler(serverUserName, serverName, serviceOperationName).deduplicateGraphContex(processContext);
         LineageEvent event = new LineageEvent();
         event.setAssetContext(processContext);
         event.setAssetLineageEventType(AssetLineageEventType.PROCESS_CONTEXT_EVENT);
