a2d243ad20fb68bc6efb51b9be80d7e20f549e76
Dimitrios (D.) Maimaris <dimitrios.maimaris@ing.com>
2020-01-09 10:34:42 +0100

    optimizations janus connector
    Signed-off-by: Dimitrios (D.) Maimaris <dimitrios.maimaris@ing.com>

--- open-metadata-implementation/adapters/open-connectors/governance-daemon-connectors/open-lineage-connectors/open-lineage-janus-connector/src/main/java/org/odpi/openmetadata/openconnectors/governancedaemonconnectors/openlineageconnectors/janusconnector/utils/Constants.java
+++ open-metadata-implementation/adapters/open-connectors/governance-daemon-connectors/open-lineage-connectors/open-lineage-janus-connector/src/main/java/org/odpi/openmetadata/openconnectors/governancedaemonconnectors/openlineageconnectors/janusconnector/utils/Constants.java
@@ -40,10 +40,12 @@ public final class Constants {
     public static final String TERM_CATEGORIZATION = "TermCategorization";
     public static final String PORT_DELEGATION = "PortDelegation";
     public static final String PROCESS_PORT = "ProcessPort";
+    public static final String PORT_IMPLEMENTATION = "PortImplementation";
     public static final String LINEAGE_MAPPING = "LineageMapping";
     public static final String SCHEMA_TYPE = "SchemaType";
     public static final String PORT_SCHEMA = "PortSchema";
     public static final String NESTED_FILE = "NestedFile";
+    public static final String NESTED_SCHEMA_ATTRIBUTE = "NestedSchemaAttribute";
 
     //Instance Properties fields
     public static final String TYPE = "dataType";
