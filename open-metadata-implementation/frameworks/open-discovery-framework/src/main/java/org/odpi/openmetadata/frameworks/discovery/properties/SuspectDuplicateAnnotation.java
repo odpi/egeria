6521c480e4b0351f99ac13ad2396edd4fdce1ff0
Mandy Chessell <mandy_chessell@uk.ibm.com>
2020-01-30 17:29:18 +0000

    Annotation beans
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/frameworks/open-discovery-framework/src/main/java/org/odpi/openmetadata/frameworks/discovery/properties/SuspectDuplicateAnnotation.java
+++ open-metadata-implementation/frameworks/open-discovery-framework/src/main/java/org/odpi/openmetadata/frameworks/discovery/properties/SuspectDuplicateAnnotation.java
@@ -25,16 +25,27 @@ public class SuspectDuplicateAnnotation extends Annotation
 {
     private static final long serialVersionUID = 1L;
 
-    List<String>  duplicateAnchorGUIDs = null;
-    List<String>  matchingPropertyNames = null;
-    List<String>  matchingClassificationNames = null;
-    List<String>  matchingAttachmentGUIDs = null;
-    List<String>  matchingRelationshipGUIDs = null;
+    private List<String>  duplicateAnchorGUIDs = null;
+    private List<String>  matchingPropertyNames = null;
+    private List<String>  matchingClassificationNames = null;
+    private List<String>  matchingAttachmentGUIDs = null;
+    private List<String>  matchingRelationshipGUIDs = null;
 
+
+    /**
+     * Default constructor
+     */
     public SuspectDuplicateAnnotation()
     {
+        super();
     }
 
+
+    /**
+     * Copy/clone constructor.
+     *
+     * @param template object to copy
+     */
     public SuspectDuplicateAnnotation(SuspectDuplicateAnnotation template)
     {
         super(template);
@@ -49,56 +60,122 @@ public class SuspectDuplicateAnnotation extends Annotation
         }
     }
 
+
+    /**
+     * Return the list of unique identifiers for the Assets that are identifier as duplicate suspects.
+     *
+     * @return list of string guids
+     */
     public List<String> getDuplicateAnchorGUIDs()
     {
         return duplicateAnchorGUIDs;
     }
 
+
+    /**
+     * Set up the list of unique identifiers for the Assets that are identifier as duplicate suspects.
+     *
+     * @param duplicateAnchorGUIDs list of string guids
+     */
     public void setDuplicateAnchorGUIDs(List<String> duplicateAnchorGUIDs)
     {
         this.duplicateAnchorGUIDs = duplicateAnchorGUIDs;
     }
 
+
+    /**
+     * Return the list of property names whose values match in all of the duplicate suspects.
+     *
+     * @return list of property names
+     */
     public List<String> getMatchingPropertyNames()
     {
         return matchingPropertyNames;
     }
 
+
+    /**
+     * Set up the list of property names whose values match in all of the duplicate suspects.
+     *
+     * @param matchingPropertyNames list of property names
+     */
     public void setMatchingPropertyNames(List<String> matchingPropertyNames)
     {
         this.matchingPropertyNames = matchingPropertyNames;
     }
 
+
+    /**
+     * Return the list of classifications that match in all of the duplicate suspects.
+     *
+     * @return list of classification names
+     */
     public List<String> getMatchingClassificationNames()
     {
         return matchingClassificationNames;
     }
 
+
+    /**
+     * Set up the list of classifications that match in all of the duplicate suspects.
+     *
+     * @param matchingClassificationNames list of classification names
+     */
     public void setMatchingClassificationNames(List<String> matchingClassificationNames)
     {
         this.matchingClassificationNames = matchingClassificationNames;
     }
 
+
+    /**
+     * Return the list of unique identifiers for attachments that match in all of the duplicate suspects.
+     *
+     * @return list of string guids
+     */
     public List<String> getMatchingAttachmentGUIDs()
     {
         return matchingAttachmentGUIDs;
     }
 
+
+    /**
+     * Set up the list of unique identifiers for attachments that match in all of the duplicate suspects.
+     *
+     * @param matchingAttachmentGUIDs list of string guids
+     */
     public void setMatchingAttachmentGUIDs(List<String> matchingAttachmentGUIDs)
     {
         this.matchingAttachmentGUIDs = matchingAttachmentGUIDs;
     }
 
+
+    /**
+     * Return the list of relationships that are common across all of the duplicate suspects.
+     *
+     * @return list of string guids
+     */
     public List<String> getMatchingRelationshipGUIDs()
     {
         return matchingRelationshipGUIDs;
     }
 
+
+    /**
+     * Set up the list of relationships that are common across all of the duplicate suspects.
+     *
+     * @param matchingRelationshipGUIDs  list of string guids
+     */
     public void setMatchingRelationshipGUIDs(List<String> matchingRelationshipGUIDs)
     {
         this.matchingRelationshipGUIDs = matchingRelationshipGUIDs;
     }
 
+
+    /**
+     * Standard toString method.
+     *
+     * @return print out of variables in a JSON-style
+     */
     @Override
     public String toString()
     {
@@ -128,6 +205,12 @@ public class SuspectDuplicateAnnotation extends Annotation
     }
 
 
+    /**
+     * Compare the values of the supplied object with those stored in the current object.
+     *
+     * @param objectToCompare supplied object
+     * @return boolean result of comparison
+     */
     @Override
     public boolean equals(Object objectToCompare)
     {
@@ -152,7 +235,11 @@ public class SuspectDuplicateAnnotation extends Annotation
     }
 
 
-
+    /**
+     * Create a hash code for this element type.
+     *
+     * @return int hash code
+     */
     @Override
     public int hashCode()
     {
