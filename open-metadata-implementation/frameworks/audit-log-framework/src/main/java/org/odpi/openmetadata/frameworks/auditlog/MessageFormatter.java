2f3d58e3fcb2ea003314702ae46589d5301ca301
Mandy Chessell <mandy_chessell@uk.ibm.com>
2020-03-07 14:18:34 +0000

    Validate message parameters (#2537)
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/frameworks/audit-log-framework/src/main/java/org/odpi/openmetadata/frameworks/auditlog/MessageFormatter.java
+++ open-metadata-implementation/frameworks/audit-log-framework/src/main/java/org/odpi/openmetadata/frameworks/auditlog/MessageFormatter.java
@@ -26,22 +26,29 @@ public class MessageFormatter
 
 
     /**
-     * Create a formatted message from a message definition instance.  This instance
-     * contains the unique message identifier, the default message template and the
-     * parameters to insert into it.  The Audit Log controls whether to use the default
-     * message template or substitute it for a version in a different language.
+     * Format the message.
      *
-     * The method is public to allow external components to format messages - for example,
-     * from exceptions.
-     *
-     * @param messageDefinition details about the message to format.
+     * @param messageTemplate message with placeholders
+     * @param messageParameters parameters to insert into placeholders
      * @return formatted message
      */
-    public String getFormattedMessageText(MessageDefinition messageDefinition)
+    private String formatMessage(String     messageTemplate,
+                                 String[]   messageParameters)
     {
-        MessageFormat mf = new MessageFormat(messageDefinition.getMessageTemplate());
+        MessageFormat mf        = new MessageFormat(messageTemplate);
+        String formattedMessage = messageTemplate;
+        int    parameterCount   = 0;
 
-        String formattedMessage = mf.format(messageDefinition.getMessageParams());
+        if (messageParameters != null)
+        {
+            parameterCount = messageParameters.length;
+            formattedMessage = mf.format(messageParameters);
+        }
+
+        if (mf.getFormats().length > parameterCount)
+        {
+            log.error("Missing parameter for message {}", messageTemplate);
+        }
 
         log.debug("New message: {}", formattedMessage);
 
@@ -61,14 +68,28 @@ public class MessageFormatter
      * @param messageDefinition details about the message to format.
      * @return formatted message
      */
+    String getFormattedMessageText(MessageDefinition messageDefinition)
+    {
+        return this.formatMessage(messageDefinition.getMessageTemplate(),
+                                  messageDefinition.getMessageParams());
+    }
+
+
+    /**
+     * Create a formatted message from a message definition instance.  This instance
+     * contains the unique message identifier, the default message template and the
+     * parameters to insert into it.  The Audit Log controls whether to use the default
+     * message template or substitute it for a version in a different language.
+     *
+     * The method is public to allow external components to format messages - for example,
+     * from exceptions.
+     *
+     * @param messageDefinition details about the message to format.
+     * @return formatted message
+     */
     public String getFormattedMessage(MessageDefinition messageDefinition)
     {
-        MessageFormat mf = new MessageFormat(messageDefinition.getMessageTemplate());
-
-        String formattedMessage = messageDefinition.getMessageId() + " " + mf.format(messageDefinition.getMessageParams());
-
-        log.debug("New message: {}", formattedMessage);
-
-        return formattedMessage;
+        return messageDefinition.getMessageId() + " " + this.formatMessage(messageDefinition.getMessageTemplate(),
+                                                                           messageDefinition.getMessageParams());
     }
 }
