88f85b1cb151ae609ed6508251c0449111b012f6
Mandy Chessell <mandy_chessell@uk.ibm.com>
2020-03-08 12:06:40 +0000

    Moving files
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

---
+++ open-metadata-implementation/common-services/ffdc-services/src/test/java/org/odpi/openmetadata/commonservices/ffdc/AuditCodeTest.java
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+package org.odpi.openmetadata.commonservices.ffdc;
+
+import org.odpi.openmetadata.test.unittest.utilities.AuditLogMessageSetTest;
+import org.testng.annotations.Test;
+
+/**
+ * Verify the OMAGCommonAuditCode enum contains unique message ids, non-null names and descriptions and can be
+ * serialized to JSON and back again.
+ */
+public class AuditLogTest extends AuditLogMessageSetTest
+{
+    /**
+     * Validated the values of the enum.
+     */
+    @Test public void testAllAuditCodeValues()
+    {
+        for (OMAGCommonAuditCode auditCode : OMAGCommonAuditCode.values())
+        {
+            testSingleAuditCodeValue(auditCode, "OMAG-COMMON");
+        }
+    }
+}
