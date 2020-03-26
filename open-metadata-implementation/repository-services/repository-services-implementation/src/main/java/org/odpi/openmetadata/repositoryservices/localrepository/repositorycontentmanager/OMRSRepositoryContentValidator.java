9eb47024260a50720d4ef0596a07060cecc4696c
Ilya Ksenofontov <fonto.trg@gmail.com>
2020-03-26 16:58:18 +0300

    # Removed duplicate in switch case
    Signed-off-by: immmus <fonto.trg@gmail.com>

--- open-metadata-implementation/repository-services/repository-services-implementation/src/main/java/org/odpi/openmetadata/repositoryservices/localrepository/repositorycontentmanager/OMRSRepositoryContentValidator.java
+++ open-metadata-implementation/repository-services/repository-services-implementation/src/main/java/org/odpi/openmetadata/repositoryservices/localrepository/repositorycontentmanager/OMRSRepositoryContentValidator.java
@@ -1875,20 +1875,10 @@ public class OMRSRepositoryContentValidator implements OMRSRepositoryValidator
                     break;
 
                 case MAP:
-                    if (propertyDefinitionType == AttributeTypeDefCategory.COLLECTION)
-                    {
-                        validPropertyType = true;
-                    }
-                    break;
-
-                case ARRAY:
-                    if (propertyDefinitionType == AttributeTypeDefCategory.COLLECTION)
-                    {
-                        validPropertyType = true;
-                    }
-                    break;
 
                 case STRUCT:
+
+                case ARRAY:
                     if (propertyDefinitionType == AttributeTypeDefCategory.COLLECTION)
                     {
                         validPropertyType = true;
