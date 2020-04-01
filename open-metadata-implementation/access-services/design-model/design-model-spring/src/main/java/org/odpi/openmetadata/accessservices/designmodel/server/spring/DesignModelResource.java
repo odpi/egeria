8fb016660084e224a53f961b17250673fdfd9b80
Nigel Jones <nigel.l.jones+git@gmail.com>
2020-04-01 16:13:28 +0100

    #1124 Add tags to split up swagger docs into sections
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- open-metadata-implementation/access-services/design-model/design-model-spring/src/main/java/org/odpi/openmetadata/accessservices/designmodel/server/spring/DesignModelResource.java
+++ open-metadata-implementation/access-services/design-model/design-model-spring/src/main/java/org/odpi/openmetadata/accessservices/designmodel/server/spring/DesignModelResource.java
@@ -2,8 +2,11 @@
 /* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.accessservices.designmodel.server.spring;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.odpi.openmetadata.accessservices.designmodel.server.DesignModelRESTServices;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/servers/{serverName}/open-metadata/access-services/data-model/users/{userId}")
+
+@Tag(name="Design Model", description="The open metadata conformance suite provides a testing framework to help the developers integrate a specific technology into the open metadata ecosystem.", externalDocs=@ExternalDocumentation(description="Design Model Open Metadata Access Service (OMAS)",url="https://egeria.odpi.org/open-metadata-implementation/access-services/design-model/"))
+
 public class DesignModelResource
 {
     private DesignModelRESTServices restAPI = new DesignModelRESTServices();
