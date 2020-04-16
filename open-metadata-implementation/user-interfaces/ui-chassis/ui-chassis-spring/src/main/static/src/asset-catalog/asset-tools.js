666ce4efee3ffae389ba52f07733f1215c2746ed
Bogdan Sava <bogdan.sava@gmail.com>
2020-04-16 15:54:53 +0300

    add asset-tools component
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

---
+++ open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/static/src/asset-catalog/asset-tools.js
@@ -0,0 +1,58 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+import { PolymerElement, html } from '@polymer/polymer';
+import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
+import { ItemViewBehavior} from '../common/item';
+import '@polymer/paper-listbox/paper-listbox.js';
+import '@polymer/paper-item/paper-item.js';
+import '@polymer/paper-item/paper-item-body';
+import '@polymer/paper-styles/paper-styles';
+import '@vaadin/vaadin-icons/vaadin-icons';
+import '@polymer/app-layout/app-grid/app-grid-style';
+
+import '../shared-styles.js';
+import '../common/props-table';
+
+class AssetTools extends mixinBehaviors([ItemViewBehavior], PolymerElement) {
+    static get template() {
+        return html`
+      
+      <style include="shared-styles">
+        :host {
+          display: block;
+          padding: 10px 24px;
+          background-color:  var(--egeria-background-color);
+          --iron-icon-fill-color: var(--egeria-primary-color);
+        }
+        ul#menu, ul#menu li {
+          display:inline;
+          padding: 0;
+          margin: 0 10pt;
+        }
+      </style>
+
+    <div style="border: solid 1px var(--egeria-primary-color); padding: 5pt;"> 
+        <ul id="menu"> 
+            <li> 
+                <a href="#/asset-lineage/ultimateSource/[[guid]]" title="Ultimate Source Lineage"><iron-icon icon="vaadin:connect-o" style="transform: rotate(180deg)"></iron-icon></a>
+            </li>
+            <li> 
+                <a href="#/asset-lineage/endToEnd/[[guid]]" title="End2End Lineage"><iron-icon icon="vaadin:cluster"></iron-icon></a>
+            </li>
+            <li> 
+                <a href="#/asset-lineage/ultimateDestination/[[guid]]" title="Ultimate Destination Lineage"><iron-icon icon="vaadin:connect-o"></iron-icon></a>
+            </li>
+            <li> 
+                <a href="#/asset-lineage/glossaryLineage/[[guid]]" title="Glossary Lineage"><iron-icon icon="vaadin:file-tree"></iron-icon></a>
+            </li>
+            <li> 
+                <a href="#/asset-lineage/sourceAndDestination/[[guid]]" title="Source and Destination Lineage"><iron-icon icon="vaadin:exchange"></iron-icon></a>
+            </li>
+        </ul>
+    </div>
+    `;
+    }
+
+}
+
+window.customElements.define('asset-tools', AssetTools);
