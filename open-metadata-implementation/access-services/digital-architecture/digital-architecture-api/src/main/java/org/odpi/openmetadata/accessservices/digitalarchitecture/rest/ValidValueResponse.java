735d17fde398560e27f11087093b0c2617f5ea55
Mandy Chessell <mandy_chessell@uk.ibm.com>
2020-04-17 20:32:53 +0100

    Fix build warning around serialVersionUID
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/access-services/digital-architecture/digital-architecture-api/src/main/java/org/odpi/openmetadata/accessservices/digitalarchitecture/rest/ValidValueResponse.java
+++ open-metadata-implementation/access-services/digital-architecture/digital-architecture-api/src/main/java/org/odpi/openmetadata/accessservices/digitalarchitecture/rest/ValidValueResponse.java
@@ -22,6 +22,8 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class ValidValueResponse extends DigitalArchitectureOMASAPIResponse
 {
+    private static final long    serialVersionUID = 1L;
+
     private ValidValueElement element = null;
 
 
--- open-metadata-implementation/access-services/digital-architecture/digital-architecture-api/src/main/java/org/odpi/openmetadata/accessservices/digitalarchitecture/rest/ValidValuesImplAssetsResponse.java
+++ open-metadata-implementation/access-services/digital-architecture/digital-architecture-api/src/main/java/org/odpi/openmetadata/accessservices/digitalarchitecture/rest/ValidValuesImplAssetsResponse.java
@@ -24,6 +24,8 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class ValidValuesImplAssetsResponse extends DigitalArchitectureOMASAPIResponse
 {
+    private static final long    serialVersionUID = 1L;
+
     private List<ValidValueImplAssetElement> elementList = null;
 
 
