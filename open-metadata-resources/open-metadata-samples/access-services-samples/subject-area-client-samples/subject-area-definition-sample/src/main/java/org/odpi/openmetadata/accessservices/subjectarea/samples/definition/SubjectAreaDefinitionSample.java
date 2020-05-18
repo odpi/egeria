7ddc6a4890688307b894d9a74ab3e940ac7beeee
Nigel Jones <nigel.l.jones+git@gmail.com>
2020-05-18 15:45:59 +0100

    #2693 refactor subject area samples
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- open-metadata-resources/open-metadata-samples/access-services-samples/subject-area-client-samples/subject-area-definition-sample/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/samples/definition/SubjectAreaDefinitionSample.java
+++ open-metadata-resources/open-metadata-samples/access-services-samples/subject-area-client-samples/subject-area-definition-sample/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/samples/definition/SubjectAreaDefinitionSample.java
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright Contributors to the ODPi Egeria project. */
-package org.odpi.openmetadata.accessservices.subjectarea.samples;
+package org.odpi.openmetadata.accessservices.subjectarea.samples.definition;
 
 import org.odpi.openmetadata.accessservices.subjectarea.SubjectArea;
 import org.odpi.openmetadata.accessservices.subjectarea.SubjectAreaCategory;
