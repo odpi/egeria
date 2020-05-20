106d325055004b729569d0d63afebb62629f97ce
Bogdan Sava <bogdan.sava@gmail.com>
2020-05-20 20:33:27 +0300

    loading default truststore
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- open-metadata-implementation/server-chassis/server-chassis-spring/src/main/java/org/odpi/openmetadata/serverchassis/springboot/OMAGServerPlatform.java
+++ open-metadata-implementation/server-chassis/server-chassis-spring/src/main/java/org/odpi/openmetadata/serverchassis/springboot/OMAGServerPlatform.java
@@ -25,8 +25,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.*;
 
 
@@ -57,6 +59,9 @@ public class OMAGServerPlatform
 
     @Value("${startup.server.list}")
     String startupServers;
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private ApplicationEventPublisher applicationEventPublisher;
@@ -184,6 +189,18 @@ public class OMAGServerPlatform
             temporaryDeactivateServers();
         }
 
+    }
+
+    @PostConstruct
+    private void configureTrustStore() {
+
+        //making sure truststore was not set using JVM options
+        if(System.getProperty("javax.net.ssl.trustStore")==null) {
+            //load the 'javax.net.ssl.trustStore' and
+            //'javax.net.ssl.trustStorePassword' from application.properties
+            System.setProperty("javax.net.ssl.trustStore", env.getProperty("server.ssl.trust-store"));
+            System.setProperty("javax.net.ssl.trustStorePassword", env.getProperty("server.ssl.trust-store-password"));
+        }
     }
 
 }
