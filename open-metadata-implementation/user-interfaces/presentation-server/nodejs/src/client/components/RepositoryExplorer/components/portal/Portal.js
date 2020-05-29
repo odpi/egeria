a1ecbd334cc080308e826a4c1d5de325661aa488
Graham Wallis <graham_wallis@uk.ibm.com>
2020-05-29 09:38:28 +0100

    Clean up of Portal module
    Signed-off-by: Graham Wallis <graham_wallis@uk.ibm.com>

--- open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/RepositoryExplorer/components/portal/Portal.js
+++ open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/RepositoryExplorer/components/portal/Portal.js
@@ -15,42 +15,23 @@ import "./portal.scss";
 
 export default function Portal(props) {
 
-  
-  //console.log("Portal: props "+props);
-
-
-  /*
-   * THE REASON NONE OF THE ATTEMPTS BELOW WORKED WAS THAT YOU HAVE TO WAIT FOR THE PARENT
-   * TO BE RENDERED - MOVING ANY OF THESE INTO COMPONENTDIDMOUNT SHOULD WORK - I USED THE 
-   * CALLBACK METHOD BUT OTHERS MAY ALSO BE OK. I QUITE LIKE THE CALLBACK ONE AS IT MAKES 
-   * THE PORTAL ENCAPSULATED AND ALLOWS THE CALLER TO DICTATE THE ANCHOR POINT. DIRECT PROP
-   * CONTAINING ANCHOR WOULD ALSO BE OK FOR THAT. 
-   */
-  //const portalRoot = document.getElementById("portal"); WORKS - BUT ALWAYS ROOTED IN INDEX.HTML
-  //const portalRoot = document.getElementById("rex-portal"); DOES NOT WORK
-  //const portalRoot = props.anchor.current; ALWAYS NULL
-  // FORWARD REFS LOOK UGLY AND AM NOT SURE THEY DO WHAT I INTEND
-  // const portalRoot = props.anchorCB().current; NOPE - CURRENT IS NULL
-
 
   const [newElement, setNewElement] = useState(null);
   
 
-  // Emulate componentDidMount - to append the wrapper element
+  /*
+   * Emulate componentDidMount - to append the wrapper
+   */
   const componentDidMount = () => {
-    const portalRoot = props.anchorCB().current; 
-    console.log("Portal: did mount - newElement is "+newElement);
-    
+    const portalRoot = props.anchorCB().current;
     let locElement = document.createElement("div");
-    //locElement.className = "portal-div";    
     portalRoot.appendChild(locElement);
-    setNewElement(locElement); 
+    setNewElement(locElement);
 
     return () => {
       // componentWillUnmount... to remove the wrapper element
-      console.log("Portal: will unmount - newElement is "+newElement+" locElement is "+locElement);
       portalRoot.removeChild(locElement);
-      setNewElement(null); 
+      setNewElement(null);
     }
   };
   useEffect (componentDidMount ,[]);
@@ -58,8 +39,7 @@ export default function Portal(props) {
   let content = (
     <div className="portal-backdrop">
       <div className="portal-div">
-        {props.children}      
-      
+        {props.children}
       </div>
     </div>
   );
@@ -73,14 +53,10 @@ export default function Portal(props) {
     }
   }
   return null;
-
-
 }
 
 Portal.propTypes = {
   children: PropTypes.node,
   show : PropTypes.bool,
-  anchorCB: PropTypes.func.isRequired, 
-  //cancelCallback : PropTypes.func.isRequired, 
-  //submitCallback : PropTypes.func.isRequired
+  anchorCB: PropTypes.func.isRequired
 };
