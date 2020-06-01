722aa1f459acdbb6fd5c6ce9aa7a66c82db62b3e
Ilya Ksenofontov <fonto.trg@gmail.com>
2020-06-01 13:32:15 +0300

    # Change for rerun DCO
    Signed-off-by: immmus <fonto.trg@gmail.com>

--- open-metadata-implementation/access-services/subject-area/subject-area-spring/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/server/spring/SubjectAreaProjectRESTResource.java
+++ open-metadata-implementation/access-services/subject-area/subject-area-spring/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/server/spring/SubjectAreaProjectRESTResource.java
@@ -29,9 +29,7 @@ public class SubjectAreaProjectRESTResource {
     /**
      * Default constructor
      */
-    public SubjectAreaProjectRESTResource() {
-
-    }
+    public SubjectAreaProjectRESTResource() { }
 
     /**
      * Create a Project.
