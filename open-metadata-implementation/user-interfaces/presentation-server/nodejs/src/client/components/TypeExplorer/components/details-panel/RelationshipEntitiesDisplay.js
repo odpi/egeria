4395f4327c8d2cec30893538b64363075f9e31d5
Graham Wallis <graham_wallis@uk.ibm.com>
2020-06-15 14:59:40 +0100

    Make relastionship ends linkable and link to entity type definitions
    Signed-off-by: Graham Wallis <graham_wallis@uk.ibm.com>

--- open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/TypeExplorer/components/details-panel/RelationshipEntitiesDisplay.js
+++ open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/TypeExplorer/components/details-panel/RelationshipEntitiesDisplay.js
@@ -1,25 +1,36 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright Contributors to the ODPi Egeria project. */
 
-import React                  from "react";
+import React, { useContext }         from "react";
 
-import PropTypes              from "prop-types";
+import PropTypes                     from "prop-types";
 
-
+import { FocusContext }              from "../../contexts/FocusContext";
 
 import "./details-panel.scss";
 
 
+
 export default function RelationshipEntitiesDisplay(props) {
 
-  const explorer           = props.expl;
+  const focusContext    = useContext(FocusContext);
+
+  const explorer        = props.expl;
 
   
-  
+  const entityLinkHandler = (evt) => {
+    const typeName = evt.target.id;
+    focusContext.typeSelected("Entity",typeName);
+  };
+
   const formatEnd = (endDef) => {
     const endEntry = (
       <ul className="details-sublist">
-        <li className="details-sublist-item"> Entity Type : {endDef.entityType.name} </li>
+        <li className="details-sublist-item"> Entity Type : <button className="linkable"
+            id={endDef.entityType.name} onClick={entityLinkHandler}>
+            {endDef.entityType.name}
+          </button>
+        </li>
         <li className="details-sublist-item"> Cardinality : {endDef.attributeCardinality} </li>
         <li className="details-sublist-item"> Attribute Name : {endDef.attributeName} </li>
       </ul>
