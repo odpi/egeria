a170e1bfeb929c68f2afdd2f6ff80e5e95d348db
Bogdan Sava <bogdan.sava@gmail.com>
2020-06-16 11:45:07 +0300

    javadoc
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/java/org/odpi/openmetadata/userinterface/uichassis/springboot/service/AssetCatalogOMASService.java
+++ open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/java/org/odpi/openmetadata/userinterface/uichassis/springboot/service/AssetCatalogOMASService.java
@@ -130,6 +130,15 @@ public class AssetCatalogOMASService {
         }
     }
 
+    /**
+     * Fetch asset's context
+     *
+     * @param userId userId of the user triggering the request
+     * @param assetId the id of the asset  (usually String representation of a GUID )
+     * @return the asset context
+     * @throws PropertyServerException   there is a problem retrieving information from the property server
+     * @throws InvalidParameterException there is a problem with the parameters
+     */
     public AssetElements getAssetContext(String userId, String assetId, String assetType)
             throws PropertyServerException, org.odpi.openmetadata.frameworks.connectors.ffdc.InvalidParameterException {
         try {
@@ -140,7 +149,14 @@ public class AssetCatalogOMASService {
         }
     }
 
-
+    /**
+     * Asset catalog supported types - a list of the types that are returned by the catalog
+     *
+     * @param userId userId of the user triggering the request
+     * @return the list of supported types
+     * @throws PropertyServerException   there is a problem retrieving information from the property server
+     * @throws InvalidParameterException there is a problem with the parameters
+     */
     public List<Type> getSupportedTypes(String userId) throws PropertyServerException,
             org.odpi.openmetadata.frameworks.connectors.ffdc.InvalidParameterException {
         try {
