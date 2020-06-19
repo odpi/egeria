1de56f09c2d02f3b4ad0527d0cd31a7e6a65b103
Graham Wallis <graham_wallis@uk.ibm.com>
2020-06-19 11:59:15 +0100

    Switch the relationship ends for Asset and SchemaType in method saveAssociatedSchemaType
    Signed-off-by: Graham Wallis <graham_wallis@uk.ibm.com>

--- open-metadata-implementation/common-services/ocf-metadata-management/ocf-metadata-handlers/src/main/java/org/odpi/openmetadata/commonservices/ocf/metadatamanagement/handlers/AssetHandler.java
+++ open-metadata-implementation/common-services/ocf-metadata-management/ocf-metadata-handlers/src/main/java/org/odpi/openmetadata/commonservices/ocf/metadatamanagement/handlers/AssetHandler.java
@@ -682,8 +682,8 @@ public class AssetHandler
                 {
                     repositoryHandler.createRelationship(userId,
                                                          AssetMapper.ASSET_TO_SCHEMA_TYPE_TYPE_GUID,
-                                                         schemaTypeGUID,
                                                          asset.getGUID(),
+                                                         schemaTypeGUID,
                                                          null,
                                                          methodName);
                 }
