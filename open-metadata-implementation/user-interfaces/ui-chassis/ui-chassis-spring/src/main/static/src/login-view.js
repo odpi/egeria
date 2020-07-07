c03ef95894f796157ce50fa810c1d3ddd53ea2db
Bogdan Sava <bogdan.sava@gmail.com>
2020-07-08 02:13:07 +0300

    remove duplicate line
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/static/src/login-view.js
+++ open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/static/src/login-view.js
@@ -43,7 +43,6 @@ class LoginView extends PolymerElement {
             box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
             max-width: 600px;
             width: 100%;
-            width: 100%;
         }
         
         #logo{
