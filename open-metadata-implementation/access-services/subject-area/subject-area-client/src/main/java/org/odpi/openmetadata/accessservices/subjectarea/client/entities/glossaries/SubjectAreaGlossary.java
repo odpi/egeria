aaf784e71b4c08c84ebf3844a44463cfee6d9294
Ilya Ksenofontov <fonto.trg@gmail.com>
2020-07-08 13:29:26 +0300

    # Added javadocs for Subject Area entity interfaces
    Signed-off-by: immmus <fonto.trg@gmail.com>

--- open-metadata-implementation/access-services/subject-area/subject-area-client/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/client/entities/glossaries/SubjectAreaGlossary.java
+++ open-metadata-implementation/access-services/subject-area/subject-area-client/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/client/entities/glossaries/SubjectAreaGlossary.java
@@ -2,9 +2,18 @@
 /* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.accessservices.subjectarea.client.entities.glossaries;
 
+import org.odpi.openmetadata.accessservices.subjectarea.client.SubjectAreaClient;
 import org.odpi.openmetadata.accessservices.subjectarea.client.SubjectAreaEntityClient;
 import org.odpi.openmetadata.accessservices.subjectarea.properties.objects.glossary.Glossary;
 
+/**
+ * The SubjectAreaDefinition Open Metadata Access Service (OMAS) API for glossaries.
+ */
 public interface SubjectAreaGlossary {
+
+   /**
+    * @return {@link SubjectAreaEntityClient} for client calls(described in {@link SubjectAreaClient})
+    * when working with Glossary objects
+    */
    SubjectAreaEntityClient<Glossary> glossary();
 }
