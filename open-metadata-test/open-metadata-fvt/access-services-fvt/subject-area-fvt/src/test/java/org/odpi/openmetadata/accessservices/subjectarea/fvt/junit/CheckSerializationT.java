73447fa3db2cab6a681a78c979148e96ecf03fc0
Ilya Ksenofontov <fonto.trg@gmail.com>
2020-08-13 20:21:58 +0300

    # Added license comments
    Signed-off-by: immmus <fonto.trg@gmail.com>

--- open-metadata-test/open-metadata-fvt/access-services-fvt/subject-area-fvt/src/test/java/org/odpi/openmetadata/accessservices/subjectarea/fvt/junit/CheckSerializationT.java
+++ open-metadata-test/open-metadata-fvt/access-services-fvt/subject-area-fvt/src/test/java/org/odpi/openmetadata/accessservices/subjectarea/fvt/junit/CheckSerializationT.java
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.accessservices.subjectarea.fvt.junit;
 
 import org.junit.jupiter.params.ParameterizedTest;
