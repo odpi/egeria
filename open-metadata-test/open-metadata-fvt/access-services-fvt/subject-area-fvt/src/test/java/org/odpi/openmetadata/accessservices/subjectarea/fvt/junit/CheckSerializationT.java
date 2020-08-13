622c67c2886b1d28042e89b51e3c6f3621b5883f
Ilya Ksenofontov <fonto.trg@gmail.com>
2020-08-13 20:19:06 +0300

    # Added SubjectArea Fvt for check serialization children of Line.
    Signed-off-by: immmus <fonto.trg@gmail.com>

---
+++ open-metadata-test/open-metadata-fvt/access-services-fvt/subject-area-fvt/src/test/java/org/odpi/openmetadata/accessservices/subjectarea/fvt/junit/CheckSerializationT.java
@@ -0,0 +1,15 @@
+package org.odpi.openmetadata.accessservices.subjectarea.fvt.junit;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.odpi.openmetadata.accessservices.subjectarea.fvt.CheckSerializationFVT;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class CheckSerializationT {
+    @ParameterizedTest
+    @ValueSource(strings = {"serverinmem","servergraph"})
+    public void testRelationships(String server) {
+        assertDoesNotThrow(() -> CheckSerializationFVT.runIt("https://localhost:10443", server, "garygeeke"));
+    }
+}
