a1530c1cd71d0836defcb65a8b2eb5de3733923c
Daniela Otelea <daniela-valentina.otelea@ing.com>
2020-08-26 14:11:57 +0300

    AL OMAS - Audit Log Improvements remove depricated implementation
    Signed-off-by: Daniela Otelea <daniela-valentina.otelea@ing.com>
    Signed-off-by: JMadsen <jacob.madsen@ibm.com>

--- open-metadata-implementation/access-services/asset-lineage/asset-lineage-api/src/main/java/org/odpi/openmetadata/accessservices/assetlineage/ffdc/exception/AssetLineageException.java
+++ open-metadata-implementation/access-services/asset-lineage/asset-lineage-api/src/main/java/org/odpi/openmetadata/accessservices/assetlineage/ffdc/exception/AssetLineageException.java
@@ -120,49 +120,4 @@ public class AssetLineageException extends OCFCheckedExceptionBase {
                 caughtErrorClassName,
                 relatedProperties);
     }
-
-
-    /**
-     * This is the typical constructor used for creating an exception.
-     *
-     * @param httpCode          http response code to use if this exception flows over a rest call
-     * @param className         name of class reporting error
-     * @param actionDescription description of function it was performing when error detected
-     * @param errorMessage      description of error
-     * @param systemAction      actions of the system as a result of the error
-     * @param userAction        instructions for correcting the error
-     */
-    @Deprecated
-    public AssetLineageException(int httpCode,
-                                 String className,
-                                 String actionDescription,
-                                 String errorMessage,
-                                 String systemAction,
-                                 String userAction) {
-        super(httpCode, className, actionDescription, errorMessage, systemAction, userAction);
-    }
-
-
-    /**
-     * This is the  constructor used for creating an exception
-     * that resulted from a previous error.
-     *
-     * @param httpCode          http response code to use if this exception flows over a rest call
-     * @param className         name of class reporting error
-     * @param actionDescription description of function it was performing when error detected
-     * @param errorMessage      description of error
-     * @param systemAction      actions of the system as a result of the error
-     * @param userAction        instructions for correcting the error
-     * @param caughtError       the error that resulted in this exception.
-     */
-    @Deprecated
-    public AssetLineageException(int httpCode,
-                                 String className,
-                                 String actionDescription,
-                                 String errorMessage,
-                                 String systemAction,
-                                 String userAction,
-                                 Throwable caughtError) {
-        super(httpCode, className, actionDescription, errorMessage, systemAction, userAction, caughtError);
-    }
 }
