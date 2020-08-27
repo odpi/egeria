9499dc0559f82ff9a55d65ccfd7a4846b1b02ae5
Liviu Popa <liviu-constantin.popa@ing.com>
2020-08-27 08:21:21 +0300

    Automated graph validation
    Signed-off-by: Liviu Popa <liviu-constantin.popa@ing.com>

--- open-metadata-implementation/adapters/open-connectors/governance-daemon-connectors/open-lineage-connectors/open-lineage-janus-connector/src/test/java/org/odpi/openmetadata/openconnectors/governancedaemonconnectors/openlineageconnectors/janusconnector/lineagegraph/OpenLineageGraphValidation.java
+++ open-metadata-implementation/adapters/open-connectors/governance-daemon-connectors/open-lineage-connectors/open-lineage-janus-connector/src/test/java/org/odpi/openmetadata/openconnectors/governancedaemonconnectors/openlineageconnectors/janusconnector/lineagegraph/OpenLineageGraphValidation.java
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.openconnectors.governancedaemonconnectors.openlineageconnectors.janusconnector.lineagegraph;
 
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
