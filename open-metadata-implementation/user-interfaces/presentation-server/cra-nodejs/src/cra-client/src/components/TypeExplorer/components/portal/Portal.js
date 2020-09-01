d4000feec88fcb4f609b13c16866bdc460d09f97
JMadsen <jacob.madsen@ibm.com>
2020-09-01 10:53:59 -0500

    new directory for cra refactoring
    Signed-off-by: JMadsen <jacob.madsen@ibm.com>

---
+++ open-metadata-implementation/user-interfaces/presentation-server/cra-nodejs/src/cra-client/src/components/TypeExplorer/components/portal/Portal.js
@@ -0,0 +1,64 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+
+
+import React, { useEffect, useState }     from "react";
+
+import ReactDOM                           from "react-dom";
+
+import PropTypes                          from "prop-types";
+
+
+import "./portal.scss";
+
+
+
+export default function Portal(props) {
+
+  const [newElement, setNewElement] = useState(null);
+  
+  /*
+   * Emulate componentDidMount - to append the wrapper
+   */
+  const componentDidMount = () => {
+    const portalRoot = props.anchorCB().current;
+    let locElement = document.createElement("div");
+    portalRoot.appendChild(locElement);
+    setNewElement(locElement);
+
+    return () => {
+      /*
+       * Emulate componentWillUnmount... to remove the wrapper
+       */
+      portalRoot.removeChild(locElement);
+      setNewElement(null);
+    }
+  };
+  useEffect (componentDidMount ,[]);
+
+  let content = (
+    <div className="portal-backdrop">
+      <div className="portal-div">
+        {props.children}
+      </div>
+    </div>
+  );
+
+  /*
+   * Render the Portal's children in the wrapper element...  
+   */
+  if (props.show) {
+    if (newElement) {
+      return (
+        ReactDOM.createPortal(content, newElement)
+      );
+    }
+  }
+  return null;
+}
+
+Portal.propTypes = {
+  children: PropTypes.node,
+  show : PropTypes.bool,
+  anchorCB: PropTypes.func.isRequired
+};
