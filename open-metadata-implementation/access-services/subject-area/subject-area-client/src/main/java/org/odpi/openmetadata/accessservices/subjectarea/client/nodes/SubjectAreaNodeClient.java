ec1ca8d73c1ec9dac3670cccdefcfd502f965ccb
Ilya Ksenofontov <fonto.trg@gmail.com>
2020-09-18 16:06:30 +0300

    # removed comment
    Signed-off-by: immmus <fonto.trg@gmail.com>

--- open-metadata-implementation/access-services/subject-area/subject-area-client/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/client/nodes/SubjectAreaNodeClient.java
+++ open-metadata-implementation/access-services/subject-area/subject-area-client/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/client/nodes/SubjectAreaNodeClient.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Target;
 
 /**
  * Annotating client class for working with Node objects.
- * These classes are scanned by {@link DefaultSubjectAreaNodeClients}
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
