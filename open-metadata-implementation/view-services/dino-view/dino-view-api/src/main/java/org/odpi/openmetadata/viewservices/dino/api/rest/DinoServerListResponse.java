686cee907d75f6cf1e2d4efbec99a32a69129f4d
Graham Wallis <graham_wallis@uk.ibm.com>
2020-09-23 10:18:01 +0100

    Improvements to comments
    Signed-off-by: Graham Wallis <graham_wallis@uk.ibm.com>

--- open-metadata-implementation/view-services/dino-view/dino-view-api/src/main/java/org/odpi/openmetadata/viewservices/dino/api/rest/DinoServerListResponse.java
+++ open-metadata-implementation/view-services/dino-view/dino-view-api/src/main/java/org/odpi/openmetadata/viewservices/dino/api/rest/DinoServerListResponse.java
@@ -15,17 +15,19 @@ import java.util.List;
 public class DinoServerListResponse extends DinoViewOMVSAPIResponse {
 
 
-    // A ServerList response is list of ServerStatus objects.
-    // Each server is identified by its server instance name to the REST client. The platformRootU~RL is not sent back as the client cannot use it to request
-    // further details about the server (instance). The return of the server list is in response to the platform services call to list active or known servers
-    // and is intended to convey much/any detail about each server instance. It must convey enough to allow the server and server instance to be identified so
-    // that the server instance is recognizable in the SevrerSelector (list of pre-defined server resource endpoints) and can be displayed in the diagram - where
-    // it can be selected to request further (full) details.
-    // If the server is not found in the configured list of server instances - the ServerStatus.serverInstanceName field is set to null.
-    //
-    // It is desirable to include an indication of whether the server is active or not. The status is depicted by the isActive boolean in the ServerStatus object.
-    // The RootURL (and other details) is not included. These stay in the VS; the RootURL is resolved by the VS so it avoids exposing an interface in which the
-    // client (any client) of the VS REST API can specify the RootURL.
+    /*
+     * A ServerList response is list of ServerStatus objects.
+     * Each server is identified by its server instance name to the REST client. The platformRootU~RL is not sent back as the client cannot use it to request
+     * further details about the server (instance). The return of the server list is in response to the platform services call to list active or known servers
+     * and is intended to convey much/any detail about each server instance. It must convey enough to allow the server and server instance to be identified so
+     * that the server instance is recognizable in the SevrerSelector (list of pre-defined server resource endpoints) and can be displayed in the diagram - where
+     * it can be selected to request further (full) details.
+     * If the server is not found in the configured list of server instances - the ServerStatus.serverInstanceName field is set to null.
+     *
+     * It is desirable to include an indication of whether the server is active or not. The status is depicted by the isActive boolean in the ServerStatus object.
+     * The RootURL (and other details) is not included. These stay in the VS; the RootURL is resolved by the VS so it avoids exposing an interface in which the
+     * client (any client) of the VS REST API can specify the RootURL.
+     */
     private List<DinoServerInstance> serverList = null;
 
     /**
