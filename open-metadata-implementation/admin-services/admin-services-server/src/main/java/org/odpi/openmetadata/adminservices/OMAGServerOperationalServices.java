33dd7218df708c68c355f39ad3db6a6d0b919c73
Ljupcho Palashevski <ljupcho.palashevski@ing.com>
2020-09-25 13:06:30 +0200

    Fix DE event client/connector issue caused by kafka consumer group config.
    Signed-off-by: Ljupcho Palashevski <ljupcho.palashevski@ing.com>

--- open-metadata-implementation/admin-services/admin-services-server/src/main/java/org/odpi/openmetadata/adminservices/OMAGServerOperationalServices.java
+++ open-metadata-implementation/admin-services/admin-services-server/src/main/java/org/odpi/openmetadata/adminservices/OMAGServerOperationalServices.java
@@ -1029,6 +1029,7 @@ public class OMAGServerOperationalServices
         {
             DataEngineProxyOperationalServices operationalDataEngineProxyServices
                     = new DataEngineProxyOperationalServices(configuration.getLocalServerName(),
+                                                             configuration.getLocalServerId(),
                                                              configuration.getLocalServerUserId(),
                                                              configuration.getLocalServerPassword());
 
