2a2e788c987938b13e2df7e4f04d37ab41a30085
Mandy Chessell <mandy_chessell@uk.ibm.com>
2020-09-29 16:12:33 +0100

    Remove OCF handlers dependency (#3703)
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/access-services/subject-area/subject-area-server/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/server/services/SubjectAreaServicesInstance.java
+++ open-metadata-implementation/access-services/subject-area/subject-area-server/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/server/services/SubjectAreaServicesInstance.java
@@ -3,10 +3,11 @@
 package org.odpi.openmetadata.accessservices.subjectarea.server.services;
 
 
+import org.odpi.openmetadata.accessservices.subjectarea.ffdc.SubjectAreaErrorCode;
 import org.odpi.openmetadata.accessservices.subjectarea.handlers.*;
 import org.odpi.openmetadata.accessservices.subjectarea.utilities.OMRSAPIHelper;
 import org.odpi.openmetadata.adminservices.configuration.registration.AccessServiceDescription;
-import org.odpi.openmetadata.commonservices.multitenant.OCFOMASServiceInstance;
+import org.odpi.openmetadata.commonservices.multitenant.OMASServiceInstance;
 import org.odpi.openmetadata.commonservices.multitenant.ffdc.exceptions.NewInstanceException;
 import org.odpi.openmetadata.frameworks.auditlog.AuditLog;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryConnector;
@@ -16,7 +17,7 @@ import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollec
  * It is also responsible for registering itself in the instance map.
  */
 
-public class SubjectAreaServicesInstance extends OCFOMASServiceInstance
+public class SubjectAreaServicesInstance extends OMASServiceInstance
 {
     private static AccessServiceDescription myDescription = AccessServiceDescription.SUBJECT_AREA_OMAS;
     private OMRSAPIHelper oMRSAPIHelper;
@@ -106,9 +107,9 @@ public class SubjectAreaServicesInstance extends OCFOMASServiceInstance
         }
         else
         {
-//            throw new NewInstanceException(SubjectAreaErrorCode.OMRS_NOT_INITIALIZED.getMessageDefinition(methodName),
-//                                           this.getClass().getName(),
-//                                           methodName);
+           throw new NewInstanceException(SubjectAreaErrorCode.OMRS_NOT_INITIALIZED.getMessageDefinition(methodName),
+                                           this.getClass().getName(),
+                                           methodName);
 
         }
     }
