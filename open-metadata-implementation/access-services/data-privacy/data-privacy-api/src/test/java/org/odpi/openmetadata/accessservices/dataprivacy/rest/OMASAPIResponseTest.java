4ea5556e7b7cf0a1b51d3b11a17167b937fd8e35
Mandy Chessell <mandy_chessell@uk.ibm.com>
2020-09-29 15:51:23 +0100

    Remove OCF handlers dependency (#3703)
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/access-services/data-privacy/data-privacy-api/src/test/java/org/odpi/openmetadata/accessservices/dataprivacy/rest/OMASAPIResponseTest.java
+++ open-metadata-implementation/access-services/data-privacy/data-privacy-api/src/test/java/org/odpi/openmetadata/accessservices/dataprivacy/rest/OMASAPIResponseTest.java
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright Contributors to the ODPi Egeria project. */
-package org.odpi.openmetadata.accessservices.dataprivacy.responses;
+package org.odpi.openmetadata.accessservices.dataprivacy.rest;
 
 import org.testng.annotations.Test;
 
