a8f8dc0492d1bffab79bf312ff8e29114a31a35e
Mandy Chessell <mandy_chessell@uk.ibm.com>
2020-09-29 16:01:19 +0100

    Remove OCF handlers dependency (#3703)
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/access-services/glossary-view/glossary-view-server/src/main/java/org/odpi/openmetadata/accessservices/glossaryview/server/service/GlossaryViewInstanceHandler.java
+++ open-metadata-implementation/access-services/glossary-view/glossary-view-server/src/main/java/org/odpi/openmetadata/accessservices/glossaryview/server/service/GlossaryViewInstanceHandler.java
@@ -4,12 +4,12 @@
 package org.odpi.openmetadata.accessservices.glossaryview.server.service;
 
 import org.odpi.openmetadata.adminservices.configuration.registration.AccessServiceDescription;
-import org.odpi.openmetadata.commonservices.multitenant.OCFOMASServiceInstanceHandler;
+import org.odpi.openmetadata.commonservices.multitenant.OMASServiceInstanceHandler;
 
 /**
  * GlossaryViewInstanceHandler provides the bridge between the REST call and the server instance.
  */
-public class GlossaryViewInstanceHandler extends OCFOMASServiceInstanceHandler {
+public class GlossaryViewInstanceHandler extends OMASServiceInstanceHandler {
 
     public GlossaryViewInstanceHandler() {
         super(AccessServiceDescription.GLOSSARY_VIEW_OMAS.getAccessServiceFullName());
