e21c59ce53afa661d6cb0e61769308080031a33e
David Radley <david_radley@uk.ibm.com>
2020-10-05 11:02:08 +0100

    git3738 display multiple term response in  new page
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/GlossaryAuthor/components/QuickTerms.js
+++ open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/GlossaryAuthor/components/QuickTerms.js
@@ -8,14 +8,15 @@ import { issueRestCreate } from "./RestCaller";
 import { useParams } from "react-router-dom";
 
 export default function QuickTerms() {
-  const currentNodeType = getNodeType("glossary");
+  const glossaryNodeType = getNodeType("glossary");
+  const termNodeType = getNodeType("term");
   const [terms, setTerms] = useState([]);
   const [termResponses, setTermResponses] = useState([]);
 
   const url = getUrl();
   function getUrl() {
     const { guid } = useParams();
-    return currentNodeType.url + "/" + guid + "/terms";
+    return glossaryNodeType.url + "/" + guid + "/terms";
   }
   const validateForm = () => {
     return true;
@@ -42,16 +43,79 @@ export default function QuickTerms() {
   };
   const onSuccessfulCreate = (json) => {
     setTermResponses(json.result);
-  }
+  };
   //return next term to create
   function onErrorCreate(msg) {
     //TODO;
   }
 
-  const getGlossaryGuid = () => {
-    const result = window.location.pathname.split("/");
-    return result[result.length - 2];
+  // properties that will be displayed be default for a node
+  const mainProperties = [
+    {
+      key: "name",
+      text: "Name",
+    },
+    {
+      key: "description",
+      text: "Description",
+    },
+    {
+      key: "qualifiedName",
+      text: "Qualified Name",
+    },
+  ];
+
+  const [headerData, setHeaderData] = useState(mainProperties);
+  const additionalProperties = calculateAdditionalProperties();
+  let selectedAdditionalProperties = [];
+
+  // calculate the results table header - this will be the default columns plus any additional coliumns the user has specified
+  function calculateHeaderData() {
+    let allProperties = mainProperties;
+    if (
+      selectedAdditionalProperties !== undefined &&
+      selectedAdditionalProperties &&
+      selectedAdditionalProperties.length > 0
+    ) {
+      allProperties = mainProperties.concat(selectedAdditionalProperties);
+    }
+    console.log(allProperties);
+    setHeaderData(allProperties);
+  }
+
+  // Additional attributes can be selected so more columns can be shown
+  // the additional attriniutes are in selectedAdditionalProperties
+  const onAdditionalAttributesChanged = (items) => {
+    console.log("onAdditionalAttributesChanged");
+    console.log(items.selectedItems);
+    selectedAdditionalProperties = [];
+    const selectedItems = items.selectedItems;
+    for (let i = 0; i < selectedItems.length; i++) {
+      let item = {};
+      item.key = selectedItems[i].id;
+      item.text = selectedItems[i].text;
+      selectedAdditionalProperties.push(item);
+    }
+    // render the table by recalculating the header state based on the new values
+    calculateHeaderData();
   };
+  // calculate the columns from the main attributes and the additional attributes.
+  function calculateAdditionalProperties() {
+    let items = [];
+    termNodeType.attributes.map(function (attribute) {
+      if (
+        attribute.key != "name" &&
+        attribute.key != "qualifiedName" &&
+        attribute.key != "description"
+      ) {
+        let item = {};
+        item.id = attribute.key;
+        item.text = attribute.label;
+        items.push(item);
+      }
+    });
+    return items;
+  }
 
   function setTermName(index, name) {
     let workingTerms = terms;
@@ -66,48 +130,59 @@ export default function QuickTerms() {
 
   return (
     <div>
-      <h3>Quickly add terms.</h3>
-
-      <Add32 kind="primary" onClick={() => onAdd()} />
-      <Form>
-        <FormGroup>
-          {terms.map((item, index) => {
+      {termResponses.length == 0 && (
+        <div>
+          <h3>Quickly add terms.</h3>
+          <Add32 kind="primary" onClick={() => onAdd()} />
+          <Form>
+            <FormGroup>
+              {terms.map((item, index) => {
+                return (
+                  <div className="bx--form-item" key={index}>
+                    <div className="row-container">
+                      <TextInput
+                        type="text"
+                        placeholder="Term name"
+                        defaultValue={item.name}
+                        onChange={(e) => {
+                          setTermName(index, e.target.value);
+                        }}
+                      />
+                      <TextInput
+                        type="text"
+                        placeholder="Term description"
+                        defaultValue={item.description}
+                        onChange={(e) => {
+                          setTermDescription(index, e.target.value);
+                        }}
+                      />
+                      <div>{item.result}</div>
+                    </div>
+                  </div>
+                );
+              })}
+              <Button
+                type="submit"
+                onClick={onSubmit}
+                disabled={!validateForm()}
+              >
+                Create Terms on Server
+              </Button>
+            </FormGroup>
+          </Form>
+        </div>
+      )}
+      {termResponses.length > 0 && (
+        <div>
+          {termResponses.map((item, index) => {
             return (
-              <div className="bx--form-item" key={index}>
-                <div className="row-container">
-                  <TextInput
-                    type="text"
-                    placeholder="Term name"
-                    defaultValue={item.name}
-                    onChange={(e) => {
-                      setTermName(index, e.target.value);
-                    }}
-                  />
-                  <TextInput
-                    type="text"
-                    placeholder="Term description"
-                    defaultValue={item.description}
-                    onChange={(e) => {
-                      setTermDescription(index, e.target.value);
-                    }}
-                  />
-                  <div>{item.result}</div>
-                </div>
+              <div key={index}>
+                Result {index} {item.result.name}  {item.relatedHTTPCode}
               </div>
             );
           })}
-         
-          {termResponses.map((item, index ) => {
-            return (
-              <div key={index}>Result {index} {item.result.name}{item.relatedHTTPCode}</div>
-            );
-          })}
-         
-          <Button type="submit" onClick={onSubmit} disabled={!validateForm()}>
-            Create Terms on Server
-          </Button>
-        </FormGroup>
-      </Form>
+        </div>
+      )}
     </div>
   );
 }
