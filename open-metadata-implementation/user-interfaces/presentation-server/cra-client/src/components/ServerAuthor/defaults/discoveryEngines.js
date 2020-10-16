02eff4c8ee23b7e46e0bf7a1e0f8f820b8f1ed1a
David Kneisly <dkneisly@gmail.com>
2020-10-15 17:58:34 -0700

    Added server configuration steps for several server categories
    Signed-off-by: David Kneisly <dkneisly@gmail.com>

---
+++ open-metadata-implementation/user-interfaces/presentation-server/cra-client/src/components/ServerAuthor/defaults/discoveryEngines.js
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+
+const discoveryEngines = [
+  {
+    "serviceName": "Sample Discovery Engine",
+    "serviceURLMarker": "sample",
+    "serviceDescription": "Not a real discovery engine.",
+    "serviceWiki": ""
+  }
+]
+
+export default discoveryEngines;
