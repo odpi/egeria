6189c10698de0258e5fe6bfd47c917feee8e494f
David Radley <david_radley@uk.ibm.com>
2020-10-19 13:22:49 +0100

    git3907 amend the menu title in presentation server UI
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/Frame.js
+++ open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/Frame.js
@@ -93,7 +93,7 @@ export default function Frame() {
                       Glossary Author
                     </SideNavLink>
                   </SideNavMenu>
-                  <SideNavMenu title="Ecosystem Tools" defaultExpanded="true">
+                  <SideNavMenu title="Ecosystem " defaultExpanded="true">
                     <SideNavLink element={Link} to={rexUrl}>
                       Repository Explorer
                     </SideNavLink>
