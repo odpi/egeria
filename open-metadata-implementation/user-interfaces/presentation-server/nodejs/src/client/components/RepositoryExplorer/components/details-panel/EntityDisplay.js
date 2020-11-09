fc20f02f7beacbb9b52a4a083e9e424bbc445151
Graham Wallis <graham_wallis@uk.ibm.com>
2020-11-09 17:12:20 +0000

    EntityDisplay now renders retrieval traceability
    Signed-off-by: Graham Wallis <graham_wallis@uk.ibm.com>

--- open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/RepositoryExplorer/components/details-panel/EntityDisplay.js
+++ open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/RepositoryExplorer/components/details-panel/EntityDisplay.js
@@ -16,6 +16,28 @@ import "./details-panel.scss";
 
 export default function EntityDisplay(props) {
 
+
+  /*
+   * Handler for flopping a collapsible
+   */
+  const flipSection = (evt) => {
+    /*
+     * Use currentTarget (not target) - because we need to operate relative to the button,
+     * which is where the handler is defined, in order for the content section to be the sibling.
+     */
+    const element = evt.currentTarget;
+    element.classList.toggle("active");
+    const content = element.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    }
+    else {
+      content.style.display = "block";
+    }
+  };
+
+  // TODO - alignment
+
     const expEntity    = props.expEntity;
 
     const entity       = expEntity.entityDetail;
@@ -23,11 +45,24 @@ export default function EntityDisplay(props) {
 
     const label        = entityDigest.label;
     const gen          = entityDigest.gen;
+    const provenance   = entityDigest.provenance;
+
+    const genEnterprise    = props.enterprise;  // this is the option of the gen of original retrieval
+    // If 'provenance' (from the digest) shows that this entity was retrievd using enterprise scope,
+    // then even if the genEnterprise is false, we should display the entity as having been retrieved
+    // by an enterprise scope operation. This is most probably because the entity was originally
+    // retrieved by a (local) retrieval of a relationship, which had this entity as a proxy. Rex has
+    // gone after the full EntityDetail so that the user can also see the (non-unique) properties.
+    // It may have done this using an enterprise operation so treat the displayed entity as an
+    // enterprise scoped one.
+    const enterprise = genEnterprise || (provenance === "ent");
+
+    const serverName   = expEntity.serverName;
+    const platformName = expEntity.platformName;
 
     return (
       <div className="instance-details-container">
         <div className="instance-details-item">Entity : {label}</div>
-        <div className="instance-details-item">Added in gen : {gen}</div>        
         <div className="instance-details-item">Type : {entity.type.typeDefName}</div>       
         <div className="instance-details-item">Version : {entity.version}</div>
         <div className="instance-details-item">Status : <InstanceStatusDisplay inst={entity} /></div>
@@ -42,7 +77,22 @@ export default function EntityDisplay(props) {
             <li className="details-sublist-item">metadataCollectionId : {entity.metadataCollectionId}</li>
           </ul>
         </div>
-        <div className="instance-details-item">OMRS Control Properties :
+
+        <button className="collapsible-non-bold" id="querySummary" onClick={flipSection}> Rex Retrieval : </button>
+        <div className="content">
+          <div className="instance-details-item">Added in gen : {gen}</div>
+          <div className="instance-details-item">Retrieved from server : {serverName} on platform : {platformName}</div>
+          {enterprise &&
+            <div className="instance-details-item">Operation was at enterprise scope</div>
+          }
+          {!enterprise &&
+            <div className="instance-details-item">Entity is a {provenance} instance</div>
+          }
+        </div>
+
+
+        <button className="collapsible-non-bold" id="controlProps" onClick={flipSection}> OMRS Control Properties : </button>
+        <div className="content">
           <ul className="details-sublist">
             <li className="details-sublist-item">createdBy : {entity.createdBy}</li>
             <li className="details-sublist-item">createTime : {entity.createTime}</li>
@@ -58,7 +108,6 @@ export default function EntityDisplay(props) {
             <li className="details-sublist-item">instanceProvenanceType : {entity.instanceProvenanceType ? entity.instanceProvenanceType : "empty"}</li> 
             <li className="details-sublist-item">replicatedBy : {entity.replicatedBy ? entity.replicatedBy : "empty"}</li>
           </ul>
-
         </div>
 
       </div>
@@ -66,6 +115,7 @@ export default function EntityDisplay(props) {
   }
 
   EntityDisplay.propTypes = {
-    expEntity: PropTypes.object
+    expEntity: PropTypes.object,
+    enterprise: PropTypes.bool
   };
   
