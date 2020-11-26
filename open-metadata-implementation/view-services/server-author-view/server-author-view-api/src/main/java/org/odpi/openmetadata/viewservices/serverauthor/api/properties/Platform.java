6711893cef1138230bec4eaed35d3f2733c4c5d3
David Radley <david_radley@uk.ibm.com>
2020-11-26 12:08:27 +0000

    #4134 add hash equals and toString to new beans
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- open-metadata-implementation/view-services/server-author-view/server-author-view-api/src/main/java/org/odpi/openmetadata/viewservices/serverauthor/api/properties/Platform.java
+++ open-metadata-implementation/view-services/server-author-view/server-author-view-api/src/main/java/org/odpi/openmetadata/viewservices/serverauthor/api/properties/Platform.java
@@ -8,14 +8,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY;
 
-@JsonAutoDetect(getterVisibility=PUBLIC_ONLY, setterVisibility=PUBLIC_ONLY, fieldVisibility=NONE)
+@JsonAutoDetect(getterVisibility = PUBLIC_ONLY, setterVisibility = PUBLIC_ONLY, fieldVisibility = NONE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 /**
  * A summary of the platform for the Server Author View. It contains a name a description of the platform, a status and
  * the associated servers if we can get them. It is a minimal representation of the platforms and associated stored servers, that could be
@@ -23,18 +24,18 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_
  */
 public class Platform {
 
-    private static final long    serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
 
     private String platformName;
     private String platformDescription;
     private PlatformStatus platformStatus;
     private Set<StoredServer> storedServers = new HashSet<>();
+
     /**
      * Default Constructor sets the properties to nulls
      */
-    public Platform()
-    {
+    public Platform() {
         /*
          * Nothing to do.
          */
@@ -42,19 +43,20 @@ public class Platform {
 
     /**
      * Constructor
-
-     * @param platformName platform name
+     *
+     * @param platformName        platform name
      * @param platformDescription platform description
      */
     public Platform(String platformName, String platformDescription) {
 
-        this.platformName        = platformName;
+        this.platformName = platformName;
         this.platformDescription = platformDescription;
     }
 
 
     /**
      * Get the platform name
+     *
      * @return platform name
      */
     public String getPlatformName() {
@@ -63,6 +65,7 @@ public class Platform {
 
     /**
      * Set a meaningful name for the platform
+     *
      * @param platformName set platform name
      */
     public void setPlatformName(String platformName) {
@@ -71,6 +74,7 @@ public class Platform {
 
     /**
      * Get the platform description
+     *
      * @return platform description
      */
     public String getPlatformDescription() {
@@ -79,6 +83,7 @@ public class Platform {
 
     /**
      * Set the description for the platform
+     *
      * @param platformDescription set platform description
      */
     public void setPlatformDescription(String platformDescription) {
@@ -87,6 +92,7 @@ public class Platform {
 
     /**
      * Set the stored servers on this platform
+     *
      * @return the stored servers on this platform
      */
     public Set<StoredServer> getStoredServers() {
@@ -95,13 +101,16 @@ public class Platform {
 
     /**
      * Get the stored servers on this platform
+     *
      * @param storedServers servers
      */
     public void setStoredServers(Set<StoredServer> storedServers) {
         this.storedServers = storedServers;
     }
+
     /**
      * Add a stored server to the platform
+     *
      * @param storedServer stored server to add
      */
     public void addStoredServer(StoredServer storedServer) {
@@ -110,6 +119,7 @@ public class Platform {
 
     /**
      * return the status of the platform
+     *
      * @return the returned platform status
      */
     public PlatformStatus getPlatformStatus() {
@@ -118,9 +128,61 @@ public class Platform {
 
     /**
      * Set the platform status
+     *
      * @param platformStatus platform status to set
      */
     public void setPlatformStatus(PlatformStatus platformStatus) {
         this.platformStatus = platformStatus;
+    }
+
+    @Override
+    public String toString() {
+        return toString(new StringBuilder()).toString();
+    }
+
+    public StringBuilder toString(StringBuilder sb) {
+        if (sb == null) {
+            sb = new StringBuilder();
+        }
+        sb.append("platform=");
+        sb.append("{");
+        sb.append("platformName=").append(this.platformName).append(",");
+        sb.append("platformDescription=").append(this.platformDescription).append(",");
+        sb.append("platformStatus=").append(this.platformStatus).append(",");
+        sb.append("storedServers={");
+        for (StoredServer storedServer:storedServers) {
+             sb.append(storedServer).append(',');
+        }
+        sb.append('}');
+        sb.append('}');
+        return sb;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Platform platform = (Platform) o;
+        for (StoredServer storedServer:storedServers) {
+            boolean foundIt = false;
+           for (StoredServer platformStoredServer: platform.storedServers) {
+               if (Objects.equals(storedServer, platformStoredServer)) {
+                   foundIt = true;
+               }
+               if (!foundIt) {
+                   return false;
+               }
+           }
+        }
+
+        return Objects.equals(platformName, platform.platformName) &&
+                Objects.equals(platformDescription, platform.platformDescription) &&
+                Objects.equals(platformStatus, platform.platformStatus);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), platformName, platformDescription, platformStatus, storedServers);
     }
 }
--- open-metadata-implementation/view-services/server-author-view/server-author-view-api/src/main/java/org/odpi/openmetadata/viewservices/serverauthor/api/properties/StoredServer.java
+++ open-metadata-implementation/view-services/server-author-view/server-author-view-api/src/main/java/org/odpi/openmetadata/viewservices/serverauthor/api/properties/StoredServer.java
@@ -7,31 +7,31 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import java.util.Set;
+import java.util.Objects;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY;
 
-@JsonAutoDetect(getterVisibility=PUBLIC_ONLY, setterVisibility=PUBLIC_ONLY, fieldVisibility=NONE)
+@JsonAutoDetect(getterVisibility = PUBLIC_ONLY, setterVisibility = PUBLIC_ONLY, fieldVisibility = NONE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 /**
  * The is the Server Author View Service's concept of a stored server, it is a minimal representation of the stored server, including
  * its name, description and type; so a UI could use htis information to show a meaningful summary of the stored server.
  */
 public class StoredServer {
 
-    private static final long    serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
 
     private String storedServerName;
     private String storedServerDescription;
     private String serverType;
+
     /**
      * Default Constructor sets the properties to nulls
      */
-    public StoredServer()
-    {
+    public StoredServer() {
         /*
          * Nothing to do.
          */
@@ -40,20 +40,21 @@ public class StoredServer {
     /**
      * Constructor
      *
-     * @param storedServerName storedServer name
+     * @param storedServerName        storedServer name
      * @param storedServerDescription storedServer description
-     * @param serverType meaningful name of the type of the server
+     * @param serverType              meaningful name of the type of the server
      */
     public StoredServer(String storedServerName, String storedServerDescription, String serverType) {
 
-        this.storedServerName        = storedServerName;
+        this.storedServerName = storedServerName;
         this.storedServerDescription = storedServerDescription;
-        this.serverType              = serverType;
+        this.serverType = serverType;
     }
 
 
     /**
      * Get the storedServer name
+     *
      * @return storedServer name
      */
     public String getStoredServerName() {
@@ -62,6 +63,7 @@ public class StoredServer {
 
     /**
      * Set a meaningful name for the storedServer
+     *
      * @param storedServerName set storedServer name
      */
     public void setStoredServerName(String storedServerName) {
@@ -70,6 +72,7 @@ public class StoredServer {
 
     /**
      * Get the storedServer description
+     *
      * @return storedServer description
      */
     public String getStoredServerDescription() {
@@ -78,6 +81,7 @@ public class StoredServer {
 
     /**
      * Set the description for the storedServer
+     *
      * @param storedServerDescription set storedServer description
      */
     public void setStoredServerDescription(String storedServerDescription) {
@@ -86,16 +90,52 @@ public class StoredServer {
 
     /**
      * Get the type of the stored Server
+     *
      * @return server type
      */
     public String getServerType() {
         return serverType;
     }
+
     /**
      * Set the type for the storedServer
+     *
      * @param serverType set storedServer type
      */
     public void setServerType(String serverType) {
         this.serverType = serverType;
+    }
+
+    @Override
+    public String toString() {
+        return toString(new StringBuilder()).toString();
+    }
+
+    public StringBuilder toString(StringBuilder sb) {
+        if (sb == null) {
+            sb = new StringBuilder();
+        }
+        sb.append("storedServer=");
+        sb.append("{");
+        sb.append("storedServerName=").append(this.storedServerName).append(",");
+        sb.append("storedServerDescription=").append(this.storedServerDescription).append(",");
+        sb.append('}');
+        return sb;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        StoredServer storedServer = (StoredServer) o;
+
+        return Objects.equals(storedServerName, storedServer.storedServerName) &&
+                Objects.equals(storedServerDescription, storedServer.storedServerDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), storedServerName, storedServerDescription);
     }
 }
