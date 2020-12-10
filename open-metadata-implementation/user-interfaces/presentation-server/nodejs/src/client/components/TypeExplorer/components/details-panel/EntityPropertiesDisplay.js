9911a87727a87de55f8364df3a45f056d66f2efb
Graham Wallis <graham_wallis@uk.ibm.com>
2020-12-10 09:54:36 +0000

    Message displayed when no replacement attribute is defined
    Signed-off-by: Graham Wallis <graham_wallis@uk.ibm.com>

--- open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/TypeExplorer/components/details-panel/EntityPropertiesDisplay.js
+++ open-metadata-implementation/user-interfaces/presentation-server/nodejs/src/client/components/TypeExplorer/components/details-panel/EntityPropertiesDisplay.js
@@ -69,7 +69,9 @@ export default function EntityPropertiesDisplay(props) {
     const formattedAttributeName = formatAttributeName(attributeName, attribute);
     const formattedAttributeType = formatAttributeType(attributeName, attribute);
     formattedAttribute = <div>{formattedAttributeName} : {formattedAttributeType}
-        {(attribute.status === "DEPRECATED_ATTRIBUTE") ? "  (use "+attribute.replacedBy+")" : ""}
+        {(attribute.status === "DEPRECATED_ATTRIBUTE")
+           ?   attribute.replacedBy ? "  (use "+attribute.replacedBy+")" : "  (no replacement)"
+           :   ""}
     </div>;
     return formattedAttribute
   };
