069ff07ac4603bc34c62032ccc2a0831ee3cef8e
Mandy Chessell <mandy_chessell@uk.ibm.com>
2021-01-01 12:38:06 +0000

    Support governance engine events as a shared service
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/governance-servers/engine-host-services/engine-host-services-registration/src/main/java/org/odpi/openmetadata/governanceservers/enginehostservices/registration/OMAGEngineServiceRegistration.java
+++ open-metadata-implementation/governance-servers/engine-host-services/engine-host-services-registration/src/main/java/org/odpi/openmetadata/governanceservers/enginehostservices/registration/OMAGEngineServiceRegistration.java
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright Contributors to the ODPi Egeria project. */
-package org.odpi.openmetadata.adminservices.configuration;
+package org.odpi.openmetadata.governanceservers.enginehostservices.registration;
 
 import org.odpi.openmetadata.adminservices.configuration.registration.EngineServiceRegistration;
 
