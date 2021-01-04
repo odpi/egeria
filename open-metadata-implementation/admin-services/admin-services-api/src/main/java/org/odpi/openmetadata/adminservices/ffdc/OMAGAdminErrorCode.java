9d985783744566dcc607e979f98902123f090870
Mandy Chessell <mandy_chessell@uk.ibm.com>
2021-01-04 21:31:42 +0000

    Add @Override annotations
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/admin-services/admin-services-api/src/main/java/org/odpi/openmetadata/adminservices/ffdc/OMAGAdminErrorCode.java
+++ open-metadata-implementation/admin-services/admin-services-api/src/main/java/org/odpi/openmetadata/adminservices/ffdc/OMAGAdminErrorCode.java
@@ -455,6 +455,7 @@ public enum OMAGAdminErrorCode implements ExceptionMessageSet
      *
      * @return message definition object.
      */
+    @Override
     public ExceptionMessageDefinition getMessageDefinition()
     {
         return messageDefinition;
@@ -467,10 +468,25 @@ public enum OMAGAdminErrorCode implements ExceptionMessageSet
      * @param params array of parameters (all strings).  They are inserted into the message according to the numbering in the message text.
      * @return message definition object.
      */
+    @Override
     public ExceptionMessageDefinition getMessageDefinition(String... params)
     {
         messageDefinition.setMessageParameters(params);
 
         return messageDefinition;
+    }
+
+
+    /**
+     * JSON-style toString
+     *
+     * @return string of property names and values for this enum
+     */
+    @Override
+    public String toString()
+    {
+        return "OMAGAdminErrorCode{" +
+                       "messageDefinition=" + messageDefinition +
+                       '}';
     }
 }
