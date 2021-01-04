2ef011bd6bea48423a3e8017dbfd791d7300433a
Mandy Chessell <mandy_chessell@uk.ibm.com>
2021-01-04 21:38:19 +0000

    Add @Overrides
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/engine-services/asset-analysis/asset-analysis-api/src/main/java/org/odpi/openmetadata/engineservices/assetanalysis/ffdc/AssetAnalysisAuditCode.java
+++ open-metadata-implementation/engine-services/asset-analysis/asset-analysis-api/src/main/java/org/odpi/openmetadata/engineservices/assetanalysis/ffdc/AssetAnalysisAuditCode.java
@@ -276,6 +276,7 @@ public enum AssetAnalysisAuditCode implements AuditLogMessageSet
      *
      * @return message definition object.
      */
+    @Override
     public AuditLogMessageDefinition getMessageDefinition()
     {
         return messageDefinition;
@@ -288,6 +289,7 @@ public enum AssetAnalysisAuditCode implements AuditLogMessageSet
      * @param params array of parameters (all strings).  They are inserted into the message according to the numbering in the message text.
      * @return message definition object.
      */
+    @Override
     public AuditLogMessageDefinition getMessageDefinition(String ...params)
     {
         messageDefinition.setMessageParameters(params);
