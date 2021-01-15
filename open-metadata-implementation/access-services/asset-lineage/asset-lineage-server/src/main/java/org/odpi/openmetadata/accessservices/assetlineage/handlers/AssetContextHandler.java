51ba6e496e535be5adc13728b2d6a90b078969a3
Raluca Popa <raluca.popa2@ing.com>
2021-01-15 13:03:12 +0200

    AL OMAS - cleanup code
    Signed-off-by: Raluca Popa <raluca.popa2@ing.com>

--- open-metadata-implementation/access-services/asset-lineage/asset-lineage-server/src/main/java/org/odpi/openmetadata/accessservices/assetlineage/handlers/AssetContextHandler.java
+++ open-metadata-implementation/access-services/asset-lineage/asset-lineage-server/src/main/java/org/odpi/openmetadata/accessservices/assetlineage/handlers/AssetContextHandler.java
@@ -4,7 +4,6 @@ package org.odpi.openmetadata.accessservices.assetlineage.handlers;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.odpi.openmetadata.accessservices.assetlineage.event.AssetLineageEventType;
-import org.odpi.openmetadata.accessservices.assetlineage.model.AssetContext;
 import org.odpi.openmetadata.accessservices.assetlineage.model.GraphContext;
 import org.odpi.openmetadata.commonservices.ffdc.InvalidParameterHandler;
 import org.odpi.openmetadata.commonservices.repositoryhandler.RepositoryHandler;
@@ -14,30 +13,21 @@ import org.odpi.openmetadata.frameworks.connectors.ffdc.PropertyServerException;
 import org.odpi.openmetadata.frameworks.connectors.ffdc.UserNotAuthorizedException;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.EntityDetail;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.Relationship;
-import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.typedefs.TypeDef;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryHelper;
-import org.odpi.openmetadata.repositoryservices.ffdc.exception.RepositoryErrorException;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.ASSET_LINEAGE_OMAS;
 import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.ASSET_SCHEMA_TYPE;
 import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.ATTRIBUTE_FOR_SCHEMA;
-import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.COMPLEX_SCHEMA_TYPE;
-import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.CONNECTION;
 import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.CONNECTION_ENDPOINT;
 import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.CONNECTION_TO_ASSET;
-import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.DATABASE;
 import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.DATA_CONTENT_FOR_DATA_SET;
-import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.DATA_FILE;
 import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.FILE_FOLDER;
 import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.FOLDER_HIERARCHY;
 import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineageConstants.GUID_PARAMETER;
@@ -53,7 +43,6 @@ import static org.odpi.openmetadata.accessservices.assetlineage.util.AssetLineag
 public class AssetContextHandler {
 
     private final RepositoryHandler repositoryHandler;
-    private final OMRSRepositoryHelper repositoryHelper;
     private final InvalidParameterHandler invalidParameterHandler;
     private final HandlerHelper handlerHelper;
     private final List<String> supportedZones;
@@ -70,7 +59,6 @@ public class AssetContextHandler {
                                RepositoryHandler repositoryHandler, List<String> supportedZones, Set<String> lineageClassificationTypes) {
         this.invalidParameterHandler = invalidParameterHandler;
         this.repositoryHandler = repositoryHandler;
-        this.repositoryHelper = repositoryHelper;
         this.handlerHelper = new HandlerHelper(invalidParameterHandler, repositoryHelper, repositoryHandler, lineageClassificationTypes);
         this.supportedZones = supportedZones;
     }
@@ -237,220 +225,5 @@ public class AssetContextHandler {
         context.addAll(handlerHelper.buildContextForRelationships(userId, relationships));
 
         return handlerHelper.getEntityAtTheEnd(userId, startEntity.getGUID(), relationships.get(0));
-    }
-
-    /**
-     * Build the context for the asset
-     *
-     * @param userId       the user id
-     * @param entityDetail the entity for which the context is build
-     * @param assetContext the asset context to be updated
-     *
-     * @throws OCFCheckedExceptionBase checked exception for reporting errors found when using OCF connectors
-     */
-    private void buildSchemaElementContext(String userId, EntityDetail entityDetail, AssetContext assetContext) throws OCFCheckedExceptionBase {
-        final String typeDefName = entityDetail.getType().getTypeDefName();
-
-        addLineageMappings(userId, entityDetail, typeDefName, assetContext);
-
-        List<EntityDetail> tableTypeEntities = buildGraphByRelationshipType(userId, entityDetail, ATTRIBUTE_FOR_SCHEMA, typeDefName, assetContext);
-
-        if (tableTypeEntities.isEmpty()) {
-            tableTypeEntities = buildGraphByRelationshipType(userId, entityDetail, NESTED_SCHEMA_ATTRIBUTE, typeDefName, assetContext);
-        }
-        for (EntityDetail schemaTypeEntity : tableTypeEntities) {
-            if (isComplexSchemaType(schemaTypeEntity.getType().getTypeDefName())) {
-                setAssetDetails(userId, schemaTypeEntity, assetContext);
-            } else {
-                Optional<EntityDetail> first = tableTypeEntities.stream().findFirst();
-                if (first.isPresent()) {
-                    buildSchemaElementContext(userId, first.get(), assetContext);
-                }
-            }
-        }
-    }
-
-    /**
-     * Add lineage mappings to asset context
-     *
-     * @param userId       the user id
-     * @param entityDetail the entity object for which
-     * @param typeDefName  the entity type name
-     * @param assetContext the asset context to be updated
-     *
-     * @throws OCFCheckedExceptionBase checked exception for reporting errors found when using OCF connectors
-     */
-    private void addLineageMappings(String userId, EntityDetail entityDetail, String typeDefName, AssetContext assetContext) throws
-                                                                                                                             OCFCheckedExceptionBase {
-        List<Relationship> relationships = handlerHelper.getRelationshipsByType(userId, entityDetail.getGUID(), LINEAGE_MAPPING, typeDefName);
-        for (Relationship relationship : relationships) {
-            handlerHelper.buildGraphEdgeByRelationship(userId, entityDetail, relationship, assetContext);
-        }
-    }
-
-    /**
-     * Add the ends of the relationship to the asset context
-     *
-     * @param userId           the user id
-     * @param entityDetail     the entity object
-     * @param relationshipType the relationship name
-     * @param typeDefName      the entity type name
-     * @param assetContext     the asset context to be updated
-     *
-     * @return the list of end entities that were added to the context
-     *
-     * @throws OCFCheckedExceptionBase checked exception for reporting errors found when using OCF connectors
-     */
-    private List<EntityDetail> buildGraphByRelationshipType(String userId, EntityDetail entityDetail,
-                                                            String relationshipType, String typeDefName, AssetContext assetContext) throws
-                                                                                                                                    OCFCheckedExceptionBase {
-        handlerHelper.addLineageClassificationToContext(entityDetail, assetContext);
-
-        List<Relationship> relationships = handlerHelper.getRelationshipsByType(userId, entityDetail.getGUID(), relationshipType, typeDefName);
-
-        if (entityDetail.getType().getTypeDefName().equals(FILE_FOLDER)) {
-            relationships = relationships.stream().filter(relationship ->
-                    relationship.getEntityTwoProxy().getGUID().equals(entityDetail.getGUID())).collect(Collectors.toList());
-        }
-
-        List<EntityDetail> entityDetails = new ArrayList<>();
-        for (Relationship relationship : relationships) {
-
-            EntityDetail endEntity = handlerHelper.buildGraphEdgeByRelationship(userId, entityDetail, relationship, assetContext);
-            if (endEntity == null) return Collections.emptyList();
-
-            entityDetails.add(endEntity);
-        }
-        return entityDetails;
-    }
-
-    /**
-     * Add asset details to the context
-     *
-     * @param userId            the user id
-     * @param complexSchemaType the complex schema type entity
-     * @param assetContext      the asset context to be updated
-     *
-     * @throws OCFCheckedExceptionBase checked exception for reporting errors found when using OCF connectors
-     */
-    private void setAssetDetails(String userId, EntityDetail complexSchemaType, AssetContext assetContext) throws OCFCheckedExceptionBase {
-        List<EntityDetail> assetEntity = buildGraphByRelationshipType(userId,
-                complexSchemaType, ASSET_SCHEMA_TYPE, complexSchemaType.getType().getTypeDefName(), assetContext);
-        Optional<EntityDetail> first = assetEntity.stream().findFirst();
-        if (first.isPresent()) {
-            buildAsset(userId, first.get(), assetContext);
-
-        }
-    }
-
-    /**
-     * Add asset schema type for the asset
-     *
-     * @param userId       the user id
-     * @param entityDetail the data set entity
-     * @param assetContext the asset context to be updated
-     *
-     * @throws OCFCheckedExceptionBase checked exception for reporting errors found when using OCF connectors
-     */
-    private void buildAsset(String userId, EntityDetail entityDetail, AssetContext assetContext) throws OCFCheckedExceptionBase {
-        final String typeDefName = entityDetail.getType().getTypeDefName();
-
-        String relationshipType = typeDefName.equals(DATA_FILE) ? NESTED_FILE : DATA_CONTENT_FOR_DATA_SET;
-        List<EntityDetail> entityDetails = buildGraphByRelationshipType(userId, entityDetail, relationshipType, typeDefName, assetContext);
-        if (CollectionUtils.isEmpty(entityDetails)) {
-            return;
-        }
-        addContextForEndpoints(userId, assetContext, entityDetails.toArray(new EntityDetail[0]));
-    }
-
-    /**
-     * Add endpoints for the asset to the context
-     *
-     * @param userId        the user id
-     * @param assetContext  the asset context to be updated
-     * @param entityDetails the list of endpoints
-     *
-     * @throws OCFCheckedExceptionBase checked exception for reporting errors found when using OCF connectors
-     */
-    private void addContextForEndpoints(String userId, AssetContext assetContext, EntityDetail... entityDetails) throws OCFCheckedExceptionBase {
-        for (EntityDetail entityDetail : entityDetails) {
-            if (entityDetail != null) {
-                if (entityDetail.getType().getTypeDefName().equals(DATABASE)) {
-                    addContextForConnections(userId, entityDetail, assetContext);
-                } else {
-                    addContextFolderHierarchy(userId, entityDetail, assetContext);
-                }
-            }
-        }
-    }
-
-    /**
-     * Add connection to the asset context
-     *
-     * @param userId       the user id
-     * @param entityDetail the database entity
-     * @param assetContext the asset context to be updated
-     *
-     * @throws OCFCheckedExceptionBase checked exception for reporting errors found when using OCF connectors
-     */
-    private void addContextForConnections(String userId, EntityDetail entityDetail, AssetContext assetContext) throws OCFCheckedExceptionBase {
-
-        List<EntityDetail> connections = buildGraphByRelationshipType(userId, entityDetail, CONNECTION_TO_ASSET, DATABASE, assetContext);
-
-        if (!connections.isEmpty()) {
-            for (EntityDetail entity : connections) {
-                buildGraphByRelationshipType(userId, entity, CONNECTION_ENDPOINT, CONNECTION, assetContext);
-            }
-        }
-    }
-
-    /**
-     * Add folder hierarchy to the asset context
-     *
-     * @param userId       the user id
-     * @param entityDetail the folder entity
-     * @param assetContext the asset context to be updated
-     *
-     * @throws OCFCheckedExceptionBase checked exception for reporting errors found when using OCF connectors
-     */
-    private void addContextFolderHierarchy(String userId, EntityDetail entityDetail, AssetContext assetContext) throws OCFCheckedExceptionBase {
-
-        List<EntityDetail> connections = buildGraphByRelationshipType(userId, entityDetail,
-                CONNECTION_TO_ASSET, entityDetail.getType().getTypeDefName(), assetContext);
-
-        Optional<EntityDetail> connection = connections.stream().findFirst();
-        if (connection.isPresent()) {
-            buildGraphByRelationshipType(userId, entityDetail, CONNECTION_ENDPOINT, CONNECTION, assetContext);
-        }
-
-        Optional<EntityDetail> nestedFolder = buildGraphByRelationshipType(userId, entityDetail, FOLDER_HIERARCHY, FILE_FOLDER, assetContext)
-                .stream()
-                .findFirst();
-
-        if (nestedFolder.isPresent()) {
-            addContextFolderHierarchy(userId, nestedFolder.get(), assetContext);
-        }
-    }
-
-    /**
-     * Checks if the type is a Complex Schema Type
-     *
-     * @param typeDefName the type name
-     *
-     * @return true if the given type is a complex schema type
-     *
-     */
-    private boolean isComplexSchemaType(String typeDefName)  {
-
-        if (repositoryHelper.getKnownTypeDefGallery() == null
-                || CollectionUtils.isEmpty(repositoryHelper.getKnownTypeDefGallery().getTypeDefs())) {
-            return false;
-        }
-
-        List<TypeDef> allTypes = repositoryHelper.getKnownTypeDefGallery().getTypeDefs();
-        Optional<TypeDef> typeDef = allTypes.stream()
-                .filter(t -> t.getName().equals(typeDefName) && t.getSuperType().getName().equals(COMPLEX_SCHEMA_TYPE))
-                .findAny();
-        return typeDef.isPresent();
     }
 }
