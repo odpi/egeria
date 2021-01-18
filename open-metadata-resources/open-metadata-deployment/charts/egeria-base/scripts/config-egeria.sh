94e688e025b7eb8069595f5adcc02fbe0e6719bd
Nigel Jones <nigel.l.jones+git@gmail.com>
2021-01-18 18:08:59 +0000

    #4454 get script for server config working
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- open-metadata-resources/open-metadata-deployment/charts/egeria-base/scripts/config-egeria.sh
+++ open-metadata-resources/open-metadata-deployment/charts/egeria-base/scripts/config-egeria.sh
@@ -1,4 +1,4 @@
-
+#!/bin/sh
 
 # Script will be run by k8s as part of our initialization job.
 # Assumed here - platform up & responding to REST api, plus Kafka is available
@@ -7,32 +7,49 @@
 # - this will facilitate error handling (vs very verbose scripting). Groovy an alternative
 # Initial a version a script to get the basics working
 
+echo '-- Environment variables --'
+env
+echo '-- End of Environment variables --'
+
+echo -e '\n-- Configuring platform with requires servers'
 # Set the URL root
-curl -f --verbose --basic admin:admin -X POST \
-  ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/server-url-root?url=${EGERIA_ENDPOINT}
+echo -e '\n\n > Setting server URL root:\n'
+curl -f -k --verbose --basic admin:admin -X POST \
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/server-url-root?url=${EGERIA_ENDPOINT}"
 
 # Setup the event bus
-curl -f --verbose --basic admin:admin -X POST
-  --header "Content-Type: application/json"
-  ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/event-bus
+echo -e '\n\n > Setting up event bus:\n'
+
+curl -f -k --verbose --basic admin:admin \
+  --header "Content-Type: application/json" \
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/event-bus" \
   --data '{"producer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"}, "consumer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"} }'
 
 # Enable all the access services (we will adjust this later)
-curl -f --verbose --basic admin:admin -X POST
-  ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/access-services?serviceMode=ENABLED
+echo -e '\n\n > Enabling all access servces:\n'
+
+curl -f -k --verbose --basic admin:admin -X POST \
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/access-services?serviceMode=ENABLED"
 
 # Use a local graph repo
-curl -f --verbose --basic admin:admin -X POST
-  ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/local-repository/mode/local-graph-repository
+echo -e '\n\n > Use local graph repo:\n'
+
+curl -f -k --verbose --basic admin:admin -X POST \
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/local-repository/mode/local-graph-repository"
 
 # Configure the cohort membership
-curl -f --verbose --basic admin:admin -X POST
-  ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/cohorts/${EGERIA_COHORT}
+echo -e '\n\n > configuring cohort membership:\n'
+
+curl -f -k --verbose --basic admin:admin -X POST \
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/cohorts/${EGERIA_COHORT}"
 
 # Start up the server
-curl -f --verbose --basic admin:admin -X POST --max-time 900
-                ${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/instance
+echo -e '\n\n > Starting the server:\n'
+
+curl -f -k --verbose --basic admin:admin -X POST --max-time 900 \
+                "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/instance"
 
 #Configure autostarting of the servers
 #
 # tbd !
+echo '-- End of configuration'
