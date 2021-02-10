c3b9215427559c267d7d41d374b3d7508d0662fa
Nigel Jones <nigel.l.jones+git@gmail.com>
2021-02-10 15:22:56 +0000

    #4670 update self signed certs
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- open-metadata-resources/open-metadata-deployment/certificates/gensamplecerts.sh
+++ open-metadata-resources/open-metadata-deployment/certificates/gensamplecerts.sh
@@ -3,7 +3,7 @@
 # Copyright Contributors to the Egeria project.
 
 # ---
-# TRUSTSTORE and KEYSTORE configuration for Egeria
+# Certificate generation for Egeria
 #
 # Creates a self-signed certificate authority, as well as appropriate certificates for both the server and client role (in a TLS interaction)
 #
@@ -11,69 +11,75 @@
 # Requires a current version of the 'openssl' tool as well as access to Java (8 and above) keytool
 #
 # Provided as a sample only. There is no error checking in this script
+# hostname is assumed to be localhost - this will need extending for your environment
+#
+# Generated files are checked in. Run manually and re-checkin to update. This is to avoid
+# the certs changing every build
+#
+# ANY GENERATED CERTIFICATES SHOULD NOT BE USED IN PRODUCTION
 # ---
 
 # Fail if any command fails
 set -e
 
-# Output stores - one for our server, one for the client, plus a truststore both have
-SERVERKEYSTORE=serverkeystore.p12
-CLIENTKEYSTORE=clientkeystore.p12
-TRUSTSTORE=truststore.p12
+# Add any additional keys here
+SERVERCERTS="ServerChassis UIChassis ReactUIServer"
+CLIENTCERTS="ReactUIClient Client"
+
+# We used to call this the truststore, but this caused some confusion
+# This file contains the Egeria Certificate Authority when we are using our own certs
+TRUSTSTORE=EgeriaCA.p12
 
 # Password used for all the stores
-PASS=egeria
+KEYPASS=egeria
+STOREPASS=egeria
 
+CA=Egeria
+
+# Hostname to add in cert -- but note this is assumed to be the same for ALL
+HOST=localhost
 # ---
 # Certificate Authority
 #
 # This signs our certificates, and is included in the truststore for both the client and the server
 # Note - in a real world environment you would create additinal intermediate CAs.
 # ---
-
-openssl genrsa -out EgeriaCA.key -passout pass:${PASS}
-openssl req -x509 -new -nodes -key EgeriaCA.key -sha256 -days 3650 -out EgeriaCA.pem -subj '/C=US/ST=CA/O=ODPi/CN=CertificateAuthority' -config openssl.cnf
-
-
-# ---
-# Server certificate
-# ---
-
-openssl genrsa -out EgeriaServer.key -passout pass:${PASS}
-openssl req -new -key EgeriaServer.key -passin pass:${PASS} -out EgeriaServer.csr  -subj '/C=US/ST=CA/O=ODPi/CN=Server' -config openssl.cnf -extensions v3_server -addext "subjectAltName = DNS:localhost"
-openssl x509 -req -in EgeriaServer.csr -CA EgeriaCA.pem -CAkey EgeriaCA.key -CAcreateserial -out EgeriaServer.crt
+printf "\n\n---- Generating CA ($FNAME)  ----\n\n"
+FNAME=${CA}
+openssl genrsa -out ${FNAME}.key -passout pass:${KEYPASS}
+openssl req -x509 -new -nodes -key ${FNAME}.key -sha256 -days 3650 -out ${FNAME}.pem -subj '/C=US/ST=CA/O=LFAIData/CN=Egeria' -config openssl.cnf
+keytool -importcert -alias ${FNAME} -file ${FNAME}.pem -keypass ${KEYPASS} -storepass ${STOREPASS} -noprompt -destkeystore ${FNAME}.p12
 
 # ---
-# Client certificate
+# Server Certs
 # ---
-
-openssl genrsa -out EgeriaClient.key -passout pass:${PASS}
-openssl req -new -key EgeriaClient.key -passin pass:${PASS} -out EgeriaClient.csr  -subj '/C=US/ST=CA/O=ODPi/CN=Client' -config openssl.cnf -extensions v3_client -addext "subjectAltName = DNS:localhost"
-openssl x509 -req -in EgeriaClient.csr -CA EgeriaCA.pem -CAkey EgeriaCA.key -CAcreateserial -out EgeriaClient.crt
-
-# ---
-# Trusstore
-#
-# This is used for checking cert validity. All that is needed is a CA
-# ---
-keytool -importcert -alias EgeriaCA -file EgeriaCA.pem -keypass egeria -storepass egeria -noprompt -destkeystore ${TRUSTSTORE}
-
+for cert in ${SERVERCERTS}
+do
+  FNAME=Egeria${cert}
+  printf "\n\n---- Generating cert ($FNAME)  ----\n\n"
+  openssl genrsa -out ${FNAME}.key -passout pass:${KEYPASS}
+  openssl req -new -key ${FNAME}.key -passin pass:${KEYPASS} -out ${FNAME}.csr  -subj '/C=US/ST=CA/O=LFAIData/CN='${cert} -config openssl.cnf -extensions v3_server -addext 'subjectAltName = DNS:'${HOST}
+  openssl x509 -req -in ${FNAME}.csr -CA ${CA}.pem -CAkey ${CA}.key -CAcreateserial -out ${FNAME}.crt
+  keytool -importcert -alias ${FNAME} -file ${FNAME}.crt -keypass ${KEYPASS} -storepass ${STOREPASS} -noprompt -destkeystore ${FNAME}.p12
+done
 
 # ---
-# Keystore (server)
+# Client certs
 # ---
-keytool -importcert -alias EgeriaServer -file EgeriaServer.crt -keypass egeria -storepass egeria -noprompt -destkeystore ${SERVERKEYSTORE}
-
-# ---
-# Keystore (server)
-# ---
-keytool -importcert -alias EgeriaClient -file EgeriaClient.crt -keypass egeria -storepass egeria -noprompt -destkeystore ${CLIENTKEYSTORE}
-
+for cert in ${CLIENTCERTS}
+do
+  printf "\n\n---- Generating cert ($FNAME)  ----\n\n"
+  FNAME=Egeria${cert}
+  openssl genrsa -out ${FNAME}.key -passout pass:${KEYPASS}
+  openssl req -new -key ${FNAME}.key -passin pass:${KEYPASS} -out ${FNAME}.csr  -subj '/C=US/ST=CA/O=LFAIData/CN='${cert} -config openssl.cnf -extensions v3_client -addext 'subjectAltName = DNS:'$HOST
+  openssl x509 -req -in ${FNAME}.csr -CA ${CA}.pem -CAkey ${CA}.key -CAcreateserial -out ${FNAME}.crt
+  keytool -importcert -alias ${FNAME} -file ${FNAME}.crt -keypass ${KEYPASS} -storepass ${STOREPASS} -noprompt -destkeystore ${FNAME}.p12
+done
 
 # ---
 # output message
 # ---
-echo "Truststores/keystores created as ${TRUSTSTORE}, ${SERVERKEYSTORE}, ${CLIENTKEYSTORE}"
+echo "Certificate generation complete."
 
 
 
