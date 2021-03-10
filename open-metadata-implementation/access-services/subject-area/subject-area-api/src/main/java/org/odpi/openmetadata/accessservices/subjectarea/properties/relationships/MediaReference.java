ea2ac1ac109e52d83a6e5df93bf22906569388b4
David Radley <david_radley@uk.ibm.com>
2021-03-10 15:07:34 +0000

    #4829 Correct Subject area LineEnd construction
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- open-metadata-implementation/access-services/subject-area/subject-area-api/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/properties/relationships/MediaReference.java
+++ open-metadata-implementation/access-services/subject-area/subject-area-api/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/properties/relationships/MediaReference.java
@@ -27,21 +27,21 @@ public class MediaReference extends Line {
     /*
      * Set up end 1.
      */
-     protected static final String END_1_NODE_TYPE = "Referenceable";
-     protected static final String END_1_ATTRIBUTE_NAME = "consumingItem";
-     protected static final String END_1_ATTRIBUTE_DESCRIPTION = "Item that is referencing this work.";
-     protected static final RelationshipEndCardinality END_1_CARDINALITY = RelationshipEndCardinality.ANY_NUMBER;
-     protected static final LineEnd LINE_END_1 = new LineEnd(END_1_NODE_TYPE,
+     private static final String END_1_NODE_TYPE = "Referenceable";
+     private static final String END_1_ATTRIBUTE_NAME = "consumingItem";
+     private static final String END_1_ATTRIBUTE_DESCRIPTION = "Item that is referencing this work.";
+     private static final RelationshipEndCardinality END_1_CARDINALITY = RelationshipEndCardinality.ANY_NUMBER;
+     private static final LineEnd LINE_END_1 = new LineEnd(END_1_NODE_TYPE,
             END_1_ATTRIBUTE_NAME, END_1_ATTRIBUTE_DESCRIPTION, END_1_CARDINALITY);
 
     /*
      * Set up end 2.
      */
-     protected static final String END_2_NODE_TYPE = "RelatedMedia";
-     protected static final String END_2_ATTRIBUTE_NAME = "relatedMedia";
-     protected static final String END_2_ATTRIBUTE_DESCRIPTION = "Link to external media.";
-     protected static final RelationshipEndCardinality END_2_CARDINALITY = RelationshipEndCardinality.ANY_NUMBER;
-     protected static final LineEnd LINE_END_2 = new LineEnd(END_2_NODE_TYPE,
+     private static final String END_2_NODE_TYPE = "RelatedMedia";
+     private static final String END_2_ATTRIBUTE_NAME = "relatedMedia";
+     private static final String END_2_ATTRIBUTE_DESCRIPTION = "Link to external media.";
+     private static final RelationshipEndCardinality END_2_CARDINALITY = RelationshipEndCardinality.ANY_NUMBER;
+     private static final LineEnd LINE_END_2 = new LineEnd(END_2_NODE_TYPE,
             END_2_ATTRIBUTE_NAME, END_2_ATTRIBUTE_DESCRIPTION, END_2_CARDINALITY);
 
     private String mediaId;
