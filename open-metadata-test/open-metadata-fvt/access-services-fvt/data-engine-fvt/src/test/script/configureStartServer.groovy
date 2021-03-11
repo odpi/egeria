082dd54ffd04600c3445774259d7178f273cba16
Nigel Jones <nigel.l.jones+git@gmail.com>
2021-03-11 18:20:44 +0000

    Update groovy start scripts to be gradle compatible & ensure asset consumer tests repeatable
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- open-metadata-test/open-metadata-fvt/access-services-fvt/data-engine-fvt/src/test/script/configureStartServer.groovy
+++ open-metadata-test/open-metadata-fvt/access-services-fvt/data-engine-fvt/src/test/script/configureStartServer.groovy
@@ -12,6 +12,7 @@ import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManager
 import javax.net.ssl.X509TrustManager
 import java.security.SecureRandom
+import java.security.cert.CertificateException
 import java.security.cert.X509Certificate
 
 // Retrieve configuration - with defaults to aid in local testing (using default ports)
@@ -23,29 +24,30 @@ maxRetries = Integer.parseInt(properties["retries"] as String)
 delay = Integer.parseInt(properties["delay"] as String)
 
 // SSL setup to avoid self-signed errors for testing
-TrustManager[] trustAllCerts = new TrustManager[] {
+def trustAllCerts = [
         new X509TrustManager() {
-
             public X509Certificate[] getAcceptedIssuers() {
-                return null;
+                return null
             }
-            public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                //No need to implement.
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
             }
-            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                //No need to implement.
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
             }
         }
-}
+] as TrustManager[]
 
-try {
-    SSLContext sc = SSLContext.getInstance("SSL")
-    sc.init(null, trustAllCerts, new SecureRandom())
-    HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory())
+try
+{
+    SSLContext sc = SSLContext.getInstance("SSL");
+    sc.init(null, trustAllCerts, new java.security.SecureRandom());
+    HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
 }
-catch (Exception e) {
-    System.out.println(e)
-    System.exit(-1)
+catch (Exception e)
+{
+    System.out.println(e);
+    System.exit(-1);
 }
 
 // Wait until the platform is ready
