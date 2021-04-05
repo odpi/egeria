0c32cd7ca72b8b6e2cda8c03570277f4863ce3c6
Alexandra Bucur <alexandra-nona.bucur@ing.com>
2021-04-05 16:57:11 +0300

    OLS - Add ability to disable jobs and preset the last update time for AL update job
    Signed-off-by: Alexandra Bucur <alexandra-nona.bucur@ing.com>

--- open-metadata-implementation/governance-servers/open-lineage-services/open-lineage-services-server/src/main/java/org/odpi/openmetadata/governanceservers/openlineage/scheduler/AssetLineageUpdateJob.java
+++ open-metadata-implementation/governance-servers/open-lineage-services/open-lineage-services-server/src/main/java/org/odpi/openmetadata/governanceservers/openlineage/scheduler/AssetLineageUpdateJob.java
@@ -2,6 +2,7 @@
 /* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.governanceservers.openlineage.scheduler;
 
+import org.apache.commons.lang3.StringUtils;
 import org.odpi.openmetadata.accessservices.assetlineage.AssetLineage;
 import org.odpi.openmetadata.frameworks.connectors.ffdc.InvalidParameterException;
 import org.odpi.openmetadata.frameworks.connectors.ffdc.PropertyServerException;
@@ -16,6 +17,8 @@ import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Optional;
 
 @DisallowConcurrentExecution
@@ -27,6 +30,8 @@ public class AssetLineageUpdateJob implements Job {
 
     private static final String RUN_ASSET_LINEAGE_UPDATE_JOB = "Run AssetLineageUpdateJob task at {} GMT";
     private static final String RUNNING_FAILURE = "AssetLineageUpdateJob task execution at {} GMT failed because of the following exception {}";
+    private static final String ASSET_LINEAGE_CONFIG_DEFAULT_VALUE_ERROR = "AssetLineageUpdateJob default value from " +
+            "config was defined as '{}' and it should have a format such as '{}'";
 
     @Override
     public void execute(JobExecutionContext context) {
@@ -39,8 +44,9 @@ public class AssetLineageUpdateJob implements Job {
     /**
      * Calls the Asset Lineage client to determine updates for Glossary Terms starting from the last saved time.
      * Then it calls the connector to save the current run time.
+     *
      * @param localDateTime the time when the job last run successfully, also the time to save in the graph
-     * @param dataMap the job context data map containing useful data to run the job
+     * @param dataMap       the job context data map containing useful data to run the job
      */
     private void performTask(LocalDateTime localDateTime, JobDataMap dataMap) {
         AssetLineage assetLineageClient = (AssetLineage) dataMap.get(JobConstants.ASSET_LINEAGE_CLIENT);
@@ -49,8 +55,9 @@ public class AssetLineageUpdateJob implements Job {
 
         try {
             LineageGraph lineageGraph = (LineageGraph) dataMap.get(JobConstants.OPEN_LINEAGE_GRAPH_STORE);
-            Optional<LocalDateTime> assetLineageLastUpdateTime = lineageGraph.getAssetLineageUpdateTime();
-
+            String configAssetLineageDefaultTime = (String) dataMap.get(JobConstants.CONFIG_ASSET_LINEAGE_LAST_UPDATE_TIME);
+            Optional<LocalDateTime> assetLineageLastUpdateTime = getAssetLineageLastUpdateTime(configAssetLineageDefaultTime,
+                    lineageGraph, localDateTime);
             assetLineageClient.publishEntities(localServerName, localServerUserId, GLOSSARY_TERM, assetLineageLastUpdateTime);
 
             lineageGraph.saveAssetLineageUpdateTime(localDateTime);
@@ -58,5 +65,31 @@ public class AssetLineageUpdateJob implements Job {
         } catch (InvalidParameterException | PropertyServerException | UserNotAuthorizedException e) {
             log.warn(RUNNING_FAILURE, localDateTime, e.getMessage());
         }
+    }
+
+    /**
+     * Gets asset lineage last update time. The result will be the one saved in the graph, if any. If no value is in the
+     * graph, then it searches for a value in the job' data map that came from the jobs configuration call.
+     *
+     * @param configAssetLineageDefaultTime the config asset lineage default time
+     * @param lineageGraph                  the lineage graph
+     * @param localDateTime                 the local date time indicating this running time, used as format
+     * @return the asset lineage last update time
+     */
+    private Optional<LocalDateTime> getAssetLineageLastUpdateTime(String configAssetLineageDefaultTime, LineageGraph lineageGraph,
+                                                                  LocalDateTime localDateTime) {
+        Optional<LocalDateTime> assetLineageLastUpdateTime = lineageGraph.getAssetLineageUpdateTime();
+
+        if (!assetLineageLastUpdateTime.isPresent()) {
+            if (StringUtils.isNotEmpty(configAssetLineageDefaultTime)) {
+                try {
+                    assetLineageLastUpdateTime = Optional.of(LocalDateTime.parse(configAssetLineageDefaultTime,
+                            DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+                } catch (DateTimeParseException exception) {
+                    log.warn(ASSET_LINEAGE_CONFIG_DEFAULT_VALUE_ERROR, configAssetLineageDefaultTime, localDateTime);
+                }
+            }
+        }
+        return assetLineageLastUpdateTime;
     }
 }
