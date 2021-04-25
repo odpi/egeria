21184467cfb4ab74c2c456bd2d48c356651d6150
Mandy Chessell <mandy_chessell@uk.ibm.com>
2021-04-25 07:18:53 +0100

    Deprecate RelationalColumnType
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-resources/open-metadata-archives/open-metadata-types/src/main/java/org/odpi/openmetadata/opentypes/OpenMetadataTypesArchive.java
+++ open-metadata-resources/open-metadata-archives/open-metadata-types/src/main/java/org/odpi/openmetadata/opentypes/OpenMetadataTypesArchive.java
@@ -159,6 +159,7 @@ public class OpenMetadataTypesArchive
         update0137ToDos();
         update0450GovernanceRollout();
         update0217Ports();
+        update0534RelationalSchema();
     }
 
 
@@ -412,6 +413,33 @@ public class OpenMetadataTypesArchive
         properties.add(property);
 
         typeDefPatch.setPropertyDefinitions(properties);
+
+        return typeDefPatch;
+    }
+
+
+    /*
+     * -------------------------------------------------------------------------------------------------------
+     */
+
+
+    /**
+     * The RelationalColumnType only allows for a column to be primitive. - could be a literal, enum or external.
+     */
+    private void update0534RelationalSchema()
+    {
+        this.archiveBuilder.addTypeDefPatch(deprecateRelationalColumnType());
+    }
+
+    private TypeDefPatch deprecateRelationalColumnType()
+    {
+        final String typeName = "RelationalColumnType";
+
+        TypeDefPatch typeDefPatch = archiveBuilder.getPatchForType(typeName);
+
+        typeDefPatch.setUpdatedBy(originatorName);
+        typeDefPatch.setUpdateTime(creationDate);
+        typeDefPatch.setTypeDefStatus(TypeDefStatus.DEPRECATED_TYPEDEF);
 
         return typeDefPatch;
     }
