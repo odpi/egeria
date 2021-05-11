9510020564bc59fb4a3e263feb7b8c94dd09f4e3
Liviu Popa <liviu-constantin.popa@ing.com>
2021-05-11 17:27:50 +0300

    de-omas - Updated user doc and postman requests
    Signed-off-by: Liviu Popa <liviu-constantin.popa@ing.com>

--- open-metadata-implementation/access-services/data-engine/data-engine-server/src/main/java/org/odpi/openmetadata/accessservices/dataengine/server/builders/ConnectionBuilder.java
+++ open-metadata-implementation/access-services/data-engine/data-engine-server/src/main/java/org/odpi/openmetadata/accessservices/dataengine/server/builders/ConnectionBuilder.java
@@ -3,40 +3,12 @@
 package org.odpi.openmetadata.accessservices.dataengine.server.builders;
 
 import org.odpi.openmetadata.commonservices.generichandlers.ReferenceableBuilder;
-import org.odpi.openmetadata.frameworks.connectors.ffdc.InvalidParameterException;
-import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.InstanceProperties;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryHelper;
-
-import static org.odpi.openmetadata.commonservices.generichandlers.OpenMetadataAPIMapper.DISPLAY_NAME_PROPERTY_NAME;
 
 public class ConnectionBuilder extends ReferenceableBuilder {
 
-    private final String name;
-
-    public ConnectionBuilder(String name, String qualifiedName, String typeId, String typeName,
-                             OMRSRepositoryHelper repositoryHelper, String serviceName, String serverName) {
+    public ConnectionBuilder(String qualifiedName, String typeId, String typeName, OMRSRepositoryHelper repositoryHelper,
+                                String serviceName, String serverName) {
         super(qualifiedName, typeId, typeName, repositoryHelper, serviceName, serverName);
-        this.name = name;
     }
-
-    /**
-     * Return the supplied bean properties in an InstanceProperties object.
-     *
-     * @param methodName name of the calling method
-     * @return InstanceProperties object
-     * @throws InvalidParameterException there is a problem with the properties
-     */
-    @Override
-    public InstanceProperties getInstanceProperties(String  methodName) throws InvalidParameterException {
-
-        InstanceProperties properties = super.getInstanceProperties(methodName);
-
-        if (name != null) {
-            properties = repositoryHelper.addStringPropertyToInstance(serviceName, properties, DISPLAY_NAME_PROPERTY_NAME,
-                    name, methodName);
-        }
-
-        return properties;
-    }
-
 }
