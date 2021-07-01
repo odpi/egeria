020630cd049d4c394937cac5782f62ee6abbd922
David Radley <david_radley@uk.ibm.com>
2021-07-01 10:44:20 +0100

    #5399 generic handlers dependancy test
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- open-metadata-implementation/access-services/subject-area/subject-area-server/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/server/services/SubjectAreaServicesInstance.java
+++ open-metadata-implementation/access-services/subject-area/subject-area-server/src/main/java/org/odpi/openmetadata/accessservices/subjectarea/server/services/SubjectAreaServicesInstance.java
@@ -11,7 +11,7 @@ import org.odpi.openmetadata.commonservices.multitenant.OMASServiceInstance;
 import org.odpi.openmetadata.commonservices.multitenant.ffdc.exceptions.NewInstanceException;
 import org.odpi.openmetadata.frameworks.auditlog.AuditLog;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryConnector;
-
+import org.odpi.openmetadata.commonservices.generichandlers.*;
 /**
  * SubjectAreaRESTServicesInstance caches references to OMRS objects for a specific server.
  * It is also responsible for registering itself in the instance map.
@@ -62,6 +62,20 @@ public class SubjectAreaServicesInstance extends OMASServiceInstance
                         repositoryHelper
                 );
             }
+            OpenMetadataAPIGenericHandler<Object> genericHandler = new OpenMetadataAPIGenericHandler<>(
+                                                                      null,
+                                                                      Object.class,    // no beans
+                                                                      serviceName,
+                                                                      serverName,
+                                                                      invalidParameterHandler,
+                                                                      repositoryHandler,
+                                                                      repositoryHelper,
+                                                                      localServerUserId,
+                                                                      securityVerifier,
+                                                                      supportedZones,
+                                                                      defaultZones,
+                                                                      publishZones,
+                                                                      auditLog);
 
             this.glossaryHandler= new SubjectAreaGlossaryHandler(oMRSAPIHelper, maxPageSize);
 
