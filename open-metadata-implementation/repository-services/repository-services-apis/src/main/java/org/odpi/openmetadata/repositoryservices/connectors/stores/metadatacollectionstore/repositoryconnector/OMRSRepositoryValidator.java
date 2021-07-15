6c778827121a3c65671f3fd30a3a4903a4ba025a
Bogdan Sava <bogdan.sava@gmail.com>
2021-07-15 16:48:23 +0300

    fix javadoc typos
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- open-metadata-implementation/repository-services/repository-services-apis/src/main/java/org/odpi/openmetadata/repositoryservices/connectors/stores/metadatacollectionstore/repositoryconnector/OMRSRepositoryValidator.java
+++ open-metadata-implementation/repository-services/repository-services-apis/src/main/java/org/odpi/openmetadata/repositoryservices/connectors/stores/metadatacollectionstore/repositoryconnector/OMRSRepositoryValidator.java
@@ -1541,8 +1541,8 @@ public interface OMRSRepositoryValidator
      * @param sourceName  source of the request (used for logging)
      * @param instance  instance to test
      * @param typeName  name of the type
-     * @param localMethodName  local method that deleted the error
-     * @return  boolean
+     * @param localMethodName  local method that is calling the method
+     * @return true if typeName is instance type or a supertype of it, otherwise false
      */
     boolean isATypeOf(String              sourceName,
                       InstanceAuditHeader instance,
