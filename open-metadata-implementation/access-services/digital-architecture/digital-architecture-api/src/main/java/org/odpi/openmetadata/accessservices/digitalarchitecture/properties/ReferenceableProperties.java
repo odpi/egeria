312dcb37f2b0cbce1bf76c7426327ef96e76175b
Mandy Chessell <mandy_chessell@uk.ibm.com>
2021-08-14 21:09:20 +0100

    Update connector type properties
    Signed-off-by: Mandy Chessell <mandy_chessell@uk.ibm.com>

--- open-metadata-implementation/access-services/digital-architecture/digital-architecture-api/src/main/java/org/odpi/openmetadata/accessservices/digitalarchitecture/properties/ReferenceableProperties.java
+++ open-metadata-implementation/access-services/digital-architecture/digital-architecture-api/src/main/java/org/odpi/openmetadata/accessservices/digitalarchitecture/properties/ReferenceableProperties.java
@@ -33,7 +33,6 @@ public class ReferenceableProperties implements Serializable
 
     private String               qualifiedName        = null;
     private Map<String, String>  additionalProperties = null;
-    private List<String>         meanings             = null;
 
     private List<ElementClassification> classifications = null;
 
@@ -60,7 +59,6 @@ public class ReferenceableProperties implements Serializable
         {
             qualifiedName        = template.getQualifiedName();
             additionalProperties = template.getAdditionalProperties();
-            meanings             = template.getMeanings();
             classifications      = template.getClassifications();
             typeName             = template.getTypeName();
             extendedProperties   = template.getExtendedProperties();
@@ -119,41 +117,8 @@ public class ReferenceableProperties implements Serializable
         }
         else
         {
-            return new HashMap<>(additionalProperties);
+            return additionalProperties;
         }
-    }
-
-
-    /**
-     * Return the assigned meanings for this metadata entity.
-     *
-     * @return list of meanings
-     */
-    public List<String> getMeanings()
-    {
-        if (meanings == null)
-        {
-            return null;
-        }
-        else if (meanings.isEmpty())
-        {
-            return null;
-        }
-        else
-        {
-            return new ArrayList<>(meanings);
-        }
-    }
-
-
-    /**
-     * Set up the assigned meanings for this metadata entity.
-     *
-     * @param meanings list of meanings
-     */
-    public void setMeanings(List<String> meanings)
-    {
-        this.meanings = meanings;
     }
 
 
@@ -231,7 +196,7 @@ public class ReferenceableProperties implements Serializable
         }
         else
         {
-            return new HashMap<>(extendedProperties);
+            return extendedProperties;
         }
     }
 
@@ -261,7 +226,6 @@ public class ReferenceableProperties implements Serializable
                 ", additionalProperties=" + additionalProperties +
                 ", typeName=" + typeName +
                 ", extendedProperties=" + getExtendedProperties() +
-                ", meanings=" + meanings +
                 ", classifications=" + getClassifications() +
                 '}';
     }
@@ -287,7 +251,6 @@ public class ReferenceableProperties implements Serializable
         ReferenceableProperties that = (ReferenceableProperties) objectToCompare;
         return Objects.equals(qualifiedName, that.qualifiedName) &&
                 Objects.equals(additionalProperties, that.additionalProperties) &&
-                Objects.equals(meanings, that.meanings) &&
                 Objects.equals(classifications, that.classifications) &&
                 Objects.equals(typeName, that.typeName) &&
                 Objects.equals(extendedProperties, that.extendedProperties);
@@ -302,6 +265,6 @@ public class ReferenceableProperties implements Serializable
     @Override
     public int hashCode()
     {
-        return Objects.hash(qualifiedName, additionalProperties, meanings, classifications, typeName, extendedProperties);
+        return Objects.hash(qualifiedName, additionalProperties, classifications, typeName, extendedProperties);
     }
 }
