e1d9e611800355e269b475370821430f14edf77c
David Radley <david_radley@uk.ibm.com>
2021-09-21 22:52:25 +0100

    #5693 remove iterator change
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- open-metadata-implementation/common-services/repository-handler/src/main/java/org/odpi/openmetadata/commonservices/repositoryhandler/RepositoryRelatedEntitiesIterator.java
+++ open-metadata-implementation/common-services/repository-handler/src/main/java/org/odpi/openmetadata/commonservices/repositoryhandler/RepositoryRelatedEntitiesIterator.java
@@ -6,16 +6,11 @@ package org.odpi.openmetadata.commonservices.repositoryhandler;
 import org.odpi.openmetadata.frameworks.connectors.ffdc.PropertyServerException;
 import org.odpi.openmetadata.frameworks.connectors.ffdc.UserNotAuthorizedException;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.EntityDetail;
-import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.EntityProxy;
-import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.InstanceStatus;
-import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.Relationship;
-import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 
 /**
@@ -29,13 +24,9 @@ public class RepositoryRelatedEntitiesIterator extends RepositoryIteratorForEnti
 {
     private String             startingEntityGUID;
     private String             startingEntityTypeName;
-    private String             relationshipTypeGUID;   // only used when there is one relationship passed
-    private List<String>       relationshipTypeNames = new ArrayList<>();
-    private List<String>       relatedEntityTypeNames = new ArrayList<>();
-    private List<InstanceStatus>  limitResultsByStatus = new ArrayList<>();
-    private List<String>       limitResultsByClassification = new ArrayList<>();
+    private String             relationshipTypeGUID;
+    private String             relationshipTypeName;
     private int                selectionEnd = 0;
-    OMRSRepositoryHelper omrsRepositoryHelper;
 
     private static final Logger log = LoggerFactory.getLogger(RepositoryRelatedEntitiesIterator.class);
 
@@ -80,64 +71,15 @@ public class RepositoryRelatedEntitiesIterator extends RepositoryIteratorForEnti
         this.startingEntityGUID     = startingEntityGUID;
         this.startingEntityTypeName = startingEntityTypeName;
         this.relationshipTypeGUID   = relationshipTypeGUID;
-        this.relationshipTypeNames.add(relationshipTypeName);
+        this.relationshipTypeName   = relationshipTypeName;
 
         if (log.isDebugEnabled())
         {
             log.debug("RepositoryRelatedEntitiesIterator :startingFrom=" + startingFrom + ",startingEntityGUID=" + startingEntityGUID);
         }
     }
-    /**
-     * Constructor takes the parameters used to call the repository handler.
-     *
-     * @param repositoryHandler interface to the open metadata repositories.
-     * @param userId  user making the request
-     * @param startingEntityGUID  starting entity's GUID
-     * @param startingEntityTypeName  starting entity's type name
-     * @param relationshipTypeNames  type names for the relationship to follow
-     * @param relatedEntityTypeNames entity type names to filter by
-     * @param limitResultsByStatus   limit results by status
-     * @param limitResultsByClassification limit results by  classification
-     * @param sequencingPropertyName name of property used to sequence the results - null means no sequencing
-     * @param startingFrom initial position in the stored list.
-     * @param pageSize maximum number of definitions to return on this call.
-     * @param effectiveTime the time that the retrieved elements must be effective for
-     * @param methodName  name of calling method
-     */
-    public RepositoryRelatedEntitiesIterator(RepositoryHandler repositoryHandler,
-                                             OMRSRepositoryHelper omrsRepositoryHelper,
-                                             String            userId,
-                                             String            startingEntityGUID,
-                                             String            startingEntityTypeName,
-                                             List<String>      relationshipTypeNames,
-                                             List<String>      relatedEntityTypeNames,
-                                             List<InstanceStatus>  limitResultsByStatus,
-                                             List<String>      limitResultsByClassification,
-                                             String            sequencingPropertyName,
-                                             int               startingFrom,
-                                             int               pageSize,
-                                             Date              effectiveTime,
-                                             String            methodName) {
-        super(repositoryHandler,
-              userId,
-              null,
-              null,
-              sequencingPropertyName,
-              startingFrom,
-              pageSize,
-              effectiveTime,
-              methodName);
 
-        this.startingEntityGUID     = startingEntityGUID;
-        this.startingEntityTypeName = startingEntityTypeName;
-        this.omrsRepositoryHelper = omrsRepositoryHelper;
 
-        this.relationshipTypeNames = relationshipTypeNames;
-        this.relatedEntityTypeNames = relatedEntityTypeNames;
-        this.limitResultsByStatus = limitResultsByStatus;
-        this.limitResultsByClassification = limitResultsByClassification;
-
-    }
     /**
      * Constructor takes the parameters used to call the repository handler.
      *
@@ -204,63 +146,21 @@ public class RepositoryRelatedEntitiesIterator extends RepositoryIteratorForEnti
              */
             while ((entitiesCache != null) && (entitiesCache.isEmpty()))
             {
-                if (selectionEnd == 0) {
-                    // TODO check whether the relationships type contain duplicates?
-                    for (String relationshipTypeName : relationshipTypeNames) {
-                        String relationshipTypeGUID = omrsRepositoryHelper.getTypeDefByName(methodName, relationshipTypeName).getGUID();
-                        List<Relationship> relationships = repositoryHandler.getRelationshipsByType(userId,
-                                                                                                    startingEntityGUID,
-                                                                                                    startingEntityTypeName,
-                                                                                                    relationshipTypeGUID,
-                                                                                                    relationshipTypeName,
-                                                                                                    0,
-                                                                                                    pageSize,
-                                                                                                    effectiveTime,
-                                                                                                    methodName);
-                        for (Relationship relationship : relationships) {
-                            EntityProxy entityProxy = relationship.getEntityOneProxy();
-                            if (relationship.getEntityOneProxy().getGUID().equals(startingEntityGUID)) {
-                                entityProxy = relationship.getEntityTwoProxy();
-                            }
-                            boolean statusOK = true;
-                            if (limitResultsByStatus != null) {
-                                statusOK = false;
-                                for (InstanceStatus instanceStatus : limitResultsByStatus) {
-                                    if (entityProxy.getStatus().equals(instanceStatus)) {
-                                        statusOK = true;
-                                    }
-                                }
-                            }
-                            if (statusOK) {
-                                boolean entityTypeOK = true;
-                                // check entity type
-                                if (relatedEntityTypeNames.size() > 0) {
-                                    entityTypeOK = false;
-                                    for (String relatedEntityTypeName : relatedEntityTypeNames) {
-                                        if (omrsRepositoryHelper.isTypeOf(methodName, entityProxy.getType().getTypeDefName(), relatedEntityTypeName)) {
-                                            entityTypeOK = true;
-                                        }
-                                    }
-                                }
-                                if (entityTypeOK) {
-                                    // check classifications
-                                    boolean classificationsOK = true;
-
-
-                                    if (classificationsOK) {
-                                        EntityDetail entityDetail = repositoryHandler.getEntityByGUID(userId, entityProxy.getGUID(), "guid", entityProxy.getType().getTypeDefName(), effectiveTime, methodName);
-                                        entitiesCache.add(entityDetail);
-                                    }
-                                }
-                            }
-                        }
-                    }
+                if (selectionEnd == 0)
+                {
+                    entitiesCache = repositoryHandler.getEntitiesForRelationshipType(userId,
+                                                                                     startingEntityGUID,
+                                                                                     startingEntityTypeName,
+                                                                                     relationshipTypeGUID,
+                                                                                     relationshipTypeName,
+                                                                                     null,
+                                                                                     startingFrom,
+                                                                                     pageSize,
+                                                                                     effectiveTime,
+                                                                                     methodName);
                 }
                 else
                 {
-                    // because of the constructor there can be only on relationship type
-                    String  relationshipTypeName = relationshipTypeNames.get(0);
-
                     boolean startAtEnd1 = false;
 
                     if (selectionEnd == 2)
@@ -268,7 +168,7 @@ public class RepositoryRelatedEntitiesIterator extends RepositoryIteratorForEnti
                         startAtEnd1 = true;
                     }
 
-                    entitiesCache = repositoryHandler.getRelatedEntityProxies(userId,
+                    entitiesCache = repositoryHandler.getEntitiesForRelationshipEnd(userId,
                                                                                     startingEntityGUID,
                                                                                     startingEntityTypeName,
                                                                                     startAtEnd1,
@@ -278,8 +178,6 @@ public class RepositoryRelatedEntitiesIterator extends RepositoryIteratorForEnti
                                                                                     pageSize,
                                                                                     effectiveTime,
                                                                                     methodName);
-
-
                 }
 
                 if (entitiesCache != null)
