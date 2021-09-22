72f517bdb2c5df03139b7d64bcc443f6558c5a2c
Alexandra Bucur <alexandra-nona.bucur@ing.com>
2021-09-22 10:59:54 +0300

    Fix ConnectionAndEndpointFVT - Change DataStoreConnectorTypes.json path
    Signed-off-by: Alexandra Bucur <alexandra-nona.bucur@ing.com>

--- open-metadata-test/open-metadata-fvt/access-services-fvt/data-engine-fvt/src/test/script/configureStartServer.groovy
+++ open-metadata-test/open-metadata-fvt/access-services-fvt/data-engine-fvt/src/test/script/configureStartServer.groovy
@@ -20,9 +20,9 @@ user=(properties["user"] ?: System.properties["user"]) ?: "admin"
 baseURL=(properties["baseURL"] ?: System.properties["baseURL"]) ?: "https://localhost:9999"
 serverMem=(properties["serverInMemory"] ?: System.properties["serverInMemory"]) ?: "serverinmem"
 serverGraph=(properties["serverLocalGraph"] ?: System.properties["serverLocalGraph"]) ?: "servergraph"
-maxRetries=Integer.parseInt((properties["retries"] ?: System.properties["retries"]) ?: 12 as String)
-delay=Integer.parseInt((properties["delay"] ?: System.properties["delay"]) ?: 10 as String)
-File connectorTypeArchive = new File("../../../../content-packs/DataStoreConnectorTypes.json")
+maxRetries=Integer.parseInt((properties["retries"].toString() ?: System.properties["retries"].toString()) ?: 12 as String)
+delay=Integer.parseInt((properties["delay"].toString() ?: System.properties["delay"].toString()) ?: 10 as String)
+File connectorTypeArchive = new File(properties["connectorTypeArchivePath"].toString())
 
 // SSL setup to avoid self-signed errors for testing
 def trustAllCerts = [
