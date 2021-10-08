6abce5a40f55c898a4d93e2059a8caeded4bbf69
Bogdan Sava <bogdan.sava@gmail.com>
2021-10-08 16:49:35 +0300

    javadoc
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/java/org/odpi/openmetadata/userinterface/uichassis/springboot/auth/SecurityConfig.java
+++ open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/java/org/odpi/openmetadata/userinterface/uichassis/springboot/auth/SecurityConfig.java
@@ -23,6 +23,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Configuration of HttpSecurity for Spring security
+ */
 public abstract class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
