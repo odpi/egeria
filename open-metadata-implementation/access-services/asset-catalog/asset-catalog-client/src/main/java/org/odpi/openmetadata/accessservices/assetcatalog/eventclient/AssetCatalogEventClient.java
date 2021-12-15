f25dd9eb5307a9f6d242cf427561b4570fdff046
Mandy Chessell <mandy.e.chessell@gmail.com>
2021-12-15 11:13:51 +0000

    Distinguish OCF REST calls and extend converter methods
    Signed-off-by: Mandy Chessell <mandy.e.chessell@gmail.com>

--- open-metadata-implementation/access-services/asset-catalog/asset-catalog-client/src/main/java/org/odpi/openmetadata/accessservices/assetcatalog/eventclient/AssetCatalogEventClient.java
+++ open-metadata-implementation/access-services/asset-catalog/asset-catalog-client/src/main/java/org/odpi/openmetadata/accessservices/assetcatalog/eventclient/AssetCatalogEventClient.java
@@ -154,11 +154,11 @@ public class AssetCatalogEventClient {
         invalidParameterHandler.validateUserId(userId, methodName);
         invalidParameterHandler.validateObject(listener, nameParameter, methodName);
 
-        ConnectionResponse restResult = restClient.callConnectionGetRESTCall(methodName,
-                serverPlatformURLRoot + urlTemplate,
-                serverName,
-                userId,
-                callerId);
+        ConnectionResponse restResult = restClient.callOCFConnectionGetRESTCall(methodName,
+                                                                                serverPlatformURLRoot + urlTemplate,
+                                                                                serverName,
+                                                                                userId,
+                                                                                callerId);
 
         Connection topicConnection = restResult.getConnection();
         ConnectorBroker connectorBroker = new ConnectorBroker();
--- open-metadata-implementation/access-services/community-profile/community-profile-server/src/main/java/org/odpi/openmetadata/accessservices/communityprofile/converters/CollectionConverter.java
+++ open-metadata-implementation/access-services/community-profile/community-profile-server/src/main/java/org/odpi/openmetadata/accessservices/communityprofile/converters/CollectionConverter.java
@@ -13,6 +13,7 @@ import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollec
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.EnumPropertyValue;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.InstanceProperties;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.InstancePropertyValue;
+import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.Relationship;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.typedefs.TypeDefCategory;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryHelper;
 
@@ -110,6 +111,28 @@ public class CollectionConverter<B> extends CommunityProfileOMASConverter<B>
         }
 
         return null;
+    }
+
+
+    /**
+     * Using the supplied instances, return a new instance of the bean. This is used for beans that
+     * contain a combination of the properties from an entity and that of a connected relationship.
+     *
+     * @param beanClass name of the class to create
+     * @param entity entity containing the properties
+     * @param relationship relationship containing the properties
+     * @param methodName calling method
+     * @return bean populated with properties from the instances supplied
+     * @throws PropertyServerException there is a problem instantiating the bean
+     */
+    @SuppressWarnings(value = "unused")
+    @Override
+    public B getNewBean(Class<B>     beanClass,
+                        EntityDetail entity,
+                        Relationship relationship,
+                        String       methodName) throws PropertyServerException
+    {
+        return this.getNewBean(beanClass, entity, methodName);
     }
 
 
