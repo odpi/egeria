04a54f61e4a3d4deb270d46200da261b28a9f58e
marius-patrascu <marius-florin.patrascu@ing.com>
2022-01-20 11:03:25 +0200

    fix licence
    Signed-off-by: marius-patrascu <marius-florin.patrascu@ing.com>

--- open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/java/org/odpi/openmetadata/userinterface/uichassis/springboot/api/exceptions/rex/UnsupportedFunctionException.java
+++ open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/java/org/odpi/openmetadata/userinterface/uichassis/springboot/api/exceptions/rex/UnsupportedFunctionException.java
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
 package org.odpi.openmetadata.userinterface.uichassis.springboot.api.exceptions.rex;
 
 import org.springframework.http.HttpStatus;
