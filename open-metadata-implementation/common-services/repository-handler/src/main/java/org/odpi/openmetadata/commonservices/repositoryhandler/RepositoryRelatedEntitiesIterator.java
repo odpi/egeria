646d7d9c86c37d9543f5a192efa9b5de38ab1535
Mandy Chessell <mandy.e.chessell@gmail.com>
2022-04-03 18:05:03 +0100

    Correct max page size in iterator
    Signed-off-by: Mandy Chessell <mandy.e.chessell@gmail.com>

--- open-metadata-implementation/common-services/repository-handler/src/main/java/org/odpi/openmetadata/commonservices/repositoryhandler/RepositoryRelatedEntitiesIterator.java
+++ open-metadata-implementation/common-services/repository-handler/src/main/java/org/odpi/openmetadata/commonservices/repositoryhandler/RepositoryRelatedEntitiesIterator.java
@@ -4,7 +4,7 @@
 package org.odpi.openmetadata.commonservices.repositoryhandler;
 
 import org.odpi.openmetadata.commonservices.ffdc.InvalidParameterHandler;
-import org.odpi.openmetadata.commonservices.ffdc.exceptions.InvalidParameterException;
+import org.odpi.openmetadata.frameworks.connectors.ffdc.InvalidParameterException;
 import org.odpi.openmetadata.frameworks.connectors.ffdc.PropertyServerException;
 import org.odpi.openmetadata.frameworks.connectors.ffdc.UserNotAuthorizedException;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.properties.instances.EntityDetail;
@@ -65,7 +65,8 @@ public class RepositoryRelatedEntitiesIterator extends RepositoryIteratorForEnti
                                              int                     startingFrom,
                                              int                     pageSize,
                                              Date                    effectiveTime,
-                                             String                  methodName) throws InvalidParameterException {
+                                             String                  methodName) throws InvalidParameterException
+    {
         super(repositoryHandler,
               invalidParameterHandler,
               userId,
