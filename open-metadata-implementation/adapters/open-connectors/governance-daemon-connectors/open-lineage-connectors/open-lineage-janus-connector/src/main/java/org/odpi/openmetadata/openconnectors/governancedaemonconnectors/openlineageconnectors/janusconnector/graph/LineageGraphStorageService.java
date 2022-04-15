3f22f9db4b9ce8d6fe212b131133bd830976ad0e
marius-patrascu <marius-florin.patrascu@ing.com>
2022-04-15 12:42:30 +0300

    fixed node label for asset_lineage_variables
    Signed-off-by: marius-patrascu <marius-florin.patrascu@ing.com>

--- open-metadata-implementation/adapters/open-connectors/governance-daemon-connectors/open-lineage-connectors/open-lineage-janus-connector/src/main/java/org/odpi/openmetadata/openconnectors/governancedaemonconnectors/openlineageconnectors/janusconnector/graph/LineageGraphStorageService.java
+++ open-metadata-implementation/adapters/open-connectors/governance-daemon-connectors/open-lineage-connectors/open-lineage-janus-connector/src/main/java/org/odpi/openmetadata/openconnectors/governancedaemonconnectors/openlineageconnectors/janusconnector/graph/LineageGraphStorageService.java
@@ -477,9 +477,11 @@ public class LineageGraphStorageService implements LineageGraph {
     }
 
     private void saveUpdateTime(GraphTraversalSource g, Long updateTime) {
-        GraphTraversal<Vertex, Vertex> lineageVariables = g.V().has("vertex--label", ASSET_LINEAGE_VARIABLES);
+        GraphTraversal<Vertex, Vertex> lineageVariables = g.V().has(PROPERTY_KEY_LABEL, ASSET_LINEAGE_VARIABLES);
         if (!lineageVariables.hasNext()) {
-            g.addV(ASSET_LINEAGE_VARIABLES).property(VARIABLE_NAME_ASSET_LINEAGE_LAST_UPDATE_TIME, updateTime).next();
+            g.addV(ASSET_LINEAGE_VARIABLES)
+                    .property(PROPERTY_KEY_LABEL, ASSET_LINEAGE_VARIABLES)
+                    .property(VARIABLE_NAME_ASSET_LINEAGE_LAST_UPDATE_TIME, updateTime).next();
         } else {
             g.V(lineageVariables.next().id()).property(VARIABLE_NAME_ASSET_LINEAGE_LAST_UPDATE_TIME, updateTime).next();
         }
@@ -492,7 +494,7 @@ public class LineageGraphStorageService implements LineageGraph {
     }
 
     private Optional<Long> getUpdateTime(GraphTraversalSource g) {
-        GraphTraversal<Vertex, Map<Object, List<Long>>> lineageVariables = g.V().has("vertex--label", ASSET_LINEAGE_VARIABLES).valueMap();
+        GraphTraversal<Vertex, Map<Object, List<Long>>> lineageVariables = g.V().has(PROPERTY_KEY_LABEL, ASSET_LINEAGE_VARIABLES).valueMap();
         if (lineageVariables.hasNext()) {
 
             Map<Object, List<Long>> next = lineageVariables.next();
