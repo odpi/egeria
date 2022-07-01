930843797020dfca3d95e24feed3652bb89111a3
William Bittles <wbittles@uk.ibm.com>
2022-07-01 16:20:15 +0100

    mend
    Signed-off-by: William Bittles <wbittles@uk.ibm.com>

--- open-metadata-test/open-metadata-fvt/view-services-fvt/glossary-author-fvt/src/main/java/org/odpi/openmetadata/viewservices/glossaryauthor/fvt/TermFVT.java
+++ open-metadata-test/open-metadata-fvt/view-services-fvt/glossary-author-fvt/src/main/java/org/odpi/openmetadata/viewservices/glossaryauthor/fvt/TermFVT.java
@@ -118,7 +118,6 @@ public class TermFVT {
             System.out.println("TermFVT runIt stopped");
         }
         catch (Exception error) {
-            /* signoff hack */
             log.error("The FVT Encountered an Exeption", error);
             throw error;
         }
