971f630187a65496c03cd2000a8694e686e25547
William Bittles <wbittles@uk.ibm.com>
2022-07-26 16:16:12 +0100

    change string format specifiers
    Signed-off-by: William Bittles <wbittles@uk.ibm.com>

--- open-metadata-conformance-suite/open-metadata-conformance-suite-api/src/main/java/org/odpi/openmetadata/conformance/ffdc/ConformanceSuiteErrorCode.java
+++ open-metadata-conformance-suite/open-metadata-conformance-suite-api/src/main/java/org/odpi/openmetadata/conformance/ffdc/ConformanceSuiteErrorCode.java
@@ -242,12 +242,12 @@ public enum ConformanceSuiteErrorCode
      */
     public String getFormattedErrorMessage(String... params)
     {
-        log.debug(String.format("<== ConformanceSuiteErrorCode.getMessage({})", Arrays.toString(params)));
+        log.debug(String.format("<== ConformanceSuiteErrorCode.getMessage(%s)", Arrays.toString(params)));
 
         MessageFormat mf = new MessageFormat(errorMessage);
         String result = mf.format(params);
         
-        log.debug(String.format("==> ConformanceSuiteErrorCode.getMessage({}): {}", Arrays.toString(params), result));
+        log.debug(String.format("==> ConformanceSuiteErrorCode.getMessage(%s): %s", Arrays.toString(params), result));
 
         return result;
     }
