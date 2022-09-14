223334af22a894d8d3533f3feeb874781d72a766
Mandy Chessell <mandy.e.chessell@gmail.com>
2022-09-14 11:54:50 +0100

    Add client for translations and valid metadata sets
    Signed-off-by: Mandy Chessell <mandy.e.chessell@gmail.com>

--- open-metadata-implementation/common-services/gaf-metadata-management/gaf-metadata-api/src/main/java/org/odpi/openmetadata/commonservices/gaf/rest/TranslationDetailResponse.java
+++ open-metadata-implementation/common-services/gaf-metadata-management/gaf-metadata-api/src/main/java/org/odpi/openmetadata/commonservices/gaf/rest/TranslationDetailResponse.java
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: Apache 2.0 */
+/* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright Contributors to the ODPi Egeria project. */
 
 package org.odpi.openmetadata.commonservices.gaf.rest;
@@ -6,35 +6,35 @@ package org.odpi.openmetadata.commonservices.gaf.rest;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import org.odpi.openmetadata.frameworks.connectors.properties.beans.ElementHeader;
+import org.odpi.openmetadata.commonservices.gaf.properties.TranslationDetail;
+import org.odpi.openmetadata.frameworks.governanceaction.properties.OpenMetadataElement;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY;
 
+
 /**
- * ElementHeadersResponse is a response object for passing back a list of element headers
- * or an exception if the request failed.
+ * TranslationDetailResponse is the response structure used on the OMAS REST API calls that return the GAF definition of the TranslationDetail.
  */
 @JsonAutoDetect(getterVisibility=PUBLIC_ONLY, setterVisibility=PUBLIC_ONLY, fieldVisibility=NONE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown=true)
-public class ElementHeadersResponse extends OMAGGAFAPIResponse
+public class TranslationDetailResponse extends OMAGGAFAPIResponse
 {
     private static final long    serialVersionUID = 1L;
 
-    private List<ElementHeader> elementList = null;
+    private TranslationDetail element = null;
 
 
     /**
      * Default constructor
      */
-    public ElementHeadersResponse()
+    public TranslationDetailResponse()
     {
+        super();
     }
 
 
@@ -43,47 +43,36 @@ public class ElementHeadersResponse extends OMAGGAFAPIResponse
      *
      * @param template object to copy
      */
-    public ElementHeadersResponse(ElementHeadersResponse template)
+    public TranslationDetailResponse(TranslationDetailResponse template)
     {
         super(template);
 
         if (template != null)
         {
-            elementList = template.getElementList();
+            this.element = template.getElement();
         }
     }
 
 
     /**
-     * Return the list of metadata elements.
+     * Return the element result.
      *
-     * @return result object
+     * @return bean
      */
-    public List<ElementHeader> getElementList()
+    public TranslationDetail getElement()
     {
-        if (elementList == null)
-        {
-            return null;
-        }
-        else if (elementList.isEmpty())
-        {
-            return null;
-        }
-        else
-        {
-            return new ArrayList<>(elementList);
-        }
+        return element;
     }
 
 
     /**
-     * Set up the metadata element to return.
+     * Set up the element result.
      *
-     * @param elementList result object
+     * @param element  bean
      */
-    public void setElementList(List<ElementHeader> elementList)
+    public void setElement(TranslationDetail element)
     {
-        this.elementList = elementList;
+        this.element = element;
     }
 
 
@@ -95,8 +84,8 @@ public class ElementHeadersResponse extends OMAGGAFAPIResponse
     @Override
     public String toString()
     {
-        return "ElementHeadersResponse{" +
-                "elementList=" + elementList +
+        return "TranslationDetailResponse{" +
+                "element=" + element +
                 ", exceptionClassName='" + getExceptionClassName() + '\'' +
                 ", exceptionCausedBy='" + getExceptionCausedBy() + '\'' +
                 ", actionDescription='" + getActionDescription() + '\'' +
@@ -124,7 +113,7 @@ public class ElementHeadersResponse extends OMAGGAFAPIResponse
         {
             return true;
         }
-        if (objectToCompare == null || getClass() != objectToCompare.getClass())
+        if (!(objectToCompare instanceof TranslationDetailResponse))
         {
             return false;
         }
@@ -132,8 +121,8 @@ public class ElementHeadersResponse extends OMAGGAFAPIResponse
         {
             return false;
         }
-        ElementHeadersResponse that = (ElementHeadersResponse) objectToCompare;
-        return Objects.equals(elementList, that.elementList);
+        TranslationDetailResponse that = (TranslationDetailResponse) objectToCompare;
+        return Objects.equals(element, that.element);
     }
 
 
@@ -145,6 +134,6 @@ public class ElementHeadersResponse extends OMAGGAFAPIResponse
     @Override
     public int hashCode()
     {
-        return Objects.hash(super.hashCode(), elementList);
+        return Objects.hash(element);
     }
 }
