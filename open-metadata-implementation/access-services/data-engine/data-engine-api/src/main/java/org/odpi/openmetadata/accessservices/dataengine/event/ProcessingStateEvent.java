4064c93aa0221c80e0ffc5f69ac2cfb2de429fe7
Alexandra Bucur <alexandra-nona.bucur@ing.com>
2022-11-08 11:53:16 +0200

    DE Proxy Service - fix sonatype-lift findings
    Signed-off-by: Alexandra Bucur <alexandra-nona.bucur@ing.com>

--- open-metadata-implementation/access-services/data-engine/data-engine-api/src/main/java/org/odpi/openmetadata/accessservices/dataengine/event/ProcessingStateEvent.java
+++ open-metadata-implementation/access-services/data-engine/data-engine-api/src/main/java/org/odpi/openmetadata/accessservices/dataengine/event/ProcessingStateEvent.java
@@ -32,7 +32,7 @@ public class ProcessingStateEvent extends DataEngineEventHeader {
      * Sets the serial version ID
      * @param serialVersionUID the serial version ID
      */
-    private long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * The processing state.
