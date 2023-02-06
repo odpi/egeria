400c5c8262d688685ef649f4cd50c0e5cb50b974
Nigel Jones <nigel.l.jones+git@gmail.com>
2023-02-06 10:48:24 +0000

    #7330 update remaining version identifiers to 4.0
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- open-metadata-implementation/admin-services/admin-services-server/src/main/java/org/odpi/openmetadata/adminservices/server/OMAGServerOriginServices.java
+++ open-metadata-implementation/admin-services/admin-services-server/src/main/java/org/odpi/openmetadata/adminservices/server/OMAGServerOriginServices.java
@@ -8,7 +8,7 @@ package org.odpi.openmetadata.adminservices.server;
  */
 public class OMAGServerOriginServices
 {
-    final String   implementationOrigin = "Egeria OMAG Server Platform (version 3.16-SNAPSHOT)\n";
+    final String   implementationOrigin = "Egeria OMAG Server Platform (version 4.0-SNAPSHOT)\n";
 
     /**
      * Return the origin of this server implementation.
