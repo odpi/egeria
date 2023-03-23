9b9d1b3ae4552edfe68ba6a460410039f0db5bcb
Bogdan Sava <bogdan.sava@gmail.com>
2023-03-23 08:41:30 +0200

    replace javax with jakarta validation api
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- open-metadata-implementation/access-services/glossary-view/glossary-view-spring/src/main/java/org/odpi/openmetadata/accessservices/glossaryview/server/spring/CategoryResource.java
+++ open-metadata-implementation/access-services/glossary-view/glossary-view-spring/src/main/java/org/odpi/openmetadata/accessservices/glossaryview/server/spring/CategoryResource.java
@@ -4,6 +4,9 @@ package org.odpi.openmetadata.accessservices.glossaryview.server.spring;
 
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.odpi.openmetadata.accessservices.glossaryview.rest.GlossaryViewEntityDetailResponse;
 import org.odpi.openmetadata.accessservices.glossaryview.server.service.CategoryService;
 import org.springframework.validation.annotation.Validated;
@@ -12,10 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PositiveOrZero;
 
 import static org.odpi.openmetadata.accessservices.glossaryview.server.spring.OmasRegistration.PAGE_FROM_DEFAULT_VALUE;
 import static org.odpi.openmetadata.accessservices.glossaryview.server.spring.OmasRegistration.PAGE_SIZE_DEFAULT_VALUE;
@@ -28,7 +27,11 @@ import static org.odpi.openmetadata.accessservices.glossaryview.server.spring.Om
 @Validated
 @RequestMapping("/servers/{serverName}/open-metadata/access-services/glossary-view/users/{userId}")
 
-@Tag(name="Glossary View OMAS", description="The Glossary View OMAS provides APIs and events for retrieving glossaries, categories and terms.", externalDocs=@ExternalDocumentation(description="Glossary View Open Metadata Access Service (OMAS)",url="https://egeria-project.org/services/omas/glossary-view/overview/"))
+@Tag(name="Glossary View OMAS",
+        description="The Glossary View OMAS provides APIs and events for retrieving glossaries, categories and terms.",
+        externalDocs=@ExternalDocumentation(
+                description="Glossary View Open Metadata Access Service (OMAS)",
+                url="https://egeria-project.org/services/omas/glossary-view/overview/"))
 
 public class CategoryResource {
 
