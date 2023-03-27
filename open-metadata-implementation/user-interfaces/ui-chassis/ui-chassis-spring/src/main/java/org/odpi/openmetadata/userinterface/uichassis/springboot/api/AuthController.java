b0c494b698ca61f378bc882c042eb18669ad4be6
Bogdan Sava <bogdan.sava@gmail.com>
2023-03-27 10:34:13 +0300

    change cors configuration add /api/token2 uri mapping for json payload
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/java/org/odpi/openmetadata/userinterface/uichassis/springboot/api/AuthController.java
+++ open-metadata-implementation/user-interfaces/ui-chassis/ui-chassis-spring/src/main/java/org/odpi/openmetadata/userinterface/uichassis/springboot/api/AuthController.java
@@ -4,20 +4,17 @@ package org.odpi.openmetadata.userinterface.uichassis.springboot.api;
 
 import org.odpi.openmetadata.userinterface.uichassis.springboot.auth.model.LoginRequest;
 import org.odpi.openmetadata.userinterface.uichassis.springboot.auth.service.TokenService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
-
-    private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
 
     private final TokenService tokenService;
     private final AuthenticationManager authenticationManager;
@@ -27,10 +24,17 @@ public class AuthController {
         this.authenticationManager = authenticationManager;
     }
 
-    @PostMapping("/api/token")
+    @PostMapping("/api/token2")
     public String token(@RequestBody LoginRequest userLogin) throws AuthenticationException {
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(userLogin.username(), userLogin.password()));
+        return tokenService.generateToken(authentication);
+    }
+
+    @PostMapping("/api/token")
+    public String token(@RequestParam String username, @RequestParam String password) throws AuthenticationException {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(username, password));
         return tokenService.generateToken(authentication);
     }
 
