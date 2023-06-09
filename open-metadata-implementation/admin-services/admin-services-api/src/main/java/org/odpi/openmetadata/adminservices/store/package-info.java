2505beb8937670bb815fd352c4e442e6b0a3315d
Mandy Chessell <mandy.e.chessell@gmail.com>
2023-06-09 15:33:23 +0100

    Move JavaDoc comment
    Signed-off-by: Mandy Chessell <mandy.e.chessell@gmail.com>

--- open-metadata-implementation/admin-services/admin-services-api/src/main/java/org/odpi/openmetadata/adminservices/store/package-info.java
+++ open-metadata-implementation/admin-services/admin-services-api/src/main/java/org/odpi/openmetadata/adminservices/store/package-info.java
@@ -1,8 +1,8 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright Contributors to the ODPi Egeria project. */
-package org.odpi.openmetadata.adminservices.store;
 
 /**
  * The <b>store</b> package provides the interface definition for the data store used to store configuration documents.
  * Egeria has a default implementation of this connector that stores each configuration document in a separate file.
  */
+package org.odpi.openmetadata.adminservices.store;
