9f976bc1c120c35daa507c76df81e6c6ecf9bc09
tijanapavicic <tijana.pavicic@ing.com>
2023-07-12 12:15:37 +0200

    [omag-server] minor refactoring
    Signed-off-by: tijanapavicic <tijana.pavicic@ing.com>

--- open-metadata-implementation/server-chassis/server-chassis-spring/src/main/java/org/odpi/openmetadata/server/OMAGServer.java
+++ open-metadata-implementation/server-chassis/server-chassis-spring/src/main/java/org/odpi/openmetadata/server/OMAGServer.java
@@ -3,14 +3,12 @@
 package org.odpi.openmetadata.server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
 import org.odpi.openmetadata.adminservices.configuration.properties.OMAGServerConfig;
-import org.odpi.openmetadata.http.HttpHelper;
 import org.odpi.openmetadata.platformservices.rest.SuccessMessageResponse;
 import org.odpi.openmetadata.platformservices.server.OMAGServerOperationalServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -20,10 +18,10 @@ import org.springframework.boot.context.event.ApplicationFailedEvent;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationContextException;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
@@ -33,90 +31,138 @@ import java.nio.file.Path;
 import java.util.List;
 
 @SpringBootApplication(
-        scanBasePackages = {"${scan.packages:org.odpi.openmetadata.*}"}
+        scanBasePackages = {"org.odpi.openmetadata"}
 )
 //TODO: ADD JAVADOCS!!!
 public class OMAGServer {
-    private final Environment env;
+
+    public static final String OMAG_SERVER = "[omag-server]-";
     private static final Logger LOG = LoggerFactory.getLogger(OMAGServer.class);
-    private static final ObjectReader OBJECT_READER = new ObjectMapper().reader();
-
-    @Value("${strict.ssl:true}")
-    Boolean strictSSL;
-
-    @Value("${server.ssl.enabled:true}")
-    Boolean serverSSL;
+    private final OMAGServerOperationalServices operationalServices;
 
     @Value("${startup.user:system}")
     String sysUser;
 
     @Value("${omag.server.config}")
     Resource omagServerConfigLocation;
-    private final OMAGServerOperationalServices operationalServices;
+
+    @Autowired
+    private ObjectMapper objectMapper;
     private String serverName = null;
     private OMAGServerConfig serverConfig = null;
 
 
-    public OMAGServer(Environment env) {
-        this.env = env;
+    public OMAGServer() {
         this.operationalServices = new OMAGServerOperationalServices();
     }
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder().sources(OMAGServer.class).run(args);
+        new SpringApplicationBuilder()
+                .listeners(new ApplicationListener<ApplicationEvent>() {
+                    @Override
+                    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+//                        LOG.info("[[ OMAGServer ]] >> LISTENER :: {}", applicationEvent.getClass().getCanonicalName());
+//                        LOG.info("{}listener::{}", OMAG_SERVER, applicationEvent.getClass().getSimpleName());
+                    }
+                })
+                .sources(OMAGServer.class).run(args);
     }
 
-    //TODO: This code is common for the platform and server spring boot applications - as such should be moved to common util class to avoid code duplication and potentially inconsistent behaviour.
-    @Bean
-    public InitializingBean getInitialize() {
-        return () ->
-        {
-            LOG.info("Working directory is: " + System.getProperty("user.dir"));
-
-            if (!strictSSL) {
-                LOG.warn("Option strict.ssl is set to false! Invalid certificates will be accepted for connection!");
-                HttpHelper.noStrictSSL();
-            }
-
-            if (serverSSL && System.getProperty("javax.net.ssl.trustStore") == null) {
-                LOG.warn("Java trust store 'javax.net.ssl.trustStore' is null - this is needed by Tomcat - using 'server.ssl.trust-store'");
-
-                /*
-                 * load the 'javax.net.ssl.trustStore' and 'javax.net.ssl.trustStorePassword' from application.properties.
-                 * Note, these variables should only used for mutual SSL.  This function is provided for backward compatibility.
-                 * Also note that there is an NPE if the java variables are set to null.
-                 */
-                if (env.getProperty("server.ssl.trust-store") != null) {
-                    System.setProperty("javax.net.ssl.trustStore", env.getProperty("server.ssl.trust-store"));
-                }
-                if (env.getProperty("server.ssl.trust-store-password") != null) {
-                    System.setProperty("javax.net.ssl.trustStorePassword", env.getProperty("server.ssl.trust-store-password"));
-                }
-            }
-        };
-    }
 
     @EventListener(ApplicationReadyEvent.class)
     private void onApplicationReadyEvent() {
-        LOG.debug(">> Application ready");
+        LOG.debug("{}{}-[accepting traffic]", OMAG_SERVER, serverName);
     }
 
     @EventListener(ApplicationStartedEvent.class)
-    private void onApplicationStartedEvent() throws Exception{
-        LOG.debug(">> Application started");
+    private void onApplicationStartedEvent() throws Exception {
+        LOG.debug("{}[started]", OMAG_SERVER);
         loadServerConfig();
     }
 
     @EventListener(ApplicationFailedEvent.class)
     private void onApplicationFailedEvent() {
-        LOG.debug(">> Application failed");
+        LOG.debug("{}[failed]", OMAG_SERVER);
     }
+
     @EventListener(ContextClosedEvent.class)
     private void onContextClosedEvent() {
-        LOG.debug(">> Context closed");
+        LOG.debug("{}[stopped]", OMAG_SERVER);
+
         if (serverName != null) {
             LOG.info("Application stopped, deactivating server {}", serverName);
             operationalServices.deactivateTemporarilyServerList(sysUser, List.of(serverName));
+        }
+    }
+
+    private void activateOMAGServerUsingPlatformServices() {
+
+        LOG.info("{}[activation]-[started] [for name] :: {}", OMAG_SERVER, serverName);
+
+        if (serverConfig == null) {
+            LOG.info("{}[activation]-[failed] [cause] :: configuration is null.", OMAG_SERVER);
+            throw new ApplicationContextException("[omag-server activation]-[failed] [cause] :: configuration is null.");
+
+       /*     TODO: Confirm if this is desired behaviour
+             This is clearly invalid application state since the OMAG system cannot start
+             without configuration. Throwing error will close application context and shut the application DOWN.*/
+        }
+
+        LOG.info("{}[activation]-[started] [request sent for server name] :: {}", OMAG_SERVER, serverName);
+
+        SuccessMessageResponse response = operationalServices
+                .activateWithSuppliedConfig(sysUser.trim(), serverConfig.getLocalServerName(), serverConfig);
+
+        if (response == null) {
+            LOG.info("{}[activation]-[failed] [cause] :: response is null.", OMAG_SERVER);
+            throw new ApplicationContextException("[omag-server activation]-[failed] [cause] :: response is null.");
+        }
+
+        if (response.getRelatedHTTPCode() != 200) {
+            LOG.error("{}[activation]-[failed] [cause] :: response code is {}.", OMAG_SERVER, response.getRelatedHTTPCode());
+            return;
+
+/*            TODO: OMAG system start-up error handling and application readiness probe
+             In most cases it is state caused by configuration problem and cannot be recovered at runtime
+             Two options:
+             1) Propagate the error further i.e. Runtime/ApplicationContextException which will cause context to be closed and application shut DOWN
+             throw new ApplicationContextException(response.getExceptionErrorMessage());
+             2) Do not propagate error, log the error message and set application ready state to FALSE
+             this will keep the application UP and the operator will have to manually change the configuration
+             and restart the application/container*/
+        }
+
+        if (response.getRelatedHTTPCode() == 200) {
+            LOG.info("{}[activation]-[success] [started server for name] :: {}", OMAG_SERVER, serverConfig.getLocalServerName());
+            //TODO: Mark the application state as ready
+            // i.e. set application ready state to TRUE
+        }
+
+    }
+
+    private void loadServerConfig() {
+
+
+        if (omagServerConfigLocation == null) {
+            LOG.info("{}[config]-[failed] [cause] :: file is null", OMAG_SERVER);
+            //TODO: Confirm if this is desired behaviour (configuration is null)
+            // This is clearly invalid application state since the OMAG system cannot start without configuration.
+            // Throwing error will close application context and shut the application DOWN.
+            throw new ApplicationContextException(String.format("{}-[config]-[failed] [cause] :: file is null", OMAG_SERVER));
+        }
+        try {
+            LOG.info("{}[config]-[started] [location] :: {}", OMAG_SERVER, omagServerConfigLocation.getFile());
+            LOG.trace("{}[config]-[started] [path] :: {}", OMAG_SERVER, Files.readString(Path.of(omagServerConfigLocation.getFile().getPath())));
+            serverConfig = objectMapper.reader().readValue(omagServerConfigLocation.getFile(), OMAGServerConfig.class);
+            serverName = serverConfig.getLocalServerName();
+            LOG.info("{}[config]-[success] [loaded configuration document for OMAG server name] :: {}", OMAG_SERVER, serverName);
+
+        } catch (IOException e) {
+            LOG.error("{}[config]-[failed] [Exception while loading OMAG server configuration] [cause] :: {}", OMAG_SERVER, e.getMessage());
+            //TODO: Confirm if this is desired behaviour (configuration is null)
+            // Same as in the case above.
+            throw new ApplicationContextException(
+                    String.format("{}[config]-[failed] [Exception while loading OMAG server configuration] [cause] :: {}", OMAG_SERVER, e.getMessage()));
         }
     }
 
@@ -124,57 +170,8 @@ public class OMAGServer {
     public class OMAGServerStartup implements ApplicationRunner {
         @Override
         public void run(ApplicationArguments args) {
-            LOG.debug(">> Application runner");
+            LOG.debug("{}[run] Application runner", OMAG_SERVER);
             activateOMAGServerUsingPlatformServices();
-        }
-    }
-
-    private void activateOMAGServerUsingPlatformServices() {
-
-            if (serverConfig != null) {
-                LOG.info("Activating OMAG server with name {}", serverName);
-                SuccessMessageResponse response = operationalServices.activateWithSuppliedConfig(sysUser.trim(), serverConfig.getLocalServerName(), serverConfig);
-                if (response.getRelatedHTTPCode() == 200) {
-                    LOG.info("Successfully started OMAG server {}", serverConfig.getLocalServerName());
-                    //TODO: Mark the application state as ready
-                    // i.e. set application ready state to TRUE
-                } else {
-                    LOG.error("OMAG server activation failure");
-                    //TODO: OMAG system start-up error handling and application readiness probe
-                    // In most cases it is state caused by configuration problem and cannot be recovered at runtime
-                    // Two options:
-                    // 1) Propagate the error further i.e. Runtime/ApplicationContextException which will cause context to be closed and application shut DOWN
-                    // throw new ApplicationContextException(response.getExceptionErrorMessage());
-                    // 2) Do not propagate error, log the error message and set application ready state to FALSE
-                    // this will keep the application UP and the operator will have to manually change the configuration and restart the application/container
-                }
-            } else {
-                LOG.info("OMAG server configuration is null, server cannot be activated");
-                //TODO: Confirm if this is desired behaviour
-                // This is clearly invalid application state since the OMAG system cannot start without configuration. Throwing error will close application context and shut the application DOWN.
-                throw new ApplicationContextException("OMAG server configuration is null");
-            }
-    }
-
-    private void loadServerConfig() {
-        try {
-            if (omagServerConfigLocation != null) {
-                LOG.info("Lading OMAG server configuration from location {}", omagServerConfigLocation.getFile());
-                LOG.trace(Files.readString(Path.of(omagServerConfigLocation.getFile().getPath())));
-                serverConfig = OBJECT_READER.readValue(omagServerConfigLocation.getFile(), OMAGServerConfig.class);
-                serverName = serverConfig.getLocalServerName();
-                LOG.info("Successfully loaded configuration document for OMAG server {}", serverName);
-            } else {
-                LOG.error("OMAG server config location provided is null");
-                //TODO: Confirm if this is desired behaviour (configuration is null)
-                // This is clearly invalid application state since the OMAG system cannot start without configuration. Throwing error will close application context and shut the application DOWN.
-                throw new ApplicationContextException("OMAG server configuration is null");
-            }
-        } catch (IOException e) {
-            LOG.error("Exception while loading OMAG server configuration", e);
-            //TODO: Confirm if this is desired behaviour (configuration is null)
-            // Same as in the case above.
-            throw new ApplicationContextException("Exception loading OMAG server configuration");
         }
     }
 }
---
+++ open-metadata-implementation/server-chassis/server-chassis-spring/src/main/java/org/odpi/openmetadata/server/config/InitializingBeanConfig.java
@@ -0,0 +1,65 @@
+package org.odpi.openmetadata.server.config;
+
+import org.odpi.openmetadata.http.HttpHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class InitializingBeanConfig {
+    public static final String PREFIX = "[[ssl config]]";
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    public static final String OPTION_STRICT_SSL_IS_SET_TO_FALSE_INVALID_CERTIFICATES_WILL_BE_ACCEPTED_FOR_CONNECTION =
+            "Option strict.ssl is set to false! Invalid certificates will be accepted for connection!";
+    public static final String JAVAX_NET_SSL_TRUST_STORE = "javax.net.ssl.trustStore";
+    public static final String SERVER_SSL_TRUST_STORE = "server.ssl.trust-store";
+    public static final String SERVER_SSL_TRUST_STORE_PASSWORD = "server.ssl.trust-store-password";
+    public static final String JAVAX_NET_SSL_TRUST_STORE_PASSWORD = "javax.net.ssl.trustStorePassword";
+
+    private final Environment env;
+
+    @Value("${strict.ssl:true}")
+    Boolean strictSSL;
+
+    @Value("${server.ssl.enabled:true}")
+    Boolean serverSSL;
+
+    public InitializingBeanConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public InitializingBean getInitialize() {
+        return () ->
+        {
+            log.info("{} working directory: {}", PREFIX, System.getProperty("user.dir"));
+
+            if (!strictSSL) {
+                log.warn("{} :: {}", PREFIX , OPTION_STRICT_SSL_IS_SET_TO_FALSE_INVALID_CERTIFICATES_WILL_BE_ACCEPTED_FOR_CONNECTION);
+                HttpHelper.noStrictSSL();
+            }
+
+            if (serverSSL && System.getProperty(JAVAX_NET_SSL_TRUST_STORE) == null) {
+                log.warn("{} Java trust store '{}' is null - this is needed by Tomcat - using '{}'", PREFIX, JAVAX_NET_SSL_TRUST_STORE, SERVER_SSL_TRUST_STORE);
+
+                /*
+                 * load the 'javax.net.ssl.trustStore' and 'javax.net.ssl.trustStorePassword' from application.properties.
+                 * Note, these variables should only used for mutual SSL.  This function is provided for backward compatibility.
+                 * Also note that there is an NPE if the java variables are set to null.
+                 */
+                if (env.getProperty(SERVER_SSL_TRUST_STORE) != null) {
+                    System.setProperty(JAVAX_NET_SSL_TRUST_STORE, env.getProperty(SERVER_SSL_TRUST_STORE));
+                }
+                if (env.getProperty(SERVER_SSL_TRUST_STORE_PASSWORD) != null) {
+                    System.setProperty(JAVAX_NET_SSL_TRUST_STORE_PASSWORD, env.getProperty(SERVER_SSL_TRUST_STORE_PASSWORD));
+                }
+            }
+        };
+
+    }
+}
