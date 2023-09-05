f932e87b5500396685e2e77989969a18ceb1fa34
Mandy Chessell <mandy.e.chessell@gmail.com>
2023-09-05 13:12:17 +0100

    Rename report packages to match gradle
    Signed-off-by: Mandy Chessell <mandy.e.chessell@gmail.com>

--- open-metadata-resources/open-metadata-reports/report-utilities/src/main/java/org/odpi/openmetadata/reports/EgeriaReport.java
+++ open-metadata-resources/open-metadata-reports/report-utilities/src/main/java/org/odpi/openmetadata/reports/EgeriaReport.java
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright Contributors to the ODPi Egeria project. */
-package org.odpi.openmetadata.devprojects.reports;
+package org.odpi.openmetadata.reports;
 
 
 
